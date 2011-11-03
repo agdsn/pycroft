112e0c7f2f5459089ade922e3ff466ea6102213f
Jan Losinski <losinski@wh2.tu-dresden.de>
2011-11-03 19:40:50 +0100

    initial commit

---
+++ pycroft/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2011 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+
+  
