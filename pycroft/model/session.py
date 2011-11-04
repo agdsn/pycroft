08574873c9bf73247376cc38e0fb255695519ddf
Jan Losinski <losinski@wh2.tu-dresden.de>
2011-11-04 03:34:46 +0100

    Some basic snippets to worf with sqlalchemy. currently untested!
    refs #136

---
+++ pycroft/model/session.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+"""
+    pycroft.model.session
+    ~~~~~~~~~~~~~~
+
+    This module contains the session stuff for db actions.
+
+    :copyright: (c) 2011 by AG DSN.
+"""
+
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+
+class SessionWrapper(object):
+    def __init__(self):
+        self._engine = create_engine("sqlite:///test_db.sqlite", echo=False)
+        self._scoped_session = scoped_session(sessionmaker(bind=_engine))
+
+    def __getattr__(self, item):
+        return getattr(self._scoped_session(), item)
+
+    def get_engine(self):
+        return self._engine
+
+    
+session = SessionWrapper()
