ae069951df13d522b0bf6219741fc1310f0a56ff
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2011-11-13 21:04:47 +0100

    ADD sqlachemy-mirgate support (run ./new_migration.sh, if model has changed)
    fixes #138 @2h

---
+++ migration/manage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Copyright (c) 2011 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(url='sqlite:///test_db.sqlite', debug='False')
