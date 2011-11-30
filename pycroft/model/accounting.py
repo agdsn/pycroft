2d6d077746afd48e9eeeb7af822278f472224256
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2011-11-30 19:40:43 +0100

    moved TrafficVolume, added TrafficLimit
    moved TrafficVolume to accounting.py
    added TrafficLimit to accounting.py and added relationship from Group to
    TrafficLimit

--- pycroft/model/accounting.py
+++ pycroft/model/accounting.py
@@ -3,12 +3,12 @@
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 """
-    pycroft.model.logging
-    ~~~~~~~~~~~~~~
+pycroft.model.accounting
+~~~~~~~~~~~~~~
 
-    This module contains the classes LogEntry, UserLogEntry, TrafficVolume.
+This module contains the classes LogEntry, UserLogEntry, TrafficVolume.
 
-    :copyright: (c) 2011 by AG DSN.
+:copyright: (c) 2011 by AG DSN.
 """
 from base import ModelBase
 from sqlalchemy import ForeignKey
@@ -18,20 +18,18 @@ from sqlalchemy.types import Boolean, BigInteger, Integer, DateTime
 from sqlalchemy.types import Text
 
 
-class LogEntry(ModelBase):
-    # variably sized string
-    message = Column(Text)
-    # created
+class TrafficVolume(ModelBase):
+    incoming = Column(Boolean)
+    # how many bytes
+    size = Column(BigInteger)
+    # when this was logged
     timestamp = Column(DateTime)
 
     # many to one from LogEntry to User
-    author = relationship("User",
-                backref=backref("log_entries"))
-    author_id = Column(Integer, ForeignKey("user.id"))
-
-
-class UserLogEntry(ModelBase):
-    # many to one from UserLogEntry to User
     user = relationship("User",
-                backref=backref("user_log_entries"))
+                backref=backref("traffic_volumes"))
     user_id = Column(Integer, ForeignKey("user.id"))
+
+class TrafficLimit(ModelBase):
+    # in megabyte
+    size= Column(Integer)
