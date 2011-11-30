791d85502ff08740df1528902157d3df49428310
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2011-11-30 20:19:26 +0100

    Added type tag to TrafficVolume and FinanceAccount
    replaced flag with Enum
    fixes #183
    
    removed FinanceAccountType
    use Enum now
    refs #149

--- pycroft/model/accounting.py
+++ pycroft/model/accounting.py
@@ -11,22 +11,23 @@ from base import ModelBase
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.types import Boolean, BigInteger, Integer, DateTime
+from sqlalchemy.types import Boolean, BigInteger, Enum, Integer, DateTime
 from sqlalchemy.types import Text
 
 
 class TrafficVolume(ModelBase):
-    incoming = Column(Boolean)
     # how many bytes
     size = Column(BigInteger)
     # when this was logged
     timestamp = Column(DateTime)
+    type = Column(Enum("IN", "OUT", name="traffic_types"))
 
     # many to one from LogEntry to User
     user = relationship("User",
                 backref=backref("traffic_volumes"))
     user_id = Column(Integer, ForeignKey("user.id"))
 
+
 class TrafficLimit(ModelBase):
     # in megabyte
-    size= Column(Integer)
+    size = Column(Integer)
--- pycroft/model/finance.py
+++ pycroft/model/finance.py
@@ -11,24 +11,22 @@ from base import ModelBase
 from sqlalchemy import ForeignKey
 from sqlalchemy import Table, Column
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.types import Integer, Text, DateTime
+from sqlalchemy.types import Enum, Integer, Text, DateTime
 from sqlalchemy.types import String
 
 
-class FinanceAccountType(ModelBase):
-    name = Column(String(255))
-    description = Column(String(255))
-
 class FinanceAccount(ModelBase):
     name = Column(String(127))
-    type_id = Column(Integer(), ForeignKey("financeaccounttype.id"))
-    type = relationship("FinanceAccountType")
+    type = Column(Enum("LIABILITY", "EXPENSE", "ASSET", "INCOME", "EQUITY",
+                        name="financeaccounttypes"))
+
 
 class Journal(ModelBase):
     account = Column(String(255))
     bank = Column(String(255))
     hbci_url = Column(String(255))
     last_update = Column(DateTime())
+
 
 class JournalEntry(ModelBase):
     message = Column(Text())
@@ -40,14 +38,17 @@ class JournalEntry(ModelBase):
     original_message = Column(Text())
     timestamp = Column(DateTime())
 
+
 class Transaction(ModelBase):
     message = Column(Text())
     journal_entry_id = Column(Integer(), ForeignKey("journalentry.id"))
-    journal_entry = relationship("JournalEntry", backref=backref("transaction"))
-	
+    journal_entry = relationship("JournalEntry",
+                                    backref=backref("transaction"))
+
+
 class Split(ModelBase):
     amount = Column(Integer())
-    from_account_id  = Column(Integer(), ForeignKey("financeaccount.id"))
+    from_account_id = Column(Integer(), ForeignKey("financeaccount.id"))
     from_account = relationship("FinanceAccount")
     to_account_id = Column(Integer(), ForeignKey("financeaccount.id"))
     to_account = relationship("FinanceAccount")
