b697fe688f1cff38879f8434cd2aecaee4ab3047
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2012-03-31 23:31:01 +0200

    shorter function names

--- web/blueprints/finance.py
+++ web/blueprints/finance.py
@@ -19,17 +19,17 @@ bp = Blueprint('bp_finance', __name__, )
 
 @bp.route('/finance')
 @bp.route('/finance/journals')
-def finance_journals():
+def journals():
     return render_template('test.html', page_title = u"Journals", subnav = 'nav/finance.html')
 
 
 @bp.route('/finance/accounts')
-def finance_accounts():
+def accounts():
     return render_template('test.html', page_title = u"Konten", subnav = 'nav/finance.html')
 
 
 @bp.route('/finance/transactions')
-def finance_transactions():
+def transactions():
     return render_template('test.html', page_title = u"Transaktionen", subnav = 'nav/finance.html')
 
 
