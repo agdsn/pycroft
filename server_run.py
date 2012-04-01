3e67168752314425cb156a0cb39f5e3008029b10
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-04-01 03:25:16 +0200

    secure app.run from execution on import

--- server_run.py
+++ server_run.py
@@ -2,4 +2,6 @@
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 from web import app
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
