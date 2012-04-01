6157006ef1e383a3749fc9c61b386102686983bf
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-04-01 03:31:41 +0200

    Create the flask app within a function instead at package import.

--- server_run.py
+++ server_run.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
-from web import app
+from web import make_app
 
 if __name__ == "__main__":
+    app = make_app()
     app.run(debug=True)
