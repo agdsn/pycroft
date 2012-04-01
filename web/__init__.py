74df2712ba5638eee4f8204c2fa7e89351fbc3fe
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-04-01 03:28:15 +0200

    The secret key of an flask app should be constant over restarts
    its used to verify stuff like cookies for example

--- web/__init__.py
+++ web/__init__.py
@@ -18,6 +18,6 @@ import time
 ### Configurations
 app = Flask(__name__)
 #initialization code
-app.secret_key = str(time.time())
+app.secret_key = "eiNohfaefaig5Iek6oshai0eijuph4ohla6Eo1vi5bahnaeh3Bah7ohy1einuaxu"
 
 from blueprints import finance, infrastructure, rights, user
