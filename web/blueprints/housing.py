18d1477fa97c5452a3f80766e64c5013f9b34150
Dominik <cookie@wh2.tu-dresden.de>
2012-04-04 20:40:20 +0200

    testing Jans macros and building a form

--- web/blueprints/housing.py
+++ web/blueprints/housing.py
@@ -8,7 +8,9 @@
     :copyright: (c) 2012 by AG DSN.
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flaskext.wtf import Form, TextField, validators
+from pycroft.model import dormitory, session
 
 bp = Blueprint('housing', __name__, )
 
@@ -18,8 +20,27 @@ bp = Blueprint('housing', __name__, )
 def rooms():
     return render_template('housing/housing_base.html', page_title = u"Räume")
 
+class DormitoryForm(Form):
+    short_name = TextField(u"Kürzel")
+    number = TextField(u"Nummer")
+    street = TextField(u"Straße", validators=[validators.Length(min=5)])
 
 @bp.route('/dormitories')
 def dormitories():
-    return render_template('housing/housing_base.html', page_title = u"Wohnheime")
+    dormi = dormitory.Dormitory.q.all()
+    return render_template('housing/dormitory_list.html',
+                           page_title = u"Wohnheime", dormitories=dormi)
 
+
+@bp.route('/dormitories/new', methods=['GET', 'POST'])
+def dormitory_new():
+    form = DormitoryForm()
+    if form.validate_on_submit():
+        myDormitory = dormitory.Dormitory(short_name=form.short_name.data,
+            street=form.street.data, number=form.number.data)
+        session.session.add(myDormitory)
+        session.session.commit()
+        flash('Haus angelegt', 'success')
+        return redirect(url_for('.dormitories'))
+    return render_template('housing/dormitory_new.html',
+                           page_title = u"Neues Wohnheim", form=form)
