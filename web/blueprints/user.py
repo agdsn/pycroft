87283e2506f0b32fc368867dbe663b4fc708f324
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2012-04-10 16:11:42 +0200

    removed tabs and renamed housing blueprint to dormitories
    fix pep8

--- web/blueprints/user.py
+++ web/blueprints/user.py
@@ -17,25 +17,24 @@ from web.blueprints import BlueprintNavigation
 bp = Blueprint('user', __name__, )
 nav = BlueprintNavigation(bp, "Nutzer")
 
+
 @bp.route('/')
 @nav.navigate(u"Übersicht")
 def overview():
-    return render_template('user/user_base.html', page_title = u"Übersicht")
+    return render_template('user/base.html', page_title=u"Übersicht")
 
 
-@bp.route('/new')
+@bp.route('/create')
 @nav.navigate("Anlegen")
 def create():
     flash("Test1", "info")
     flash("Test2", "warning")
     flash("Test3", "error")
     flash("Test4", "success")
-    return render_template('user/user_base.html', page_title = u"Neuer Nutzer")
+    return render_template('user/base.html', page_title=u"Neuer Nutzer")
 
 
 @bp.route('/search')
 @nav.navigate("Suchen")
 def search():
-    return render_template('user/user_base.html', page_title = u"Nutzer Suchen")
-
-
+    return render_template('user/base.html', page_title=u"Nutzer Suchen")
