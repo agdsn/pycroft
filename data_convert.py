bb084de321e095a17516314b7d68345005b3eaf0
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-04-10 16:07:35 +0200

    add a script to use the data converter

---
+++ data_convert.py
@@ -0,0 +1,14 @@
+from legacy import convert
+from pycroft import model
+
+if __name__ == "__main__":
+
+    print "drop old db model"
+    model.drop_db_model()
+
+    print "create new db model"
+    model.create_db_model()
+
+    print "convert data"
+    convert.do_convert()
+    print "complete"
