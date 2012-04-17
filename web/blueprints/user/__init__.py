8fc340e7e0d9c0a2e4fc7fa17899497c08384325
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2012-04-17 17:54:27 +0200

    started an overview of users

--- web/blueprints/user/__init__.py
+++ web/blueprints/user/__init__.py
@@ -16,13 +16,30 @@ bp = Blueprint('user', __name__, )
 nav = BlueprintNavigation(bp, "Nutzer")
 
 
+def sort_key(dormitory):
+    key = 0
+    power = 1
+    for char in reversed(dormitory.number.lower()):
+        key += ord(char) * pow(10, power)
+        power += power
+
+    return key
+
 @bp.route('/')
 @nav.navigate(u"Übersicht")
 def overview():
     dormitories_list = dormitory.Dormitory.q.all()
+    dormitories_list = sorted(dormitories_list,
+        key=lambda dormitory: sort_key(dormitory))
+
     return render_template('user/overview.html',
         dormitories=dormitories_list)
 
+@bp.route('/dormitory/<dormitory_id>')
+def dormitory_floors(dormitory_id):
+    floors_list = ["dummy 1","dummy 2", "dummy 3"]
+    return render_template('user/floors.html',
+        floors=floors_list, page_title=u"Etagen Wohnheim XY")
 
 @bp.route('/create')
 @nav.navigate("Anlegen")
