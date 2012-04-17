96a75685f5c2bfc9d09e06ddcfc40fa2b5005ccf
Florian Österreich <florian.oesterreich@googlemail.com>
2012-04-18 00:15:32 +0200

    refs #208 add functionality to create new users

--- web/blueprints/user/__init__.py
+++ web/blueprints/user/__init__.py
@@ -11,10 +11,11 @@
     :copyright: (c) 2012 by AG DSN.
 """
 
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, url_for
 from pycroft.model import user, session
 from pycroft.model.user import User
 from web.blueprints import BlueprintNavigation
+from web.blueprints.user.forms import UserCreateForm
 
 bp = Blueprint('user', __name__, )
 nav = BlueprintNavigation(bp, "Nutzer")
@@ -25,25 +26,31 @@ nav = BlueprintNavigation(bp, "Nutzer")
 def overview():
     user_list = user.User.q.all()
     return render_template('user/user_list.html',
-                           page_title=u"Nutzerübersicht", users=user_list)
+        page_title=u"Nutzerübersicht", users=user_list)
 
 
 @bp.route('/show/<user_id>')
 def user_show(user_id):
     user_list = user.User.q.filter(User.id == user_id).all()
     return render_template('user/user_show.html',
-                           page_title=u"Nutzer anzeigen: "+user_id,
-                           user=user_list)
+        page_title=u"Nutzer anzeigen: " + user_id,
+        user=user_list)
 
 
-@bp.route('/create')
+@bp.route('/create', methods=['GET', 'POST'])
 @nav.navigate("Anlegen")
 def create():
-    flash("Test1", "info")
-    flash("Test2", "warning")
-    flash("Test3", "error")
-    flash("Test4", "success")
-    return render_template('user/base.html', page_title=u"Neuer Nutzer")
+    form = UserCreateForm()
+    if form.validate_on_submit():
+        myUser = user.User(login=form.login.data,
+            name=form.name.data, registration_date=form.registration_date.data,
+            room_id=form.room_id.data)
+        session.session.add(myUser)
+        session.session.commit()
+        flash('Benutzer angelegt', 'success')
+        return redirect(url_for('.overview'))
+    return render_template('user/user_create.html',
+        page_title=u"Neuer Nutzer", form=form)
 
 
 @bp.route('/search')
