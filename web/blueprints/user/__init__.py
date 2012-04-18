2b79de7c13992e9adf1d67d391a0d2e0a1ed9e6b
Dominik Pataky <pataky@wh2.tu-dresden.de>
2012-04-18 21:55:17 +0200

    fixed user_create "registration_date" with datetime.datetime.now()
    refs #212

--- web/blueprints/user/__init__.py
+++ web/blueprints/user/__init__.py
@@ -17,6 +17,7 @@ from pycroft.model.user import User
 from web.blueprints import BlueprintNavigation
 from web.blueprints.user.forms import UserSearchForm, UserCreateForm
 from pycroft.model import dormitory
+import datetime
 
 bp = Blueprint('user', __name__, )
 nav = BlueprintNavigation(bp, "Nutzer")
@@ -61,8 +62,7 @@ def create():
     form = UserCreateForm()
     if form.validate_on_submit():
         myUser = user.User(login=form.login.data,
-            name=form.name.data, registration_date=form.registration_date.data,
-            room_id=form.room_id.data)
+            name=form.name.data, room_id=form.room_id.data, registration_date=datetime.datetime.now())
         session.session.add(myUser)
         session.session.commit()
         flash('Benutzer angelegt', 'success')
