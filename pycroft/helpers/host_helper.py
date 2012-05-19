20dc502e12a900c2c4d45ccb12a679c2c72cea7f
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-05-19 22:03:26 +0200

    improve sort_ports helper
    Use precompiled regexp and change the unnecessary lambda to the function name

--- pycroft/helpers/host_helper.py
+++ pycroft/helpers/host_helper.py
@@ -7,15 +7,17 @@
 """
 import re
 
-def sort_port_key(port):
-    number = re.search("[0-9]+", port.name)
-    letter = re.search("[a-z]", port.name.lower())
-
-    return int(number.group(0)) + 1024 * ord(letter.group(0))
-
 
 def sort_ports(ports):
-    sorted_ports = sorted(ports,
-        key=lambda port: sort_port_key(port))
+    number_re = re.compile(r"[0-9]+")
+    letter_re = re.compile(r"[a-z]")
+
+    def make_sort_key(port):
+        number = number_re.search(port.name)
+        letter = letter_re.search(port.name.lower())
+
+        return int(number.group(0)) + 1024 * ord(letter.group(0))
+
+    sorted_ports = sorted(ports, key=make_sort_key)
 
     return sorted_ports
