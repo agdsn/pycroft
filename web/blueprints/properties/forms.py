fe8998109116a1359b4e741915bb613213c8e645
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2012-06-06 17:16:51 +0200

    added traffic group list, create, delete

---
+++ web/blueprints/properties/forms.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+
+
+from flaskext.wtf import Form, TextField
+from wtforms.validators import Required, Regexp
+
+class TrafficGroupForm(Form):
+    name = TextField(u"Gruppenname",[Required(message=u"Name?")])
+    traffic_limit = TextField(u"Traffic Limit (GB)",
+                        [Required(message=u"Wie viel GB?"),
+                        Regexp(regex=u"[0-9]+",
+                        message=u"Muss eine natürliche Zahl sein!")])
+
+
+class PropertyGroupForm(Form):
+    name = TextField(u"Gruppenname",[Required(message=u"Name?")])
