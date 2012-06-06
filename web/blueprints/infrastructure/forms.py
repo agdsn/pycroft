d2265e481473249285598bc1b6e9b8e102834488
Christian Jacobs <jacobs@wh2.tu-dresden.de>
2012-06-06 21:18:42 +0200

    create switch port form

---
+++ web/blueprints/infrastructure/forms.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+from flaskext.wtf import Form, TextField
+from wtforms.validators import Required, Regexp
+from pycroft.model.ports import Port
+
+class SwitchPortForm(Form):
+    name = TextField(u"Port Name",
+                        [Required(message=u"Name?"),
+                         Regexp(regex=Port.name_regex,
+                             message=u"Richtig ist z.B. A2")])
+
