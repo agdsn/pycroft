d70bdb70003b5e622f349ddcc0cf7803b7783ed8
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-06-21 01:00:17 +0200

    change the todaybutton javascript to be a jQuery plugin

--- web/static/custom/js/form.js
+++ web/static/custom/js/form.js
@@ -5,27 +5,56 @@
  * Time: 8:59 PM
  * To change this template use File | Settings | File Templates.
  */
-function parseDateFormat(format){
-    var separator = format.match(/[.\/-].*?/),
-            parts = format.split(/\W+/);
-    if (!separator || !parts || parts.length == 0){
-        throw new Error("Invalid date format.");
-    }
-    return {separator: separator, parts: parts};
-}
 
-function formatDate(date, format){
-    var val = {
-        d: date.getDate(),
-        m: date.getMonth() + 1,
-        yy: date.getFullYear().toString().substring(2),
-        yyyy: date.getFullYear()
+
+!function ( $ ) {
+
+    var TodayButton = function (element, options) {
+        this.element = $(element);
+        this.target = $("#" + this.element.data("target"));
+        this.datepicker = this.target.data("datepicker")
+        this.dateFormat = this.datepicker.format;
+
+        this.element.on('click', $.proxy(this.click, this));
     };
-    val.dd = (val.d < 10 ? '0' : '') + val.d;
-    val.mm = (val.m < 10 ? '0' : '') + val.m;
-    var date = [];
-    for (var i=0, cnt = format.parts.length; i < cnt; i++) {
-        date.push(val[format.parts[i]]);
-    }
-    return date.join(format.separator);
-}
+
+    TodayButton.prototype = {
+        constructor:TodayButton,
+
+        click:function (ev) {
+            ev.preventDefault();
+            this.target.val(this.formatDate(new Date()));
+            this.datepicker.update()
+        },
+
+        formatDate:function (date) {
+            var val = {
+                d:date.getDate(),
+                m:date.getMonth() + 1,
+                yy:date.getFullYear().toString().substring(2),
+                yyyy:date.getFullYear()
+            };
+            val.dd = (val.d < 10 ? '0' : '') + val.d;
+            val.mm = (val.m < 10 ? '0' : '') + val.m;
+            var date = [];
+            for (var i = 0, cnt = this.dateFormat.parts.length; i < cnt; i++) {
+                date.push(val[this.dateFormat.parts[i]]);
+            }
+            return date.join(this.dateFormat.separator);
+        }
+    };
+
+    $.fn.todayButton = function (options) {
+        return this.each(function () {
+            if (undefined == $(this).data('todayButton')) {
+                var plugin = new TodayButton(this, options);
+                $(this).data('todayButton', plugin);
+            }
+        });
+    };
+
+    $.fn.todayButton.defaults = {};
+	$.fn.todayButton.Constructor = TodayButton;
+
+}(window.jQuery);
+
