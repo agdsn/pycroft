5519daed9b5e2320878d8074e87e7b7595ec6261
Florian Österreich <florian.oesterreich@googlemail.com>
2012-06-27 22:16:43 +0200

    Empty DataPickerFields are now allowed. If you dont want an empty field use validator required.

--- web/form/fields.py
+++ web/form/fields.py
@@ -1,5 +1,6 @@
 from web.form.widgets import DatePickerWidget, LazyLoadSelectWidget
 from wtforms import fields
+import datetime
 
 
 class DatePickerField(fields.DateField):
@@ -52,6 +53,13 @@ class DatePickerField(fields.DateField):
         date_format = date_format.replace(u"%m", u"mm")
         date_format = date_format.replace(u"%d", u"dd")
         return date_format
+
+    def process_formdata(self, valuelist):
+        try:
+            super(DatePickerField, self).process_formdata(valuelist)
+        except ValueError:
+            if not sum(map(len, valuelist)):
+                return
 
 
 class LazyLoadSelectField(fields.SelectField):
