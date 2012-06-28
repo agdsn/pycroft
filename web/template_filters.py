fed96123cc92fe25ffdfb6c74778c82d5fe53f8d
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-06-29 01:09:17 +0200

    add template filters: date, datetime, timesince
    refs #259

--- web/template_filters.py
+++ web/template_filters.py
@@ -13,6 +13,7 @@
 
 
 from functools import wraps
+from datetime import datetime
 
 
 _filter_registry = {}
@@ -35,6 +36,54 @@ _category_map = {"warning": "Warnung",
 @template_filter("pretty_category")
 def pretty_category_filter(category):
     return _category_map.get(category, "Hinweis")
+
+
+@template_filter("date")
+def date_filter(dt, format="%d.%m.%Y"):
+    if dt is None:
+        return "k/A"
+    return  dt.strftime(format)
+
+
+@template_filter("datetime")
+def datetime_filter(dt, format="%d.%m.%Y %H:%M"):
+    if dt is None:
+        return "k/A"
+    return dt.strftime(format)
+
+
+@template_filter("timesince")
+def timesince_filter(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+
+    adopted from
+    http://flask.pocoo.org/snippets/33/
+    """
+    if dt is None:
+        return "k/A"
+
+    now = datetime.now()
+    diff = now - dt
+
+    periods = (
+        (diff.days / 365, "Jahr", "Jahre"),
+        (diff.days / 30, "Monat", "Monate"),
+        (diff.days / 7, "Woche", "Wochen"),
+        (diff.days, "Tag", "Tage"),
+        (diff.seconds / 3600, "Stunde", "Stunden"),
+        (diff.seconds / 60, "Minute", "Minuten"),
+        (diff.seconds, "Sekunde", "Sekunden"),
+    )
+
+    for period, singular, plural in periods:
+
+        if period:
+            return "vor %d %s" % (period, singular if period == 1 else plural)
+
+    return default
+
 
 
 def register_filters(app):
