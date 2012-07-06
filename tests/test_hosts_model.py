2f940ffec47dbd9957b93ed2078e5abb2cc4b860
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-07-06 05:04:44 +0200

    Add a simple and basic unit test case for the mac validator
    fixes #274 Testcase für Mac-Validator

---
+++ tests/test_hosts_model.py
@@ -0,0 +1,58 @@
+import unittest
+import re
+
+from pycroft import model
+from pycroft.model import session, hosts
+
+
+class Test_010_NetDeviceValidators(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        session.reinit_session("sqlite://")
+        model.drop_db_model()
+        model.create_db_model()
+        cls.host = hosts.Host(hostname="dummy")
+        session.session.commit()
+
+    def test_0010_mac_validate(self):
+        mac_regex = re.compile(r"^[a-f0-9]{2}(:[a-f0-9]{2}){5}$")
+
+        nd = hosts.NetDevice(host=self.host)
+        def set_mac(mac):
+            nd.mac = mac
+
+        def test_mac(mac):
+            parts = mac.split(":")
+            if len(mac) != 17 or len(parts) != 6:
+                self.assertRaisesRegexp(Exception, "invalid MAC address!", set_mac, mac)
+                return
+            if mac_regex.match(mac) is None:
+                self.assertRaisesRegexp(Exception, "invalid MAC address!", set_mac, mac)
+                return
+            if int(parts[0], base=16) & 1:
+                self.assertRaisesRegexp(Exception, "Multicast-Flag ", set_mac, mac)
+                return
+            nd.mac = mac
+
+        # Try some bad macs
+        test_mac("ff:ff:ff:ff:ff")
+        test_mac("ff:ff:ff:ff:ff:ff")
+        test_mac("ff:asjfjsdaf:ff:ff:ff:ff")
+        test_mac("aj:00:ff:1f:ff:ff")
+        test_mac("ff:ff:ff:ff:ff:ff:ff")
+
+        # Assert that we have no mac assigned
+        session.session.add(nd)
+        self.assertRaisesRegexp(Exception, "\(IntegrityError\) netdevice.mac may not be NULL", session.session.commit)
+        session.session.rollback()
+
+        # Assert a correct mac
+        test_mac("00:00:00:00:00:00")
+
+        # Assert that we have the mac assigned
+        session.session.add(nd)
+        session.session.commit()
+
+        # Wipe the instance
+        session.session.delete(nd)
+        session.session.commit()
