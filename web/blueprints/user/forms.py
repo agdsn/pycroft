e09189bb4c9bce68b653728ad414168b9c813f3d
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-07-06 04:56:30 +0200

    at flask 0.9 the flaskext thing will no longer work - replace with flask.ext

--- web/blueprints/user/forms.py
+++ web/blueprints/user/forms.py
@@ -4,7 +4,7 @@
 # the Apache License, Version 2.0. See the LICENSE file for details.
 
 
-from flaskext.wtf import Form, TextField, QuerySelectField, SelectField,\
+from flask.ext.wtf import Form, TextField, QuerySelectField, SelectField,\
     PasswordField, DateTimeField, BooleanField, TextAreaField
 from wtforms.validators import Required, EqualTo, Regexp
 from pycroft.model.user import User
