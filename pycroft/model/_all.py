bb5d64ad9fb171d554c67d13a13ccbf3a52d69bd
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-07-07 11:32:25 +0200

    add a dummy module including all models for schema drawing

---
+++ pycroft/model/_all.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+pycroft.model._all
+~~~~~~~~~~~~~~
+
+Dummy module to get all the mapped stuff in one namespace.
+This is necessary for things like sqlalchemy-schemadisplay.
+
+:copyright: (c) 2011 by AG DSN.
+"""
+
+from pycroft.model.base import *
+from pycroft.model.dormitory import *
+from pycroft.model.hosts import *
+from pycroft.model.logging import *
+from pycroft.model.session import *
+#from pycroft.model.finance import *
+from pycroft.model.user import *
+from pycroft.model.properties import *
+from pycroft.model.accounting import *
+from pycroft.model.ports import *
+
