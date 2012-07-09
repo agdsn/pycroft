2ab36ee3ded0d22070a284a8bdd051275899099a
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-07-09 16:09:08 +0200

    also fix assertIsNone for 2.6

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@
 __author__ = 'jan'
 
 import unittest
+from unittest.util import safe_repr
 import re
 
 
@@ -44,7 +45,8 @@ class _AssertRaisesContext(object):
 
 class OldPythonTestCase(unittest.TestCase):
 
-    _old_py_mapping = {"assertRaisesRegexp": "_assertRaisesRegexp"}
+    _old_py_mapping = {"assertRaisesRegexp": "_assertRaisesRegexp",
+                       "assertIsNone": "_assertIsNone"}
 
     def __getattr__(self, item):
         if item in self._old_py_mapping:
@@ -71,3 +73,9 @@ class OldPythonTestCase(unittest.TestCase):
             return context
         with context:
             callable_obj(*args, **kwargs)
+
+    def _assertIsNone(self, obj, msg=None):
+        """Same as self.assertTrue(obj is None), with a nicer default message."""
+        if obj is not None:
+            standardMsg = '%s is not None' % (safe_repr(obj),)
+            self.fail(self._formatMessage(msg, standardMsg))
