71632827a5e6ce26548f83c9780e5f8749ff3c4f
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-07-09 02:22:52 +0200

    Assert that all splits will be deleted on transaction deletion + test

--- tests/test_finance_model.py
+++ tests/test_finance_model.py
@@ -36,7 +36,7 @@ class Test_010_TransactionSplits(unittest.TestCase):
         #self.s.rollback()
 
     def test_0030_insert_balanced(self):
-        tr = finance.Transaction(message="Transaction2")
+        tr = finance.Transaction(message="Transaction3")
         self.s.add(tr)
         self.s.commit()
         sp1 = finance.Split(amount=100, account=self.account, transaction=tr)
@@ -44,3 +44,18 @@ class Test_010_TransactionSplits(unittest.TestCase):
         self.s.add(sp1)
         self.s.add(sp2)
         self.s.commit()
+
+    def test_0040_delete_cascade(self):
+        tr = finance.Transaction(message="Transaction4")
+        sp1 = finance.Split(amount=234, account=self.account, transaction=tr)
+        sp2 = finance.Split(amount=-234, account=self.account, transaction=tr)
+        self.s.add(tr)
+        self.s.add(sp1)
+        self.s.add(sp2)
+        self.s.commit()
+
+        tr = finance.Transaction.q.filter_by(message="Transaction4").one()
+        self.s.delete(tr)
+        self.s.commit()
+
+        self.assertEqual(finance.Split.q.filter_by(amount=-234).count(), 0)
