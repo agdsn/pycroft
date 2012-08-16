22523efe4c35c1c638f6b46e1b929cd5cd0dd42d
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-08-16 19:44:45 +0200

    refactor the database fixture handling in for the unittests.
    The fixtures are now handled by a class called tests.FixtureDataTestBase. So its in
    one place and can be used from all testing modules. The modules have only to
    define new TestCases deriving from the new class and the fixture data.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,43 @@
 __author__ = 'jan'
 
+from fixture.style import TrimmedNameStyle
+from fixture import DataSet, SQLAlchemyFixture, DataTestCase
+from pycroft.model import session, _all
+from pycroft import model
+
 from tests.unittest26_compat import OldPythonTestCase
+
+
+def make_fixture():
+    """A helper to create a database fixture.
+    """
+    fixture = SQLAlchemyFixture(
+            env=_all,
+            style=TrimmedNameStyle(suffix="Data"),
+            engine=session.session.get_engine() )
+    return fixture
+
+
+class FixtureDataTestBase(DataTestCase, OldPythonTestCase):
+    """A TestCase baseclass that handles database fixtures.
+
+    You only need to define a `datasets` class member with a list of
+    the fixture DataSets. The type of the fixture element will be taken
+    from the name of the DataSet class. It needs "Data" as suffic. So if
+    you want to provide fixtures for the User model the name of the DataSet
+    has to be "UserData". See also test_properties.py for an example.
+
+    If you overwrite the `tearDown` or `setUpClass` methods don't forget
+    to call the ones in the superclass.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        session.reinit_session("sqlite://")
+        model.drop_db_model()
+        model.create_db_model()
+        cls.fixture = make_fixture()
+
+    def tearDown(self):
+        super(FixtureDataTestBase, self).tearDown()
+        session.session.remove()
