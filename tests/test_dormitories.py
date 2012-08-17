e428a6863d2b1bdd2aa29a807476f01d6d624c94
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-08-17 16:59:07 +0200

    add a simple testcase for the dormitory sorter

---
+++ tests/test_dormitories.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+from pycroft.helpers.dormitory_helper import sort_dormitories
+from tests import OldPythonTestCase
+
+class Test_010_SimpleHelpers(OldPythonTestCase):
+    def test_0010_dormitory_name_sorting(self):
+        before = ["41A", "41", "41B", "3", "5", "41D", "9", "11", "1", "7", "41C"]
+        after = ["1", "3", "5", "7", "9", "11", "41", "41A", "41B", "41C", "41D"]
+
+        class fake_dorm(object):
+            def __init__(self, num):
+                self.number = num
+
+        sorted = sort_dormitories([fake_dorm(num) for num in before])
+        self.assertEqual([d.number for d in sorted], after)
