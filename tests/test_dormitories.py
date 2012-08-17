9742870c769eb257264052b2dc2216ead42cd1e7
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-08-17 17:45:35 +0200

    rename a testcase class

--- tests/test_dormitories.py
+++ tests/test_dormitories.py
@@ -1,7 +1,7 @@
 from pycroft.helpers.dormitory_helper import sort_dormitories
 from tests import OldPythonTestCase
 
-class Test_010_SimpleHelpers(OldPythonTestCase):
+class Test_010_SimpleDormitoryHelpers(OldPythonTestCase):
     def test_0010_dormitory_name_sorting(self):
         before = ["41A", "41", "41B", "3", "5", "41D", "9", "11", "1", "7", "41C"]
         after = ["1", "3", "5", "7", "9", "11", "41", "41A", "41B", "41C", "41D"]
