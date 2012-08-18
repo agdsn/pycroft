3b2a12c67e61b5089169ca3cd44192bf6588bf79
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-08-18 20:30:29 +0200

    put the host fixtures in an extra module for easier reuse

--- tests/test_hosts_helper.py
+++ tests/test_hosts_helper.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-from fixture import DataSet
 from tests import OldPythonTestCase, FixtureDataTestBase
 from random import randint
 import ipaddr
@@ -7,67 +5,7 @@ import ipaddr
 from pycroft.helpers.host_helper import sort_ports, generate_hostname, get_free_ip, select_subnet_for_ip, SubnetFullException
 from pycroft.model import dormitory, hosts, session
 
-
-class DormitoryData(DataSet):
-    class dummy_house:
-        number = "01"
-        short_name = "abc"
-        street = "dummy"
-
-
-class VLanData(DataSet):
-    class vlan1:
-        name = "vlan1"
-        tag = "1"
-    class vlan2:
-        name = "vlan2"
-        tag = 2
-
-
-class SubnetData(DataSet):
-    class subnet1:
-        address = "141.30.216.0/24"
-        gateway = "141.30.216.1"
-        dns_domain = "wh12.tu-dresden.de"
-        reserved_addresses = 10
-    class subnet2:
-        address = "141.30.227.0/24"
-        gateway = "141.30.227.1"
-        dns_domain = "wh13.tu-dresden.de"
-        reserved_addresses = 10
-
-
-class RoomData(DataSet):
-    class dummy_room:
-        number = 1
-        level = 1
-        inhabitable = True
-        dormitory = DormitoryData.dummy_house
-
-
-class UserData(DataSet):
-    class dummy_user:
-        login = "test"
-        name = "John Doe"
-        registration_date = datetime.now()
-        room = RoomData.dummy_room
-
-
-class HostData(DataSet):
-    class dummy_host1:
-        hostname = "host1"
-        user = UserData.dummy_user
-        room = RoomData.dummy_room
-    class dummy_host2:
-        hostname = "host2"
-        user = UserData.dummy_user
-        room = RoomData.dummy_room
-
-
-class NetDeviceData(DataSet):
-    class dummy_device:
-        mac = "00:00:00:00:00:00"
-        host = HostData.dummy_host1
+from tests.fixtures.hosts_fixtures import DormitoryData, VLanData, SubnetData, RoomData, UserData, HostData, NetDeviceData
 
 
 class Test_010_SimpleHostsHelper(OldPythonTestCase):
@@ -99,7 +37,7 @@ class Test_010_SimpleHostsHelper(OldPythonTestCase):
                 self.assertEqual(generated, expected)
 
 
-class Test_030_IpHelper(FixtureDataTestBase):
+class Test_020_IpHelper(FixtureDataTestBase):
     datasets = [DormitoryData, VLanData, SubnetData, RoomData, UserData, HostData, NetDeviceData]
 
     def ip_s1(self, num):
