5e61cf693bb71be5f152b4f40ba3154c1172f5ae
Jan Losinski <losinski@wh2.tu-dresden.de>
2012-08-18 20:31:01 +0200

    add unittests to check the new netdevice events and methods

--- tests/test_hosts_model.py
+++ tests/test_hosts_model.py
@@ -1,9 +1,12 @@
 import unittest
 import re
 
-from tests import OldPythonTestCase
+from tests import OldPythonTestCase, FixtureDataTestBase
 from pycroft import model
-from pycroft.model import session, hosts
+from pycroft.model import session, hosts, dormitory
+
+from tests.fixtures.hosts_fixtures import DormitoryData, VLanData, SubnetData, RoomData, UserData, HostData, NetDeviceData
+from pycroft.helpers.host_helper import get_free_ip
 
 
 class Test_010_NetDeviceValidators(OldPythonTestCase):
@@ -57,3 +60,67 @@ class Test_010_NetDeviceValidators(OldPythonTestCase):
         # Wipe the instance
         session.session.delete(nd)
         session.session.commit()
+
+
+class Test_020_NetworkDeviceMethods(FixtureDataTestBase):
+    datasets = [DormitoryData, VLanData, SubnetData, RoomData, UserData, HostData, NetDeviceData]
+
+    def test_0010_set_v4address(self):
+        subnets = dormitory.Subnet.q.all()
+        netdev = hosts.NetDevice.q.first()
+
+        ip = get_free_ip((subnets[0], ))
+        def call_method(subnet):
+            netdev.set_v4address(ip, subnet)
+
+        call_method(subnets[0])
+        self.assertRaises(AssertionError, call_method, subnets[1])
+
+
+class Test_030_NetworkDeviceEvents(FixtureDataTestBase):
+    datasets = [DormitoryData, VLanData, SubnetData, RoomData, UserData, HostData, NetDeviceData]
+
+    def test_0010_correct_subnet_and_ip(self):
+        subnet = dormitory.Subnet.q.first()
+        netdev = hosts.NetDevice.q.first()
+
+        netdev.ipv4 = get_free_ip((subnet, ))
+        netdev.subnet = subnet
+        session.session.commit()
+
+        netdev = hosts.NetDevice.q.first()
+        ip = get_free_ip((subnet, ))
+        netdev.set_v4address(ip, subnet)
+        session.session.commit()
+
+
+    def test_0020_missing_subnet(self):
+        subnet = dormitory.Subnet.q.first()
+        netdev = hosts.NetDevice.q.first()
+
+        netdev.ipv4 = get_free_ip((subnet, ))
+
+        def commit():
+            session.session.commit()
+        self.assertRaisesRegexp(AssertionError, "NetDevice has an ip bot no Subnet assigned!", commit)
+
+    def test_0030_missing_ip(self):
+        subnet = dormitory.Subnet.q.first()
+        netdev = hosts.NetDevice.q.first()
+
+        netdev.subnet = subnet
+
+        def commit():
+            session.session.commit()
+        self.assertRaisesRegexp(AssertionError, "A Subnet is assigned but no ip was set", commit)
+
+    def test_0040_wrong_subnet(self):
+        subnets = dormitory.Subnet.q.all()
+        netdev = hosts.NetDevice.q.first()
+
+        netdev.ipv4 = get_free_ip((subnets[0], ))
+        netdev.subnet = subnets[1]
+
+        def commit():
+            session.session.commit()
+        self.assertRaisesRegexp(AssertionError, "Assigned Subnet does not contain the assigned ip", commit)
