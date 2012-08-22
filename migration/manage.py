e3c94d865213f0d0a131878f3cafa9dd1d78c678
Sebastian Schrader <sebastian.schrader@mailbox.tu-dresden.de>
2012-08-22 20:59:37 +0200

    Fixed database URL in migration manage scripts.

--- migration/manage.py
+++ migration/manage.py
@@ -2,4 +2,4 @@
 from migrate.versioning.shell import main
 
 if __name__ == '__main__':
-    main(url='sqlite:///test_db.sqlite', debug='False')
+    main(url='sqlite:////tmp/test.db', debug='False')
