1477b076e8da65bef41de0f3bc8b115baec49cd0
Florian Österreich <florian.oesterreich@googlemail.com>
2012-09-05 21:26:52 +0200

    renamed traffic_volume.ips in traffic_volume.ip
    in function json_traffic

--- web/blueprints/user/__init__.py
+++ web/blueprints/user/__init__.py
@@ -160,7 +160,7 @@ def json_trafficdata(user_id):
     trafficvolumes = session.query(
         TrafficVolume
     ).join(
-        TrafficVolume.ips
+        TrafficVolume.ip
     ).join(
         Ip.host
     ).filter(
