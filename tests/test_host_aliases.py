b48329cd20b6329b03565ade4127d030db90f5f5
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-09-06 14:33:54 +0200

    Fixed bugs in host alias model and added new tests
    bugs:
        - fixed typo in aaaarecord-genentry method for no-ttl entry
        - removed wrong host_id and ip_id usage in fixture
    
    test:
        - added for all record types test which check the gen_entry method

--- tests/test_host_aliases.py
+++ tests/test_host_aliases.py
@@ -1,10 +1,10 @@
 __author__ = 'l3nkz'
 
-from fixtures.host_aliases_fixture import ARecordData, AAAARecordData, \
+from fixtures.host_aliases_fixture import ARecordData, AAAARecordData,\
     MXRecordData, CNameRecordData, NSRecordData, IpData
-from tests import FixtureDataTestBase, OldPythonTestCase
-from pycroft import model
-from pycroft.model.hosts import ARecord, AAAARecord, Ip
+from tests import FixtureDataTestBase
+from pycroft.model.hosts import ARecord, AAAARecord, MXRecord, CNameRecord,\
+    NSRecord, Ip
 
 class Test_010_ARecordValidator(FixtureDataTestBase):
 
@@ -39,3 +39,65 @@ class Test_020_AAAARecordValidator(FixtureDataTestBase):
             if ip.subnet.ip_type == "4":
                 self.assertRaises(AssertionError, set_ip, ip)
                 break
+
+class Test_030_GenEntryMethods(FixtureDataTestBase):
+
+    datasets = [ARecordData, AAAARecordData, MXRecordData, CNameRecordData,\
+    NSRecordData]
+
+    def test_0010_arecord_without_ttl(self):
+        record = ARecord.q.filter(ARecord.time_to_live == None).first()
+        entry = record.gen_entry
+        entry_expected = u"%s IN A %s" % (record.content, record.ip.address)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0015_arecord_with_ttl(self):
+        record = ARecord.q.filter(ARecord.time_to_live != None).first()
+        entry = record.gen_entry
+        entry_expected = u"%s %s IN A %s" % (record.content, record.time_to_live,\
+                                             record.ip.address)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0020_aaaarecord_without_ttl(self):
+        record = AAAARecord.q.filter(AAAARecord.time_to_live == None).first()
+        entry = record.gen_entry
+        entry_expected = u"%s IN AAAA %s" % (record.content, record.ip.address)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0025_aaaarecord_with_ttl(self):
+        record = AAAARecord.q.filter(AAAARecord.time_to_live != None).first()
+        entry = record.gen_entry
+        entry_expected = u"%s %s IN AAAA %s" % (record.content,\
+                                                record.time_to_live, record.ip.address)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0030_mxrecord(self):
+        record = MXRecord.q.first()
+        entry = record.gen_entry
+        entry_expected = u"%s IN MX %s %s" % (record.domain, record.priority, record.content)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0040_cnamerecord(self):
+        record = CNameRecord.q.first()
+        entry = record.gen_entry
+        entry_expected = u"%s IN CNAME %s" % (record.content, record.alias_for)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0050_nsrecord_without_ttl(self):
+        record = NSRecord.q.filter(NSRecord.time_to_live == None).first()
+        entry = record.gen_entry
+        entry_expected = u"%s IN NS %s" % (record.domain, record.content)
+
+        self.assertEqual(entry, entry_expected)
+
+    def test_0055_nsrecord_with_ttl(self):
+        record = NSRecord.q.filter(NSRecord.time_to_live != None).first()
+        entry =  record.gen_entry
+        entry_expected = u"%s %s IN NS %s" % (record.domain, record.time_to_live, \
+                                              record.content)
