85c4e19adb79adaf1dabdf588371be0c14978582
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-09-06 14:05:50 +0200

    First tests and fixtues for the hostalias model
    - fixtures for all alias types
    - tests for the validators of the ARecord and AAAARecord classes

---
+++ tests/test_host_aliases.py
@@ -0,0 +1,41 @@
+__author__ = 'l3nkz'
+
+from fixtures.host_aliases_fixture import ARecordData, AAAARecordData, \
+    MXRecordData, CNameRecordData, NSRecordData, IpData
+from tests import FixtureDataTestBase, OldPythonTestCase
+from pycroft import model
+from pycroft.model.hosts import ARecord, AAAARecord, Ip
+
+class Test_010_ARecordValidator(FixtureDataTestBase):
+
+    datasets = [ARecordData, IpData]
+
+    def test_0010_ip_validate(self):
+        record =  ARecord.q.first()
+
+        def set_ip(ip):
+            record.ip = ip
+
+        ips = Ip.q.all()
+
+        for ip in ips:
+            if ip.subnet.ip_type == "6":
+                self.assertRaises(AssertionError, set_ip, ip)
+                break
+
+class Test_020_AAAARecordValidator(FixtureDataTestBase):
+
+    datasets = [AAAARecordData, IpData]
+
+    def test_0010_ip_validate(self):
+        record =  AAAARecord.q.first()
+
+        def set_ip(ip):
+            record.ip = ip
+
+        ips = Ip.q.all()
+
+        for ip in ips:
+            if ip.subnet.ip_type == "4":
+                self.assertRaises(AssertionError, set_ip, ip)
+                break
