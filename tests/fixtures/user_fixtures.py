112198e916ac4d827dde08860a67430975129ea2
Florian Österreich <florian.oesterreich@googlemail.com>
2012-09-26 23:25:39 +0200

    add some more rooms to the user fixtures

--- tests/fixtures/user_fixtures.py
+++ tests/fixtures/user_fixtures.py
@@ -29,6 +29,11 @@ class RoomData(DataSet):
         level = 2
         inhabitable = True
         dormitory = DormitoryData.dummy_house2
+    class dummy_room3:
+        number = 2
+        level = 2
+        inhabitable = True
+        dormitory = DormitoryData.dummy_house1
 
 
 class UserData(DataSet):
