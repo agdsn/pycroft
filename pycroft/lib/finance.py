275d7344fabb55491506dc54af8e0488654ba5e4
Florian Österreich <florian.oesterreich@googlemail.com>
2012-10-11 01:45:53 +0200

    add the creation of the semester accounts
    fixes #355

--- pycroft/lib/finance.py
+++ pycroft/lib/finance.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Florian Österreich'
 
-from pycroft.model.finance import Semester
+from pycroft.model.finance import Semester, FinanceAccount
 from pycroft.model import session
 
 def semester_create(name, registration_fee, semester_fee, begin_date, end_date):
     """
     This function creates a new Semester.
+    There are created a registration fee account and a semester fee account
+    which ones are attached to the semester
     The name could be something like: "Wintersemester 2012/13"
     :param name: A usefull name for the semester.
     :param registration_fee: The fee a student have to pay when he moves in first.
@@ -20,6 +22,13 @@ def semester_create(name, registration_fee, semester_fee, begin_date, end_date):
         semester_fee=semester_fee,
         begin_date=begin_date,
         end_date=end_date)
+
+    new_registration_fee_account = FinanceAccount("Anmeldegebühr %s" % name)
+
+    new_semester_fee_account = FinanceAccount("Semestergebühr %s" % name)
+
+    session.session.add(new_registration_fee_account)
+    session.session.add(new_semester_fee_account)
     session.session.add(new_semester)
     session.session.commit()
     return new_semester
