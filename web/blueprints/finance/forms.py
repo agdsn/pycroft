fd47f4f1eaeffeda90256f623798834bb7d92215
Florian Österreich <florian.oesterreich@googlemail.com>
2012-10-11 01:24:18 +0200

    add the frontend to create semester
    the backend file for finance is created

---
+++ web/blueprints/finance/forms.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+__author__ = 'florian'
+
+from web.form.fields import DatePickerField
+from flask.ext.wtf import Form, TextField, IntegerField
+
+class SemesterCreateForm(Form):
+    name = TextField(u"Semestername")
+    registration_fee = IntegerField(u"Anmeldegebühr")
+    semester_fee = IntegerField(u"Semesterbeitrag")
+    begin_date = DatePickerField(u"Anfang")
+    end_date = DatePickerField(u"Ende")
