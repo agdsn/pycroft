7286a786f3d3ea2999c1250a9e16d95a93262eda
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-10-16 11:00:58 +0200

    Added some more functions for host handling
    - added the possibility to create hosts (at the moment just cnamerecords)
    - added the possibility to remove records (any type but arecord and
      aaaarecord)

--- web/blueprints/infrastructure/forms.py
+++ web/blueprints/infrastructure/forms.py
@@ -1,18 +1,38 @@
 # -*- coding: utf-8 -*-
 
 
-from flask.ext.wtf import Form, TextField
+from flask.ext.wtf import Form, TextField, SelectField, QuerySelectField
 from wtforms.validators import Required, Regexp
 from pycroft.model.ports import Port
 from web.form.fields import ReadonlyTextField
+from pycroft.model.hosts import ARecord
+
+
+def arecords_query(host_id):
+    return ARecord.q.filter(
+        ARecord.host_id == host_id
+    ).order_by(ARecord.id)
+
 
 class SwitchPortForm(Form):
     name = TextField(u"Port Name",
-                        [Required(message=u"Name?"),
-                         Regexp(regex=Port.name_regex,
-                             message=u"Richtig ist z.B. A2")])
+        [Required(message=u"Name?"),
+         Regexp(regex=Port.name_regex,
+             message=u"Richtig ist z.B. A2")])
 
 
 class CNameRecordEditForm(Form):
     name = TextField(u"Alias Name", [Required(message=u"Alias?")])
     alias_for = ReadonlyTextField(u"für")
+
+
+class RecordCreateForm(Form):
+    type = SelectField(u"Type",
+        choices=[('arecord', 'ARecord'), ('aaaarecord', 'AAAARecord'),
+                 ('cnamerecord', 'CNameRecord'), ('mxrecord', 'MXRecord'),
+                 ('nsrecord', 'NSRecord'), ('srvrecord', 'SRVRecord')])
+
+
+class CNameRecordCreateForm(Form):
+    name = TextField(u"Alias Name", [Required(message=u"Alias?")])
+    alias_for = QuerySelectField(u"für", get_label='name')
