da25392171d286648a709b2c10971a58518c454e
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-12-05 19:55:34 +0100

    Removed a senseless add to the database

--- pycroft/lib/hosts.py
+++ pycroft/lib/hosts.py
@@ -9,16 +9,22 @@ from pycroft.model.logging import UserLogEntry
 from pycroft.model import session
 
 def change_mac(net_device, mac, processor):
+    """
+    This method will change the mac address of the given netdevice to the new
+    mac address.
+
+    :param net_device: the netdevice which should become a new mac address.
+    :param mac: the new mac address.
+    :param processor: the user who initiated the mac address change.
+    :return: the changed netdevice with the new mac address.
+    """
     net_device.mac = mac
 
     change_mac_log_entry = UserLogEntry(author_id=processor.id,
         message=u"Die Mac-Adresse in %s geändert." % mac,
         timestamp=datetime.datetime.now(), user_id=net_device.host.user.id)
 
-    session.session.add(net_device)
     session.session.add(change_mac_log_entry)
-
     session.session.commit()
 
     return net_device
-
