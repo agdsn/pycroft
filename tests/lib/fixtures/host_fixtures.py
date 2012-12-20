79a58e1bf775609683ac11d712d0aecdf65903b8
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-12-20 11:22:30 +0100

    Create and Remove methods for hosts model objects are now in its lib.
    This commit adds remove and create methods for every type in the host
    model. Additionally tests for each method is added too.
    
    refs #395

--- tests/lib/fixtures/host_fixtures.py
+++ tests/lib/fixtures/host_fixtures.py
@@ -15,6 +15,7 @@ class DormitoryData(DataSet):
 
 class RoomData(DataSet):
     class dummy_room1:
+        id = 1
         number = "1"
         level = 1
         inhabitable = True
@@ -23,7 +24,7 @@ class RoomData(DataSet):
 
 class UserData(DataSet):
     class dummy_user1:
-        id=1
+        id = 1
         login = "test"
         name = "John Doe"
         registration_date = datetime.now()
@@ -31,14 +32,70 @@ class UserData(DataSet):
 
 
 class UserHostData(DataSet):
-    class dummy_host1:
+    class dummy_user_host1:
         id = 1
         user = UserData.dummy_user1
         room = RoomData.dummy_room1
 
 
+class ServerHostData(DataSet):
+    class dummy_server_host1:
+        id = 2
+        user = UserData.dummy_user1
+        room = RoomData.dummy_room1
+
+
 class UserNetDeviceData(DataSet):
-    class dummy_device1:
-        id=1
+    class dummy_user_device1:
+        id = 1
         mac = "00:00:00:00:00:00"
-        host = UserHostData.dummy_host1
+        host = UserHostData.dummy_user_host1
+
+
+class SubnetData(DataSet):
+    class dummy_subnet1:
+        id = 1
+        address = "141.30.216.0/24"
+        gateway = "141.30.216.1"
+        dns_domain = "wh12.tu-dresden.de"
+        reserved_addresses = 10
+        ip_type = 4
+
+
+class SwitchData(DataSet):
+    class dummy_switch1:
+        id = 10
+        name = "dummy_switch"
+        user = UserData.dummy_user1
+        room = RoomData.dummy_room1
+        management_ip = "141.30.216.15"
+
+
+class SwitchNetDeviceData(DataSet):
+    class dummy_switch_device1:
+        id = 3
+        mac = "00:00:00:00:00:00"
+        host = SwitchData.dummy_switch1
+
+
+class SwitchPortData(DataSet):
+    class dummy_switch_port1:
+        id = 1
+        name = "name"
+        switch = SwitchData.dummy_switch1
+
+
+class ServerNetDeviceData(DataSet):
+    class dummy_server_device1:
+        id = 2
+        mac = "00:00:00:00:00:00"
+        host = ServerHostData.dummy_server_host1
+        switch_port = SwitchPortData.dummy_switch_port1
+
+
+class IpData(DataSet):
+    class dummy_ip1:
+        id = 1
+        address = "141.30.216.15"
+        net_device = SwitchNetDeviceData.dummy_switch_device1
+        subnet = SubnetData.dummy_subnet1
