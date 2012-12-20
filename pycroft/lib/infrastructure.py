eb451042f96dc059d6e36366017b6db14231dce7
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-12-20 16:08:15 +0100

    Added create and remove methods for port model to infrastructure lib.
    Added methods and tests for creation and removal of ports to the lib!
    
    refs #395

--- pycroft/lib/infrastructure.py
+++ pycroft/lib/infrastructure.py
@@ -1,16 +1,87 @@
 from pycroft.model import session
-from pycroft.model.ports import SwitchPort
+from pycroft.model.ports import SwitchPort, DestinationPort, PatchPort, \
+    PhonePort, Port
 
-def create_switch_port(*args, **kwargs):
+def _create_port(type, *args, **kwargs):
     """
-    This method will create a new switch port.
+    This method will create a new port.
 
+    :param type: the type of the port.
     :param args: the positionals which will be passed to the constructor.
     :param kwargs: the keyword arguments which will be passed to the constructor.
-    :return: the newly created switch port.
+    :return: the newly created port.
     """
-    port = SwitchPort(*args, **kwargs)
+    type = str(type).lower()
+
+    if type == "patch_port":
+        port = PatchPort(*args, **kwargs)
+    elif type == "phone_port":
+        port = PhonePort(*args, **kwargs)
+    elif type == "switch_port":
+        port = SwitchPort(*args, **kwargs)
+    else:
+        raise ValueError("Unknown port type!")
+
     session.session.add(port)
     session.session.commit()
 
     return port
+
+
+def create_patch_port(*args, **kwargs):
+    """
+    This method will create a new PatchPort.
+
+    :param args: the positionals which will be passed to the constructor.
+    :param kwargs: the keyword arguments which will be passed to the constructor.
+    :return: the newly created PatchPort.
+    """
+    return _create_port("patch_port", *args, **kwargs)
+
+
+def create_phone_port(*args, **kwargs):
+    """
+    This method will create a new PhonePort.
+
+    :param args: the positionals which will be passed to the constructor.
+    :param kwargs: the keyword arguments which will be passed to the constructor.
+    :return: the newly created PhonePort.
+    """
+    return _create_port("phone_port", *args, **kwargs)
+
+
+def create_switch_port(*args, **kwargs):
+    """
+    This method will create a new SwitchPort.
+
+    :param args: the positionals which will be passed to the constructor.
+    :param kwargs: the keyword arguments which will be passed to the constructor.
+    :return: the newly created SwitchPort.
+    """
+    return _create_port("switch_port", *args, **kwargs)
+
+
+def delete_port(port_id):
+    """
+    This method will remove the Port for the given id.
+
+    :param port_id: the id of the Port which should be removed.
+    :return: the removed Port.
+    """
+    port = Port.q.get(port_id)
+    if port is None:
+        raise ValueError("The given id wrong!")
+
+    if port.discriminator == "patch_port":
+        del_port = PatchPort.q.get(port_id)
+    elif port.discriminator == "phone_port":
+        del_port = PhonePort.q.get(port_id)
+    elif port.discriminator == "switch_port":
+        del_port = SwitchPort.q.get(port_id)
+    else:
+        raise ValueError("Unknown port type!")
+
+    session.session.delete(del_port)
+    session.session.commit()
+
+    return del_port
