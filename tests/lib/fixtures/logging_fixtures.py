7ea1452d2fdee01c717bc70657ace4331fadcf20
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2012-12-20 12:06:14 +0100

    Added remove and create methods for logging model to its lib.
    Added methods for creating and removing UserLogEntries and LogEntries at
    all. Furthermore this commit adds tests and fixtures which test these
    methods.
    
    refs #395

---
+++ tests/lib/fixtures/logging_fixtures.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+from tests import DataSet
+from datetime import datetime
+
+
+class DormitoryData(DataSet):
+    class dummy_house1:
+        number = "01"
+        short_name = "abc"
+        street = "dummy"
+
+
+class RoomData(DataSet):
+    class dummy_room1:
+        id = 1
+        number = "1"
+        level = 1
+        inhabitable = True
+        dormitory = DormitoryData.dummy_house1
+
+
+class UserData(DataSet):
+    class dummy_user1:
+        id = 1
+        login = "test"
+        name = "John Doe"
+        registration_date = datetime.now()
+        room = RoomData.dummy_room1
+
+
+class UserLogEntryData(DataSet):
+    class dummy_log_entry1:
+        id = 1
+        message = "dummy_user_log_entry"
+        timestamp = datetime.now()
+        author = UserData.dummy_user1
+        user = UserData.dummy_user1
