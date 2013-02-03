e1f150f56d31a113cdedf290f866f4d3561672b4
Florian Österreich <florian.oesterreich@googlemail.com>
2013-02-03 22:23:51 +0100

    Add forgotten dict entry for assert translation
    An assert has to be translatet to a function for old
    test cases so it has to be an entry in _old_py_mapping
    to translate assertNotIn in _assertNotIn.

--- tests/unittest26_compat.py
+++ tests/unittest26_compat.py
@@ -69,6 +69,7 @@ class OldPythonTestCase(unittest.TestCase):
                        "assertListEqual": "_assertListEqual",
                        "assertSequenceEqual": "_assertSequenceEqual",
                        "assertIn": "_assertIn",
+                       "assertNotIn": "_assertNot   In",
                        "assertGreaterEqual": "_assertGreaterEqual"}
 
     def __getattr__(self, item):
