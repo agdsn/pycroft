15a1be692310fcef98c02786517b58253688601c
Dominik Pataky <pataky@wh2.tu-dresden.de>
2013-04-04 13:47:22 +0200

    Change user_create finance entries, fix user_show finance log
    refs #200

--- pycroft/lib/user_config.py
+++ pycroft/lib/user_config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2013 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 __author__ = 'Florian Österreich'
@@ -8,7 +8,5 @@ from datetime import timedelta
 
 #if you enter 0 for end_date it is an infinite group membership
 initial_groups = [
-    dict(group_name="one_month_negative_balance",
-         dates=dict(start_date=timedelta(0), end_date=timedelta(days=31))),
-    dict(group_name="standard_traffic",
-         dates=dict(start_date=timedelta(0), end_date=0))]
+    dict(group_name="NegativKonto",
+         dates=dict(start_date=timedelta(0), end_date=timedelta(days=31)))]
