308cb1fe35d2425d0c88cfe8a55151bd1e3fff04
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2013-06-26 17:28:50 +0200

    Refactor lib -- logging --
    Add commit flag to all simple create and delete methods. Change usage of
    args and kwargs to real parameters.

--- pycroft/lib/logging.py
+++ pycroft/lib/logging.py
@@ -4,12 +4,15 @@
 from pycroft.model.logging import UserLogEntry, LogEntry, RoomLogEntry
 from pycroft.model import session
 
-def _create_log_entry(type, *args, **kwargs):
+
+def _create_log_entry(type, commit=True, *args, **kwargs):
     """
     This method will create a new LogEntry of the given type with the given
     arguments.
 
     :param type: the type of the LogEntry which should be created.
+    :param commit: flag which indicates whether the session should be commited
+                   or not. Default: True
     :param args: the positionals which will be passed to the constructor.
     :param kwargs: the keyword arguments which will be passed to the constructor.
     :return: the newly created LogEntry.
@@ -24,18 +27,22 @@ def _create_log_entry(type, *args, **kwargs):
         raise ValueError("Unknown LogEntry type!")
 
     session.session.add(entry)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return entry
 
-def delete_log_entry(log_entry_id):
+
+def delete_log_entry(log_entry_id, commit=True):
     """
     This method will remove the LogEntry for the given id.
 
     :param log_entry_id: the id of the LogEntry which should be removed.
+    :param commit: flag which indicates whether the session should be commited
+                   or not. Default: True
     :return: the removed LogEntry.
     """
-    entry= LogEntry.q.get(log_entry_id)
+    entry = LogEntry.q.get(log_entry_id)
     if entry is None:
         raise ValueError("The given id is wrong!")
 
@@ -47,28 +54,41 @@ def delete_log_entry(log_entry_id):
         raise ValueError("Unknown LogEntry type!")
 
     session.session.delete(del_entry)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return del_entry
 
 
-def create_user_log_entry(*args, **kwargs):
+def create_user_log_entry(message, timestamp, author_id, user_id, commit=True):
     """
     This method will create a new UserLogEntry.
 
-    :param args: the positionals which will be passed to the constructor.
-    :param kwargs: the keyword arguments which will be passed to the constructor.
+    :param message: the message of the log
+    :param timestamp: the timestamp of the log
+    :param author_id: the id of the user which created the log
+    :param user_id: the id of the user for which the log should be created
+    :param commit: flag which indicates whether the session should be commited
+                   or not. Default: True
     :return: the newly created UserLogEntry.
     """
-    return _create_log_entry("userlogentry", *args, **kwargs)
+    return _create_log_entry("userlogentry", message=message,
+                             timestamp=timestamp, author_id=author_id,
+                             user_id=user_id, commit=commit)
 
 
-def create_room_log_entry(*args, **kwargs):
+def create_room_log_entry(message, timestamp, author_id, room_id, commit=True):
     """
     This method will create a new RoomLogEntry.
 
-    :param args: the positionals which will be passed to the constructor.
-    :param kwargs: the keyword arguments which will be passed to the constructor.
+    :param message: the message of the log
+    :param timestamp: the timestamp of the log
+    :param author_id: the id of the user which created the log
+    :param room_id: the id of the room for which the log should be created
+    :param commit: flag which indicates whether the session should be commited
+                   or not. Default: True
     :return: the newly created RoomLogEntry.
     """
-    return _create_log_entry("roomlogentry", *args, **kwargs)
+    return _create_log_entry("roomlogentry", message=message,
+                             timestamp=timestamp, author_id=author_id,
+                             room_id=room_id, commit=commit)
