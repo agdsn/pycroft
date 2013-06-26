841acede09d02fb90650bcfe2663161c94709369
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2013-06-26 12:15:14 +0200

    Refactor lib -- dormitory
    Add commit flag to all simple create, delete methods. Changed usage of
    args, kwargs to the real parameters.

--- pycroft/lib/dormitory.py
+++ pycroft/lib/dormitory.py
@@ -1,29 +1,36 @@
-# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2013 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 from pycroft.model.dormitory import Dormitory, Room, Subnet, VLan
 from pycroft.model import session
 
-def create_dormitory(*args, **kwargs):
+
+def create_dormitory(number, short_name, street, commit=True):
     """
     This method will create a new dormitory.
 
-    :param args: the arguments which will be passed to the constructor.
-    :param kwargs: the keyword arguments which will be passed to the constructor
+    :param short_name: the name which is used as abbreviation.
+    :param street: the street where the dormitory is located.
+    :param number: the number of the dormitory
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :return: the newly created dormitory
     """
-    dormitory = Dormitory(*args, **kwargs)
+    dormitory = Dormitory(number=number, short_name=short_name, street=street)
     session.session.add(dormitory)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return dormitory
 
 
-def delete_dormitory(dormitory_id):
+def delete_dormitory(dormitory_id, commit=True):
     """
     This method will remove the dormitory fot the given id.
 
     :param dormitory_id: the id of the dormitory which should be removed
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :return: the deleted dormitory
     """
     dormitory = Dormitory.q.get(dormitory_id)
@@ -31,31 +38,41 @@ def delete_dormitory(dormitory_id):
         raise ValueError("The given id is wrong!")
 
     session.session.delete(dormitory)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return dormitory
 
 
-def create_room(*args, **kwargs):
+def create_room(number, level, inhabitable, dormitory_id, commit=True):
     """
     This method creates a new room.
 
-    :param args: arguments which will be passed to the constructor
-    :param kwargs: the keyword arguments which will be passed to the constructor
+
+    :param number: the number of the room.
+    :param level: the level within the dormitory where the room is located.
+    :param inhabitable: whether or not someone can live in the room.
+    :param dormitory_id: the id of the dormitory in which the room is located.
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :return: the newly created room
     """
-    room = Room(*args, **kwargs)
+    room = Room(number=number, level=level, inhabitable=inhabitable,
+                dormitory_id=dormitory_id)
     session.session.add(room)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return room
 
 
-def delete_room(room_id):
+def delete_room(room_id, commit=True):
     """
     This method will remove the room for the given id.
 
     :param room_id: the id of the room which should be deleted
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :return: the deleted room
     """
     room = Room.q.get(room_id)
@@ -63,30 +80,42 @@ def delete_room(room_id):
         raise ValueError("The given id is wrong!")
 
     session.session.delete(room)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return room
 
 
-def create_subnet(*args, **kwargs):
+def create_subnet(address, gateway, dns_domain, reserved_addresses, ip_type,
+                  commit=True):
     """
     This method will create a new subnet.
 
-    :param args: the positionals which will be passed to the constructor.
-    :param kwargs: the keyword arguments which will be passed to the constructor.
+
+    :param address: the subnet address
+    :param gateway: the standard gateway
+    :param dns_domain: the dns domain
+    :param reserved_addresses: the number of reserved addresses
+    :param ip_type: the ip version which should be used
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :return: the newly created subnet.
     """
-    subnet = Subnet(*args, **kwargs)
+    subnet = Subnet(address=address, gateway=gateway, dns_domain=dns_domain,
+                    reserved_addresses=reserved_addresses, ip_type=ip_type)
     session.session.add(subnet)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return subnet
 
 
-def delete_subnet(subnet_id):
+def delete_subnet(subnet_id, commit=True):
     """
     This method will remove the subnet for the given id.
 
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :param subnet_id: the id of the subnet which should be removed.
     :return: the removed subnet.
     """
@@ -95,30 +124,37 @@ def delete_subnet(subnet_id):
         raise ValueError("The given id is wrong!")
 
     session.session.delete(subnet)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return subnet
 
 
-def create_vlan(*args, **kwargs):
+def create_vlan(name, tag, commit=True):
     """
     This method will create a new vlan.
 
-    :param args: the positionals which will be passed to the constructor,
-    :param kwargs: the keyword arguments which will be passed to the constructor.
+
+    :param name: the name of the vlan
+    :param tag: the tag which should be used for this vlan
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :return: the newly created vlan.
     """
-    vlan = VLan(*args, **kwargs)
+    vlan = VLan(name=name, tag=tag)
     session.session.add(vlan)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return vlan
 
 
-def delete_vlan(vlan_id):
+def delete_vlan(vlan_id, commit=True):
     """
     This method will remove the vlan for the given id.
 
+    :param commit: flag which indicates whether the session should be
+                   commited or not. Default: True
     :param vlan_id: the id of the vlan which should be removed.
     :return: the removed vlan.
     """
@@ -127,6 +163,7 @@ def delete_vlan(vlan_id):
         raise ValueError("The given id is wrong!")
 
     session.session.delete(vlan)
-    session.session.commit()
+    if commit:
+        session.session.commit()
 
     return vlan
