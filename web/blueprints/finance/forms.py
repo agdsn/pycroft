6617c724fa55f2e7054f2eac14f2ad728a5bc34d
Dominik Pataky <pataky@wh2.tu-dresden.de>
2013-08-09 00:42:00 +0200

    Add journalentry list and edit form for every entry.
    refs #450

--- web/blueprints/finance/forms.py
+++ web/blueprints/finance/forms.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2013 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 __author__ = 'florian'
 
 from web.form.fields import DatePickerField
-from flask.ext.wtf import Form, TextField, IntegerField
+from flask.ext.wtf import Form, TextField, IntegerField, QuerySelectField
+from pycroft.model.finance import FinanceAccount
+
+
+def financeaccounts_query():
+    return FinanceAccount.q.order_by(FinanceAccount.name)
+
 
 class SemesterCreateForm(Form):
     name = TextField(u"Semestername")
@@ -13,3 +19,10 @@ class SemesterCreateForm(Form):
     semester_fee = IntegerField(u"Semesterbeitrag")
     begin_date = DatePickerField(u"Anfang")
     end_date = DatePickerField(u"Ende")
+
+
+class JournalLinkForm(Form):
+    linked_accounts = QuerySelectField(u"Zugehoeriges Konto",
+                          get_label='name',
+                          query_factory=financeaccounts_query,
+                          allow_blank=True)
