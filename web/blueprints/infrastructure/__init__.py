2db90652d2cbbc771b430ad4490a05055c59cf3e
Felix Wollert <fxwt@wh2.tu-dresden.de>
2013-10-17 22:09:52 +0200

    Conflicts resolved in: 	web/blueprints/infrastructure/__init__.py

--- web/blueprints/infrastructure/__init__.py
+++ web/blueprints/infrastructure/__init__.py
@@ -14,6 +14,7 @@
 from flask import Blueprint, flash, redirect, render_template, url_for
 from pycroft.helpers import host
 from pycroft.model.host import Switch, Host
+from pycroft.model.dormitory import Subnet
 from pycroft.model.host_alias import HostAlias, CNameRecord
 from web.blueprints.navigation import BlueprintNavigation
 from web.blueprints.infrastructure.forms import SwitchPortForm
@@ -32,7 +33,9 @@ nav = BlueprintNavigation(bp, "Infrastruktur")
 @bp.route('/subnets')
 @nav.navigate(u"Subnetze")
 def subnets():
-    return render_template('infrastructure/base.html')
+    subnets_list = Subnet.q.all()
+    return render_template('infrastructure/subnets_list.html',
+        subnets = subnets_list)
 
 
 @bp.route('/switches')
@@ -185,7 +188,7 @@ def switch_port_create(switch_id):
     form = SwitchPortForm()
     switch = Switch.q.get(switch_id)
     if form.validate_on_submit():
-        create_switch_port(name=form.name.data, switch=switch)
+        create_switch_port(name=form.name.data, switch_id=switch_id)
         flash(u'Neuer Switch Port angelegt', 'success')
         return redirect(url_for('.switch_show', switch_id=switch_id))
     return render_template('infrastructure/switch_port_create.html',
