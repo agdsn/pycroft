1352ab57c94508d84ee96d0270ee98ab9399fe47
felix <felix_kluge@wh2.tu-dresden.de>
2013-10-28 21:15:51 +0100

    Fix for wrong test: create_semester_accounts
    refs #448

--- tests/lib/test_finance.py
+++ tests/lib/test_finance.py
@@ -2,13 +2,13 @@ from tests import OldPythonTestCase
 
 __author__ = 'felix_kluge'
 
-from pycroft.lib.finance import create_semester
-from pycroft.lib.config import get,config
-from pycroft.model.finance import FinanceAccount
+from pycroft.lib.finance import create_semester, import_csv
+from pycroft.lib.config import get, config
+from pycroft.model.finance import FinanceAccount, Journal, JournalEntry
 from sqlalchemy.orm import backref
 from pycroft.model import session
 import time
-from datetime import date
+from datetime import date, datetime
 
 
 class Test_010_Semester(OldPythonTestCase):
@@ -18,12 +18,15 @@ class Test_010_Semester(OldPythonTestCase):
         This test should verify that all semester-related finance-accounts have
         been created.
         """
-        new_semester = create_semester("NewSemesterName", 2500, 1500, date(2013, 9, 1), date(2014, 2, 1))
+        new_semester = create_semester("NewSemesterName",
+                                       2500, 1500,
+                                       date(2013, 9, 1),
+                                       date(2014, 2, 1))
         config._configpath = "../tests/example/test_config.json"
         for account in config["finance"]["semester_accounts"]:
-            for new_account in new_semester.accounts:
-                if(new_account.tag == account["tag"]):
-                    new_account_equivalent = new_account
-            compare_account = FinanceAccount(type=account["type"],name=account["name"],semester=new_semester,tag=account["tag"])
-            self.assertEqual(new_account_equivalent.name, compare_account.name)
-            self.assertEqual(new_account_equivalent.type, compare_account.type)
+            new_created_account = FinanceAccount.q.filter(
+                FinanceAccount.semester == new_semester,
+                FinanceAccount.tag == account["tag"]).first()
+            self.assertEqual(new_created_account.name, account["name"])
+            self.assertEqual(new_created_account.type, account["type"])
+        session.session.commit()
