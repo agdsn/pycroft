d4d326d193deaf952f910703f54e094c75a2307c
Tim Kluge <timklge@wh2.tu-dresden.de>
2014-04-01 19:32:41 +0200

    Use jQuery focus method to focus first input element

--- web/static/custom/js/form.js
+++ web/static/custom/js/form.js
@@ -69,8 +69,7 @@
  */
 
 $(function(){
-    var inputele = $(".control-group:first .controls:first :first").get(0);
-    if(inputele) inputele.focus();
+    $(".control-group:first .controls:first :first").focus();
 });
 
 
