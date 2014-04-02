6660abc3dd96126ea7bd63528e96e020557b5a2f
Dominik Pataky <pataky@wh2.tu-dresden.de>
2014-04-02 19:24:39 +0200

    Fixed UNIQUE user login name
    fixes #494

--- pycroft/model/user.py
+++ pycroft/model/user.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2014 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 """
@@ -25,7 +25,7 @@ from pycroft.helpers.user import hash_password, verify_password
 
 
 class User(ModelBase, UserMixin):
-    login = Column(String(40), nullable=False)
+    login = Column(String(40), nullable=False, unique=True)
     name = Column(String(255), nullable=False)
     registration_date = Column(DateTime, nullable=False)
     passwd_hash = Column(String)
