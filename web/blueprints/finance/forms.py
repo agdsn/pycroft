35b4dd369b5695994cb6c54eb175fafb095a7cb0
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2014-04-30 23:02:04 +0200

    Rename form to FinanceAccountCreateForm.

--- web/blueprints/finance/forms.py
+++ web/blueprints/finance/forms.py
@@ -58,7 +58,7 @@ class JournalImportForm(Form):
     csv_file = FileField()
 
 
-class FinanceaccountCreateForm(Form):
+class FinanceAccountCreateForm(Form):
     name = TextField(u"Name")
     type = SelectField(u"Typ", choices=[("LIABILITY","Passivkonto"), ("EXPENSE", "Aufwandskonto"),
                                         ("ASSET", "Aktivkonto"), ("INCOME", "Ertragskonto"), ("EQUITY", "Equity")])
