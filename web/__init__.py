c6142b268f8cfb2182ad9c92b09fe776e3ba2b24
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2014-05-11 11:24:13 +0200

    Rename template_check to template_test
    To be consistent with the common Jinja2 terminology.

--- web/__init__.py
+++ web/__init__.py
@@ -42,7 +42,7 @@ def make_app(connection_string="sqlite:////tmp/test.db"):
     app.register_blueprint(login.bp)
 
     template_filters.register_filters(app)
-    template_tests.register_checks(app)
+    template_tests.register_tests(app)
 
     user.nav.register_on(app)
     finance.nav.register_on(app)
