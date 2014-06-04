896d2b4768e8538d901ad059f2a8cb2d0c963a7d
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-06-05 01:29:32 +0200

    fix forgotten rename in test_property.py
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- tests/lib/test_property.py
+++ tests/lib/test_property.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2014 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 from tests import FixtureDataTestBase
@@ -167,7 +167,8 @@ class Test_050_MalformedGroup(FixtureDataTestBase):
     def test_0010_create_malformed_group(self):
         name = "malformed_group1"
 
-        self.assertRaises(ValueError, _create_group, type='malformed_group',
+        self.assertRaises(ValueError, _create_group,
+            group_type='malformed_group',
             name=name, id=100)
 
     def test_0020_delete_malformed_group(self):
