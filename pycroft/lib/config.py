c6a8266fd7ef7696f3b0897d89f805ab827f964d
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2014-06-06 02:36:24 +0200

    Locate config via pkgutil
    In order to make the project independent from the current working
    directory.

--- pycroft/lib/config.py
+++ pycroft/lib/config.py
@@ -3,27 +3,31 @@
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 
-import json, collections
+import json, collections, pkgutil
 from os.path import isfile
 
 
 class Config(object):
     def __init__(self):
-        self._configdata = None
-        self._configpath = "pycroft/config.json"
+        self._config_data = None
+        self._package = "pycroft"
+        self._resource = "config.json"
 
     def load(self):
-        if not isfile(self._configpath):
+        data = pkgutil.get_data(self._package, self._resource)
+        if data is None:
             raise Exception(
-                "The configfile does not exist at '" + self._configpath + "'!")
-        self._configdata = json.load(open(self._configpath))
-        if not isinstance(self._configdata, collections.Mapping):
+                "Could not load config file {1} "
+                "from package {0}".format(self._package, self._resource)
+            )
+        self._config_data = json.loads(data)
+        if not isinstance(self._config_data, collections.Mapping):
             raise Exception("Config must be a JSON object!")
 
     def __getitem__(self, key):
-        if self._configdata is None:
+        if self._config_data is None:
             self.load()
-        return self._configdata[key]
+        return self._config_data[key]
 
     def __setitem__(self, key, value):
         raise Exception("It is not possible to set configuration entries!")
