5412e86a857a18e6aa9fa51f9a3c6c202a3e5d49
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2014-06-10 08:37:24 +0200

    Create global UnboundedInterval constant

--- pycroft/helpers/interval.py
+++ pycroft/helpers/interval.py
@@ -214,3 +214,5 @@ class Interval(collections.namedtuple('BaseInterval', ['begin', 'end'])):
             self._max(self.begin, other.begin),
             self._min(self.end, other.end)
         )
+
+UnboundedInterval = Interval(None, None)
