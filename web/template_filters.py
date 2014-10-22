0c1ce234a0c34a4b961eb3901474c4ee2e987354
Johannes Falke <mueslo@wh2.tu-dresden.de>
2014-10-23 01:16:20 +0200

    Fix spelling bug (which had no implications)

--- web/template_filters.py
+++ web/template_filters.py
@@ -30,7 +30,7 @@ def template_filter(name):
 _category_map = {"warning": "Warnung",
                  "error": "Fehler",
                  "info": "Hinweis",
-                 "mesage": "Hinweis",
+                 "message": "Hinweis",
                  "success": "Erfolgreich"}
 
 
