agdsn/pycroft

commit 0290d9ad6b8c42224e38b4456bc5cc56ccf3453e

Add status information to user view
This regards basic status information such as internet access, violation etc.
Add dummy function user_has_paid() in lib/finance.py, leave for future implementation.

pycroft/lib/user.py
@@ -27,7 +27,7 @@ from pycroft.lib.host import create_user_net_device, create_user_host, create_ip
 from pycroft.lib.property import create_membership
 from pycroft.lib.logging import create_user_log_entry
 from pycroft.lib.config import config
-from pycroft.lib.finance import get_current_semester
+from pycroft.lib.finance import get_current_semester, user_has_paid
 
 def encode_type1_user_id(user_id):
     """Append a type-1 error detection code to the user_id."""
@@ -519,3 +519,26 @@ def is_back(user, processor):
     )
 
     return user
+
+
+def determine_status(user):
+    """Returns The Status
+    :param User user: User object
+    :return: Status dict
+    :rtype: dict[str, bool]
+    """
+    ret = {}
+    if user.has_property("internet"):
+        ret['internet'] = True
+    else:
+        ret['internet'] = False
+        # a: traffic        -> property: "traffic_exceeded"
+        if has_exceeded_traffic(user):
+            ret += u'Traffic'
+    # b: payment        -> property: "no_payment"
+    ret['paid'] = user_has_paid(user)
+    # c: Infringement   -> property: "violation"
+    ret['no_violation'] = not user.has_property("violation")
+    # has mail:
+    ret['mail'] = user.has_property("mail")
+    return ret

pycroft/lib/finance.py
@@ -339,3 +339,7 @@ def process_record(index, record, import_time):
         transaction_date=transaction_date,
         valid_date=valid_date
     )
+
+def user_has_paid(user):
+    # TODO check if user has paid
+    return True

web/blueprints/user/__init__.py
@@ -100,7 +100,8 @@ def user_show(user_id):
         room=room,
         form=form,
         memberships=memberships.all(),
-        memberships_active=memberships_active.all())
+        memberships_active=memberships_active.all(),
+        status=lib.user.determine_status(user))
 
 
 @bp.route('/add_membership/<int:user_id>/', methods=['GET', 'Post'])

web/templates/user/_user_show_basedata.html
@@ -3,6 +3,14 @@
     <dd>{{ caller() }}</dd>
 {%- endmacro %}
 
+{% macro show_status(key) %}
+    {% if status[key] %}
+        <strong class="text-success">Ja</strong>
+    {% else %}
+        <strong class="text-danger">Nein</strong>
+    {% endif %}
+{% endmacro %}
+
 <div class="row">
     <div class="col-sm-6 col-md-5 col-lg-4">
         <dl class="dl-horizontal">
@@ -38,9 +46,20 @@
                 {% endif %}
             {% endcall %}
 
-            {% call info_row("Netzstatus") %}
-                {# TODO implementieren #}
-                #todo#
+            {% call info_row("Internetzugang") %}
+                {{ show_status("internet") }}
+            {% endcall %}
+
+            {% call info_row("Mailkonto") %}
+                {{ show_status("mail") }}
+            {% endcall %}
+
+            {% call info_row("Bezahlt") %}
+                {{ show_status("paid") }}
+            {% endcall %}
+
+            {% call info_row("Verstoßfrei") %}
+                {{ show_status("no_violation") }}
             {% endcall %}
         </dl>
     </div>

commit d488f9b4698b39a03deaa5e63fd190335e11f59d

Remove bug introduced in 0290d9a

pycroft/lib/user.py
@@ -527,18 +527,10 @@ def determine_status(user):
     :return: Status dict
     :rtype: dict[str, bool]
     """
-    ret = {}
-    if user.has_property("internet"):
-        ret['internet'] = True
-    else:
-        ret['internet'] = False
-        # a: traffic        -> property: "traffic_exceeded"
-        if has_exceeded_traffic(user):
-            ret += u'Traffic'
-    # b: payment        -> property: "no_payment"
-    ret['paid'] = user_has_paid(user)
-    # c: Infringement   -> property: "violation"
-    ret['no_violation'] = not user.has_property("violation")
-    # has mail:
-    ret['mail'] = user.has_property("mail")
+    ret = {
+        'internet': user.has_property("internet"),
+        'traffic_exceeded': has_exceeded_traffic(user),
+        'paid': user_has_paid(user),
+        'no_violation': not user.has_property("violation"),
+        'mail': user.has_property("mail")}
     return ret