agdsn/pycroft

commit eefe7561ac63f5f039a85198d1cda80baecb7a52

Various changes to accomodate automated Vagrant VM creation:  - Added database connection string to conf.json  - Added new .sql dumps and a script to recreate postgres db from these dumps (pg_recreate_test_db)  - Renamed DummySession to DummySessionWrapper in accordance with SessionWrapper  - Deleted Pycroft.git/pycroft/lib/config.py and moved contents to Pycroft.git/pycroft/__init__.py, made it look for config.json first and then config.json.default, so you can have custom conf.jsons without git pull overwriting them  - Added Pycroft.git/vagrant, from where you can automatically create VMs running Pycroft  - Some minor things to harmonize database contents with our database schema
 Note: If you want to develop with SQLite, copy config.json.default to config.json and modify connection_string. Nothing else should change for you.
 Note: The postgres database currently has some primary key issues, so it is non-modifiable at this time.

 refs #553 @3h

pycroft/__init__.py
@@ -7,3 +7,34 @@
 
     :copyright: (c) 2011 by AG DSN.
 """
+
+import json, collections, pkgutil
+
+class Config(object):
+    def __init__(self):
+        self._config_data = None
+        self._package = "pycroft"
+        self._resource = "config.json"
+
+    def load(self):
+        data = (pkgutil.get_data(self._package, self._resource) or
+                pkgutil.get_data(self._package+".default", self._resource))
+        if data is None:
+            raise Exception(
+                "Could not load config file {1} "
+                "from package {0}".format(self._package, self._resource)
+            )
+        self._config_data = json.loads(data)
+        if not isinstance(self._config_data, collections.Mapping):
+            raise Exception("Config must be a JSON object!")
+
+    def __getitem__(self, key):
+        if self._config_data is None:
+            self.load()
+        return self._config_data[key]
+
+    def __setitem__(self, key, value):
+        raise Exception("It is not possible to set configuration entries!")
+
+
+config = Config()

pycroft/lib/__init__.py
@@ -8,4 +8,4 @@
     :copyright: (c) 2012 by AG DSN.
 """
 
-import user, host, logging, config, property
+import user, host, logging, property

web/__init__.py
@@ -21,12 +21,12 @@ from flask.ext.login import current_user, current_app
 from web.templates import page_resources
 
 
-def make_app(connection_string="sqlite:////tmp/test.db"):
+def make_app():
     """  Create and configure the main? Flask app object
 
     :return: The fully configured app object
     """
-    session.init_session(connection_string=connection_string)
+    session.init_session()
 
     app = Flask(__name__)
 
@@ -69,7 +69,7 @@ def make_app(connection_string="sqlite:////tmp/test.db"):
         :param e: The error from the errorhandler
         """
         if e.code in (401, 403):
-            flash(u"Nicht genügend Rechte, um die Seite zu sehen!", "error")
+            flash(u"Nicht genügend Rechte um die Seite zu sehen!", "error")
         elif e.code in (404,):
             flash(u"Seite wurde nicht gefunden!", "error")
         elif e.code in (500,):

.gitignore
@@ -8,3 +8,5 @@
 .idea/inspectionProfiles/Project_Default.xml
 .idea/inspectionProfiles/profiles_settings.xml
 doc/_build
+vagrant/.vagrant
+pycroft/config.json

pg_recreate_test_db
@@ -1,0 +1,40 @@
+#!/usr/bin/env python2
+from sys import exit
+from subprocess import call
+from shlex import split
+from os import devnull
+nulldevice = open(devnull,"w")
+
+print ("WARNING: This will delete the database pycroft.db and"
+       "recreate it from the schema")
+print "All data will be lost! Are you sure you wish to continue? [y/N]"
+if raw_input().lower() != "y":
+    print "Aborted. No data lost."
+    exit()
+
+
+dbname = "pycroft.db"
+schemadumpfname = "example/pg_schema.sql"
+datadumpfname = "example/pg_data.sql"
+
+print "Dropping DB", dbname
+call(["dropdb", dbname])
+
+print "Creating DB", dbname
+call(["createdb", dbname])
+
+print "Loading schema"
+from pycroft.model import _all
+from pycroft import model
+
+print "Writing schema to DB"
+model.create_db_model()
+
+print "Save SQL dump of schema? [y/N]"
+if raw_input().lower() == "y":
+    print "Writing SQL dump to",schemadumpfname
+    with open(schemadumpfname,"w") as f:
+        call(["pg_dump", dbname], stdout=f, stderr=f)
+
+print "Filling DB with "+datadumpfname
+call(split("psql pycroft.db -f "+datadumpfname), stdout=nulldevice)

pycroft/config.json.default
@@ -1,5 +1,5 @@
 {
-    "db_connection_string" : "sqlite:////tmp/test.db",
+    "db_connection_string" : "postgresql+psycopg2:///pycroft.db?host=/var/run/postgresql",
     "file_upload": {
         "max_file_size": "67108864",
         "temp_dir": "/tmp"

commit 5bdf6e47859f725a48fb26642a5b72525bdfd384

Actually remove config.json, fix config.json.default bug introduced in commit:eefe7561

pycroft/__init__.py
@@ -18,7 +18,7 @@ class Config(object):
 
     def load(self):
         data = (pkgutil.get_data(self._package, self._resource) or
-                pkgutil.get_data(self._package+".default", self._resource))
+                pkgutil.get_data(self._package, self._resource+".default"))
         if data is None:
             raise Exception(
                 "Could not load config file {1} "