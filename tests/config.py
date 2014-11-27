7ce8996a4e9dbd3da165cfdf4d357d0031e58c9f
Johannes Falke <mueslo@wh2.tu-dresden.de>
2014-11-27 16:34:20 +0100

    Only run tests with postgres if current user is 'vagrant'

--- tests/config.py
+++ tests/config.py
@@ -3,11 +3,26 @@
 # the Apache License, Version 2.0. See the LICENSE file for details.
 __author__ = 'jfalke'
 
+
+import getpass
+
+
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
+
+def get_database_uri():
+    user = getpass.getuser()
+    postgres_test_uri = ("postgresql+psycopg2:///tests.db"
+                         "?host=/var/run/postgresql")
+    sqlite_test_uri = "sqlite:///:memory:"
+    if user not in ['vagrant']:
+        return sqlite_test_uri
+    return postgres_test_uri
+
+
 test_config = AttrDict({
-    "database_uri": "sqlite:///:memory:"
+    "database_uri": get_database_uri()
 })
