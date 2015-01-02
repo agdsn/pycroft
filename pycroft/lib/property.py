2af60b83770431256fceb4733e22f05ea86ab7d3
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2015-01-02 20:14:35 +0100

    Remove property lib functionality
    Refs #565

--- pycroft/lib/property.py
+++ pycroft/lib/property.py
@@ -1,100 +1,9 @@
-# Copyright (c) 2014 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 from pycroft.model import session
 from pycroft.model.session import with_transaction
-from pycroft.model.property import TrafficGroup, PropertyGroup, Property,\
-    Membership, Group
-
-
-def _create_group(group_type, *args, **kwargs):
-    """
-    This method will create a new Group.
-
-    :param group_type: the type of the group. Equals the discriminator.
-    :param args: the positionals which will be passed to the constructor.
-    :param kwargs: the keyword arguments which will be passed to the constructor.
-    :return: the newly created group.
-    """
-    group_type = str(group_type).lower()
-
-    if group_type == "property_group":
-        group = PropertyGroup(*args, **kwargs)
-    elif group_type == "traffic_group":
-        group = TrafficGroup(*args, **kwargs)
-    else:
-        raise ValueError("Unknown group type!")
-
-    session.session.add(group)
-    return group
-
-
-@with_transaction
-def _delete_group(group_id):
-    """
-    This method will remove the Group for the given id.
-
-    :param group_id: the id of the Group which should be removed.
-    :return: the removed Group.
-    """
-    group = Group.q.get(group_id)
-    if group is None:
-        raise ValueError("The given id is wrong!")
-
-    if group.discriminator == "property_group":
-        del_group = PropertyGroup.q.get(group_id)
-    elif group.discriminator == "traffic_group":
-        del_group = TrafficGroup.q.get(group_id)
-    else:
-        raise ValueError("Unknown group type")
-
-    session.session.delete(del_group)
-    return del_group
-
-
-@with_transaction
-def create_traffic_group(name, traffic_limit):
-    """
-    This method will create a new traffic group.
-
-    :param name: the name of the group
-    :param traffic_limit: the traffic limit of the group
-    :return: the newly created traffic group
-    """
-    return _create_group("traffic_group", name=name, traffic_limit=traffic_limit)
-
-
-@with_transaction
-def delete_traffic_group(traffic_group_id):
-    """
-    This method will remove the traffic group for the given id.
-
-    :param traffic_group_id: the if of the group which should be deleted
-    :return: the deleted traffic group
-    """
-    return _delete_group(traffic_group_id)
-
-
-@with_transaction
-def create_property_group(name):
-    """
-    This method will create a new property group.
-
-    :param name: the name of the group
-    :return: the newly created property group
-    """
-    return _create_group("property_group", name=name)
-
-
-@with_transaction
-def delete_property_group(property_group_id):
-    """
-    This method will remove the property group for the given id.
-
-    :param property_group_id: the id of the group which should be removed.
-    :return: the deleted property group
-    """
-    return _delete_group(property_group_id)
+from pycroft.model.property import PropertyGroup, Property, Membership
 
 
 @with_transaction
@@ -154,19 +63,3 @@ def create_membership(start_date, end_date, user, group):
                             user=user, group=group)
     session.session.add(membership)
     return membership
-
-
-@with_transaction
-def delete_membership(membership_id):
-    """
-    This method will remove the Membership for the given id.
-
-    :param membership_id: the id of the Membership which should be removed.
-    :return: the removed membership.
-    """
-    del_membership = Membership.q.get(membership_id)
-    if del_membership is None:
-        raise ValueError("The given id is wrong!")
-
-    session.session.delete(del_membership)
-    return del_membership
--- tests/lib/test_property.py
+++ tests/lib/test_property.py
@@ -1,94 +1,16 @@
-# Copyright (c) 2014 The Pycroft Authors. See the AUTHORS file.
+# Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
-from tests import FixtureDataTestBase
-from tests.lib.fixtures.property_fixtures import UserData, PropertyGroupData,\
-    PropertyData, MembershipData, TrafficGroupData
+from datetime import timedelta
 
 from pycroft.lib.property import (
-    create_membership, delete_membership,
-    create_property_group, delete_property_group,
-    create_traffic_group, delete_traffic_group,
-    grant_property, deny_property, remove_property,
-    _create_group, _delete_group
-)
-
-from pycroft.model.property import TrafficGroup, PropertyGroup, Property,\
-    Membership, Group
+    create_membership, grant_property, deny_property, remove_property)
+from pycroft.model.property import PropertyGroup, Property, Membership
 from pycroft.model.user import User
 from pycroft.model import session
-
-from datetime import datetime, timedelta
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy.types import Integer
-
-class Test_010_PropertyGroup(FixtureDataTestBase):
-    datasets = [PropertyGroupData]
-
-    def test_0010_create_property_group(self):
-        name = "dummy_property_group2"
-
-        property_group = create_property_group(name=name)
-
-        self.assertIsNotNone(PropertyGroup.q.get(property_group.id))
-
-        db_property_group = PropertyGroup.q.get(property_group.id)
-
-        self.assertEqual(db_property_group.name, name)
-
-        session.session.delete(db_property_group)
-        session.session.commit()
-
-    def get_property_group(self):
-        return PropertyGroup.q.filter_by(
-            name=PropertyGroupData.dummy_property_group1.name).one()
-
-    def test_0020_delete_property_group(self):
-        test_property_group = self.get_property_group()
-        del_property_group = delete_property_group(test_property_group.id)
-
-        self.assertIsNone(PropertyGroup.q.get(del_property_group.id))
-
-    def test_0025_delete_wrong_property_group(self):
-        test_property_group = self.get_property_group()
-        self.assertRaises(ValueError, delete_property_group,
-                          test_property_group.id + 100)
-
-
-class Test_020_TrafficGroup(FixtureDataTestBase):
-    datasets = [TrafficGroupData]
-
-    def test_0010_create_traffic_group(self):
-        name = "dummy_traffic_group2"
-        traffic_limit = 100000
-
-        traffic_group = create_traffic_group(name=name,
-            traffic_limit=traffic_limit)
-
-        self.assertIsNotNone(TrafficGroup.q.get(traffic_group.id))
-
-        db_traffic_group = TrafficGroup.q.get(traffic_group.id)
-
-        self.assertEqual(db_traffic_group.name, name)
-        self.assertEqual(db_traffic_group.traffic_limit, traffic_limit)
-
-        session.session.delete(db_traffic_group)
-        session.session.commit()
-
-    def get_traffic_group(self):
-        return TrafficGroup.q.filter_by(
-            name=TrafficGroupData.dummy_traffic_group1.name).one()
-
-    def test_0020_delete_traffic_group(self):
-        test_traffic_group = self.get_traffic_group()
-        del_traffic_group = delete_traffic_group(test_traffic_group.id)
-
-        self.assertIsNone(TrafficGroup.q.get(del_traffic_group.id))
-
-    def test_0025_delete_wrong_traffic_group(self):
-        test_traffic_group = self.get_traffic_group()
-        self.assertRaises(ValueError, delete_traffic_group,
-                          test_traffic_group.id + 100)
+from tests import FixtureDataTestBase
+from tests.lib.fixtures.property_fixtures import (
+    UserData, PropertyGroupData, PropertyData, MembershipData)
 
 
 class Test_030_Membership(FixtureDataTestBase):
@@ -107,26 +29,13 @@ class Test_030_Membership(FixtureDataTestBase):
 
         self.assertIsNotNone(Membership.q.get(membership.id))
 
-        db_membership = Membership.q.get(membership.id)
+        self.assertEqual(membership.start_date, start_date)
+        self.assertEqual(membership.end_date, end_date)
+        self.assertEqual(membership.group, group)
+        self.assertEqual(membership.user, user)
 
-        self.assertEqual(db_membership.start_date, start_date)
-        self.assertEqual(db_membership.end_date, end_date)
-        self.assertEqual(db_membership.group, group)
-        self.assertEqual(db_membership.user, user)
-
-        session.session.delete(db_membership)
+        session.session.delete(membership)
         session.session.commit()
-
-    def test_0020_delete_membership(self):
-        test_membership = Membership.q.first()
-        del_membership = delete_membership(test_membership.id)
-
-        self.assertIsNone(Membership.q.get(del_membership.id))
-
-    def test_0025_delete_wrong_membership(self):
-        test_membership = Membership.q.first()
-        self.assertRaises(ValueError, delete_membership,
-            test_membership.id + 100)
 
 
 class Test_040_Property(FixtureDataTestBase):
@@ -183,31 +92,3 @@ class Test_040_Property(FixtureDataTestBase):
         self.assertRaises(ValueError, remove_property,
                           empty_group,
                           self.property_name)
-
-
-class Test_050_MalformedGroup(FixtureDataTestBase):
-    datasets = [UserData]
-
-    class MalformedGroup(Group):
-        id = Column(Integer, ForeignKey("group.id"), primary_key=True)
-        __mapper_args__ = {'polymorphic_identity': 'malformed_group'}
-
-    def test_0010_create_malformed_group(self):
-        name = "malformed_group1"
-
-        self.assertRaises(ValueError, _create_group,
-            group_type='malformed_group',
-            name=name, id=100)
-
-    def test_0020_delete_malformed_group(self):
-        malformed_group = Test_050_MalformedGroup.MalformedGroup(id=1000,
-            name="malformed_group2")
-
-        session.session.add(malformed_group)
-        session.session.commit()
-
-        self.assertRaises(ValueError, _delete_group, malformed_group.id)
-
-        session.session.delete(malformed_group)
-        session.session.commit()
-
