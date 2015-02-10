b2395ae41b5316d64c1c1beef1c41ad7147606be
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2015-02-10 17:52:32 +0100

    Tweak PostgreSQL development instance settings
    * use more general regexps
    * increase shared_buffers to 128MB
    * set checkpoints_segments to 1
    * set wal_keep_segments to 0

--- vagrant/usr/local/bin/postgresql.sh
+++ vagrant/usr/local/bin/postgresql.sh
@@ -7,26 +7,30 @@ export PGDATA=/postgresql
 export PGHOST=$PGDATA
 export PGDATABASE=pycroft
 PGVERSION=9.1
+PGPATH=/usr/lib/postgresql/$PGVERSION
 PYCROFT_EXAMPLE_DATA=/pycroft/example/pg_example_data.sql
 
 if [ ! -f $PGDATA/postgresql.conf ]; then
     echo Initialising postgres data directory $PGDATA
     sudo -n mkdir -p $PGDATA
     sudo -n mount -t tmpfs -o size=192M,nr_inodes=8k,mode=0700,uid=vagrant,gid=vagrant,noexec,nodev,nosuid tmpfs $PGDATA
-    /usr/lib/postgresql/$PGVERSION/bin/initdb -A peer
-    sed -i -e 's/#temp_buffers = 8MB/temp_buffers = 64MB/' $PGDATA/postgresql.conf
-    sed -i -e 's/#fsync = on/fsync = off/' $PGDATA/postgresql.conf
-    sed -i -e 's/#synchronous_commit = on/synchronous_commit = off/' $PGDATA/postgresql.conf
-    sed -i -e 's/#full_page_writes = on/full_page_writes = off/' $PGDATA/postgresql.conf
-    sed -i -e 's/#bgwriter_lru_maxpages = 100/bgwriter_lru_maxpages = 0   /' $PGDATA/postgresql.conf
-    sed -i -e "s|#unix_socket_directory = ''|unix_socket_directory = '$PGDATA'|" $PGDATA/postgresql.conf
+    $PGPATH/bin/initdb -A peer
+    sed -i -e "s|#\?unix_socket_directory = ''|unix_socket_directory = '$PGDATA'|" $PGDATA/postgresql.conf
+    sed -i -e 's|#\?temp_buffers = [0-9]\+MB|temp_buffers = 64MB|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?shared_buffers = [0-9]\+MB|shared_buffers = 128MB|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?fsync = on|fsync = off|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?synchronous_commit = on|synchronous_commit = off|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?full_page_writes = on|full_page_writes = off|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?bgwriter_lru_maxpages = [0-9]\+|bgwriter_lru_maxpages = 0|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?checkpoint_segments = [0-9]\+|checkpoint_segments = 1|' $PGDATA/postgresql.conf
+    sed -i -e 's|#\?wal_keep_segments = [0-9]\+|wal_keep_segments = 0|' $PGDATA/postgresql.conf
     echo Setting up databases
-    /usr/lib/postgresql/$PGVERSION/bin/pg_ctl start -w
+    $PGPATH/bin/pg_ctl start -w
     createdb pycroft
     createdb pycroft_tests
     [ -f $PYCROFT_EXAMPLE_DATA ] && psql < $PYCROFT_EXAMPLE_DATA
-    /usr/lib/postgresql/$PGVERSION/bin/pg_ctl stop -w
+    $PGPATH/bin/pg_ctl stop -w
 fi
 
-exec /usr/lib/postgresql/$PGVERSION/bin/postgres
+exec $PGPATH/bin/postgres
 
