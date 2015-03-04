dc90db4c45cd83e4c8e478aa81f0cd1fd497b3c6
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2015-03-04 01:21:41 +0100

    Add l10n and i18n facilities

---
+++ pycroft/helpers/i18n.py
@@ -0,0 +1,403 @@
+# coding=utf-8
+# Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+from datetime import date, datetime, time, timedelta
+from functools import partial
+from itertools import imap
+import json
+import gettext as gt
+import operator
+from babel import Locale, dates, numbers
+from babel.support import Translations
+import jsonschema
+from pycroft.helpers.interval import Interval, Bound, NegativeInfinity, \
+    PositiveInfinity
+
+_unspecified_locale = Locale('en', 'US')
+_null_translations = Translations()
+_locale_lookup = lambda: _unspecified_locale
+_translations_lookup = lambda: _null_translations
+
+
+def get_locale():
+    return _locale_lookup()
+
+
+def get_translations():
+    return _translations_lookup()
+
+
+def set_locale_lookup(lookup_func):
+    global _locale_lookup
+    _locale_lookup = lookup_func
+
+
+def set_translation_lookup(lookup_func):
+    global _translations_lookup
+    _translations_lookup = lookup_func
+
+
+def gettext(message):
+    return get_translations().ugettext(message)
+
+
+def dgettext(domain, message):
+    return get_translations().udgettext(domain, message)
+
+
+def ngettext(singular, plural, n):
+    return get_translations().ungettext(singular, plural, n)
+
+
+def dngettext(domain, singular, plural, n):
+    return get_translations().udngettext(domain, singular, plural, n)
+
+
+def format_number(n, **options):
+    return numbers.format_number(n, locale=get_locale())
+
+
+def format_date(d, **options):
+    options = options.get(date, {})
+    return dates.format_date(d, locale=get_locale(), **options)
+
+
+def format_datetime(d, **options):
+    options = options.get(datetime, {})
+    return dates.format_datetime(d, locale=get_locale(), **options)
+
+
+def format_time(t, **options):
+    options = options.get(time, {})
+    return dates.format_time(t, locale=get_locale(), **options)
+
+
+def format_timedelta(delta, **options):
+    options = options.get(timedelta, {})
+    return dates.format_timedelta(delta, locale=get_locale(), **options)
+
+
+def format_bool(v, **options):
+    return gettext(u"True") if v else gettext(u"False")
+
+
+def format_none(n, **options):
+    return gettext(u"None")
+
+
+def format_interval(interval, **options):
+    lower_bound = interval.lower_bound
+    upper_bound = interval.upper_bound
+    return u"{0}{1}, {2}{3}".format(
+        u'[' if lower_bound.closed else u'(',
+        format_param(lower_bound.value, options)
+        if not lower_bound.unbounded
+        else u'-∞',
+        format_param(upper_bound.value, options)
+        if not upper_bound.unbounded
+        else u'∞',
+        u']' if upper_bound.closed else u')',
+    )
+
+
+identity = lambda x: x
+
+formatter_map = {
+    type(None): format_none,
+    str: identity,
+    unicode: identity,
+    bool: format_bool,
+    float: format_number,
+    int: format_number,
+    date: format_date,
+    datetime: format_datetime,
+    time: format_time,
+    timedelta: format_timedelta,
+    Interval: format_interval,
+}
+
+
+def format_param(p, options):
+    concrete_type = type(p)
+    formatters = (formatter_map[type_] for type_ in concrete_type.__mro__
+                  if type_ in formatter_map)
+    try:
+        formatter = next(formatters)
+    except StopIteration:
+        raise TypeError("No formatter available for type {} or any supertype."
+                        .format(concrete_type.__name__))
+    return formatter(p, **options)
+
+
+def serialize_interval(interval):
+    """
+    :param Interval interval:
+    :return:
+    """
+    lower = interval.lower_bound
+    upper = interval.upper_bound
+    lower_value = serialize_param(lower.value) if not lower.unbounded else None
+    upper_value = serialize_param(upper.value) if not upper.unbounded else None
+    return {
+        'lower_closed': lower.closed,
+        'lower_value': lower_value,
+        'upper_closed': upper.closed,
+        'upper_value': upper_value,
+    }
+
+
+def deserialize_interval(value):
+    try:
+        lower_value = (deserialize_param(value['lower_value'])
+                       if value['lower_value'] is not None
+                       else NegativeInfinity)
+        lower_bound = Bound(lower_value, value['lower_closed'])
+        upper_value = (deserialize_param(value['upper_value'])
+                       if value['upper_value'] is not None
+                       else PositiveInfinity)
+        upper_bound = Bound(upper_value, value['upper_closed'])
+    except KeyError:
+        raise ValueError()
+    return Interval(lower_bound, upper_bound)
+
+
+def qualified_typename(type_):
+    return type.__module__ + type_.__name__
+
+
+serialize_map = {
+    type(None): identity,
+    str: identity,
+    unicode: identity,
+    bool: identity,
+    float: identity,
+    int: identity,
+    date: operator.methodcaller("isoformat"),
+    datetime: operator.methodcaller("isoformat"),
+    time: operator.methodcaller("isoformat"),
+    timedelta: lambda v: {"days": v.days, "seconds": v.seconds,
+                          "microseconds": v.microseconds},
+    Interval: serialize_interval,
+}
+
+
+deserialize_map = {qualified_typename(t): f for t, f in {
+    type(None): identity,
+    str: identity,
+    unicode: identity,
+    bool: identity,
+    float: identity,
+    int: identity,
+    date: lambda v: datetime.strptime(v, "%Y-%m-%d").date(),
+    datetime: lambda v: datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"),
+    time: lambda v: datetime.strptime(v, "%H:%M:%S.%f").time(),
+    timedelta: lambda v: timedelta(**v),
+    Interval: deserialize_interval,
+}.iteritems()}
+
+
+def serialize_param(param):
+    concrete_type = type(param)
+    serializers = ((type_, serialize_map[type_])
+                   for type_ in concrete_type.__mro__
+                   if type_ in serialize_map)
+    try:
+        type_, serializer = next(serializers)
+    except StopIteration:
+        raise TypeError("No serialization available for type {} or any"
+                        "supertype".format(concrete_type.__name__))
+    type_name = qualified_typename(type_)
+    return {"type": type_name, "value": serializer(param)}
+
+
+def deserialize_param(param):
+    type_name = param["type"]
+    try:
+        deserializer = deserialize_map[type_name]
+    except KeyError:
+        raise TypeError("No deserialization available for type {}"
+                        .format(type_name))
+    return deserializer(param["value"])
+
+
+schema = {
+    "id": "http://agdsn.de/localized-schema#",
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "description": "Message format for deferred localization.",
+    "oneOf": [
+        {"$ref": "#/definitions/singular"},
+        {"$ref": "#/definitions/plural"},
+    ],
+    "definitions": {
+        "singular": {
+            "type": "object",
+            "properties": {
+                "domain": {"type": "string"},
+                "message": {"type": "string"},
+                "args": {"$ref": "#/definitions/args"},
+                "kwargs": {"$ref": "#/definitions/kwargs"},
+            },
+            "required": ["message"],
+            "additionalProperties": False,
+        },
+        "plural": {
+            "type": "object",
+            "properties": {
+                "domain": {"type": "string"},
+                "singular": {"type": "string"},
+                "n": {"type": "integer"},
+                "plural": {"type": "string"},
+                "args": {"$ref": "#/definitions/args"},
+                "kwargs": {"$ref": "#/definitions/kwargs"},
+            },
+            "required": ["singular", "plural", "n"],
+            "additionalProperties": False,
+        },
+        "args": {
+            "type": "array",
+            "items": {"$ref": "#/definitions/param"}
+        },
+        "kwargs": {
+            "type": "object",
+            "patternProperties": {
+                "^[a-zA-Z_][a-zA-Z0-9_]+": {"$ref": "#/definitions/param"}
+            },
+            "additionalProperties": False,
+        },
+        "param": {
+            "type": "object",
+            "properties": {
+                "type": {"type": "string"},
+                "value": {},
+            },
+            "required": ["type", "value"],
+            "additionalProperties": False,
+        },
+    },
+}
+
+
+class Message(object):
+    __slots__ = ("domain", "args", "kwargs")
+
+    @classmethod
+    def from_json(cls, json_string):
+        try:
+            obj = json.loads(json_string, encoding='utf8')
+        except ValueError:
+            return ErroneousMessage(json_string)
+        try:
+            jsonschema.validate(obj, schema)
+        except jsonschema.ValidationError:
+            return ErroneousMessage("Invalid message: {}".format(json_string))
+        if u'plural' in obj:
+            m = NumericalMessage(obj[u"singular"], obj[u"plural"], obj[u"n"],
+                                 obj.get(u"domain"))
+        else:
+            m = SimpleMessage(obj[u"message"], obj.get(u"domain"))
+        if u'args' in obj:
+            m.args = tuple(deserialize_param(a) for a in obj[u"args"])
+        if u'kwargs' in obj:
+            m.kwargs = {k: deserialize_param(v)
+                        for k, v in obj[u"kwargs"].iteritems()}
+        return m
+
+    def __init__(self, domain=None):
+        self.domain = domain
+        self.args = ()
+        self.kwargs = {}
+
+    def _base_dict(self):
+        raise NotImplementedError()
+
+    def _message(self):
+        raise NotImplementedError()
+
+    def to_json(self):
+        obj = self._base_dict()
+        if self.domain is not None:
+            obj["domain"] = self.domain
+        if self.args:
+            obj["args"] = tuple(serialize_param(a) for a in self.args)
+        if self.kwargs:
+            obj["kwargs"] = {k: serialize_param(v)
+                             for k, v in self.kwargs.iteritems()}
+        return unicode(
+            json.dumps(obj, ensure_ascii=False, encoding='utf-8'))
+
+    def format(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        return self
+
+    def localize(self, **options):
+        msg = self._message()
+        f = partial(format_param, options=options)
+        args = imap(f, self.args)
+        kwargs = {k: f(v) for k, v in self.kwargs.iteritems()}
+        return msg.format(*args, **kwargs)
+
+
+class ErroneousMessage(Message):
+    def __init__(self, text):
+        super(ErroneousMessage, self).__init__(None)
+        self.text = text
+
+    def _base_dict(self):
+        raise AssertionError()
+
+    def _message(self):
+        return self.text
+
+
+class SimpleMessage(Message):
+    __slots__ = ("message",)
+
+    def __init__(self, message, domain=None):
+        super(SimpleMessage, self).__init__(domain)
+        self.message = message
+
+    def _base_dict(self):
+        return {"message": self.message}
+
+    def _message(self):
+        if self.domain:
+            return gt.dgettext(self.domain, self.message)
+        else:
+            return gt.gettext(self.message)
+
+
+class NumericalMessage(Message):
+    __slots__ = ("singular", "plural", "n")
+
+    def __init__(self, singular, plural, n, domain=None):
+        super(NumericalMessage, self).__init__(domain)
+        self.singular = singular
+        self.plural = plural
+        self.n = n
+
+    def _base_dict(self):
+        return {"singular": self.singular, "plural": self.plural, "n": self.n}
+
+    def _message(self):
+        if self.domain:
+            return dngettext(self.domain, self.singular, self.plural, self.n)
+        else:
+            return ngettext(self.singular, self.plural, self.n)
+
+
+def deferred_gettext(message):
+    return SimpleMessage(message)
+
+
+def deferred_dgettext(domain, message):
+    return SimpleMessage(message, domain)
+
+
+def deferred_ngettext(singular, plural, n):
+    return NumericalMessage(singular, plural, n)
+
+
+def deferred_dngettext(domain, singular, plural, n):
+    return NumericalMessage(singular, plural, n, domain)
---
+++ tests/helpers/test_i18n.py
@@ -0,0 +1,172 @@
+# Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+import datetime
+import json
+import unittest
+from unittest import TestCase
+import jsonschema
+from pycroft.helpers.i18n import (
+    ErroneousMessage, Message, NumericalMessage, SimpleMessage,
+    deserialize_param, serialize_param, schema, deferred_dgettext,
+    deferred_dngettext, deferred_gettext, deferred_ngettext, format_datetime)
+from pycroft.helpers.interval import (
+    UnboundedInterval, closed, closedopen, openclosed, open)
+
+
+class TestParameterSerialization(unittest.TestCase):
+    def assertValidSerialization(self, param):
+        s = serialize_param(param)
+        try:
+            json.dumps(s)
+        except (ValueError, TypeError):
+            self.fail("Param {} cannot be serialized to JSON.".format(param))
+        self.assertEqual(deserialize_param(s), param)
+
+    def test_serialize_string(self):
+        self.assertValidSerialization("test")
+
+    def test_serialize_unicode(self):
+        self.assertValidSerialization(u"test")
+
+    def test_serialize_bool(self):
+        self.assertValidSerialization(True)
+
+    def test_serialize_int(self):
+        self.assertValidSerialization(42)
+
+    def test_serialize_float(self):
+        self.assertValidSerialization(0.5)
+
+    def test_serialize_datetime(self):
+        self.assertValidSerialization(datetime.datetime.utcnow())
+
+    def test_serialize_date(self):
+        self.assertValidSerialization(datetime.date.today())
+
+    def test_serialize_timedelta(self):
+        self.assertValidSerialization(datetime.timedelta(1))
+
+    def test_serialize_time(self):
+        self.assertValidSerialization(datetime.datetime.utcnow().time())
+
+    def test_serialize_interval(self):
+        self.assertValidSerialization(UnboundedInterval)
+        now = datetime.datetime.utcnow()
+        then = now + datetime.timedelta(1)
+        self.assertValidSerialization(closed(now, then))
+        self.assertValidSerialization(closedopen(now, then))
+        self.assertValidSerialization(openclosed(now, then))
+        self.assertValidSerialization(open(now, then))
+
+    def test_serialize_unknown_type(self):
+        self.assertRaises(TypeError, serialize_param, object())
+
+
+class DeferredMessageTestCase(TestCase):
+    validator = jsonschema.Draft4Validator(schema)
+
+    def assertValidJSON(self, json_string):
+        try:
+            obj = json.loads(json_string)
+        except (ValueError, TypeError):
+            self.fail()
+        try:
+            self.validator.validate(obj)
+        except jsonschema.ValidationError as e:
+            self.fail("Export failed schema validation: {}".format(e))
+
+    def assertMessageEquals(self, m, domain, args, kwargs, expected_result):
+        self.assertEqual(m.domain, domain)
+        self.assertEqual(m.args, args)
+        self.assertEqual(m.kwargs, kwargs)
+        self.assertEqual(m.localize(), expected_result)
+
+    def assertSimpleMessageCorrect(self, m, message, domain, args, kwargs,
+                                   expected_result):
+        self.assertIsInstance(m, SimpleMessage)
+        self.assertEqual(m.message, message)
+        self.assertMessageEquals(m, domain, args, kwargs, expected_result)
+        json_string = m.to_json()
+        self.assertValidJSON(json_string)
+        m2 = Message.from_json(json_string)
+        self.assertIsInstance(m2, SimpleMessage)
+        self.assertMessageEquals(m2, domain, args, kwargs, expected_result)
+
+    def assertNumericMessageCorrect(self, m, singular, plural, n, domain,
+                                    args, kwargs, expected_result):
+        self.assertIsInstance(m, NumericalMessage)
+        self.assertEqual(m.singular, singular)
+        self.assertEqual(m.plural, plural)
+        self.assertEqual(m.n, n)
+        self.assertMessageEquals(m, domain, args, kwargs, expected_result)
+        json_string = m.to_json()
+        self.assertValidJSON(json_string)
+        m2 = Message.from_json(json_string)
+        self.assertIsInstance(m2, NumericalMessage)
+        self.assertMessageEquals(m2, domain, args, kwargs, expected_result)
+
+
+class TestJSONExport(DeferredMessageTestCase):
+    def test_invalid_json(self):
+        self.assertIsInstance(Message.from_json("not JSON"), ErroneousMessage)
+
+    def test_wrong_json(self):
+        json_string = json.dumps({"key": "value"})
+        self.assertIsInstance(Message.from_json(json_string), ErroneousMessage)
+
+    def test_simple(self):
+        message = "test"
+        m = deferred_gettext(message)
+        self.assertSimpleMessageCorrect(m, message, None, (), {}, message)
+
+    def test_simple_with_domain(self):
+        message = "test"
+        domain = "domain"
+        m = deferred_dgettext(domain, message)
+        self.assertSimpleMessageCorrect(m, message, domain, (), {}, message)
+
+    def test_simple_format_args(self):
+        message = "test {} at {}"
+        arg1 = "arg1"
+        arg2 = datetime.datetime.utcnow()
+        m = deferred_gettext(message).format(arg1, arg2)
+        expected_result = message.format(arg1, format_datetime(arg2))
+        self.assertSimpleMessageCorrect(m, message, None, (arg1, arg2), {},
+                                        expected_result)
+
+    def test_simple_format_kwargs(self):
+        message = "test {arg1} at {arg2}"
+        arg1 = "arg1"
+        arg2 = datetime.datetime.utcnow()
+        m = deferred_gettext(message).format(arg1=arg1, arg2=arg2)
+        expected_result = message.format(arg1=arg1, arg2=format_datetime(arg2))
+        self.assertSimpleMessageCorrect(m, message, None, (),
+                                        {"arg1": arg1, "arg2": arg2},
+                                        expected_result)
+
+    def test_singular(self):
+        singular = "singular"
+        plural = "plural"
+        n = 1
+        m = deferred_ngettext(singular, plural, n)
+        self.assertNumericMessageCorrect(m, singular, plural, n, None, (), {},
+                                         singular)
+
+    def test_singular_domain(self):
+        singular = "singular"
+        plural = "plural"
+        n = 1
+        domain = "domain"
+        m = deferred_dngettext(domain, singular, plural, n)
+        self.assertNumericMessageCorrect(m, singular, plural, n, domain, (), {},
+                                         singular)
+
+    def test_plural(self):
+        singular = "singular"
+        plural = "plural"
+        n = 1000
+        domain = "domain"
+        m = deferred_dngettext(domain, singular, plural, n)
+        self.assertNumericMessageCorrect(m, singular, plural, n, domain, (), {},
+                                         plural)
