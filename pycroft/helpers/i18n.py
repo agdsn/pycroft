8d41e11b8b1711a5bda09da620f740eb06f70a1c
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2015-04-09 18:52:27 +0200

    Reformat interval imports in i18n helper

--- pycroft/helpers/i18n.py
+++ pycroft/helpers/i18n.py
@@ -13,8 +13,8 @@ from babel.support import Translations
 from decimal import Decimal
 import collections
 import jsonschema
-from pycroft.helpers.interval import Interval, Bound, NegativeInfinity, \
-    PositiveInfinity
+from pycroft.helpers.interval import (
+    Interval, Bound, NegativeInfinity, PositiveInfinity)
 
 _unspecified_locale = Locale('en', 'US')
 _null_translations = Translations()
