3e7d8e08b6847f647d52261f219fae2887dfa8b7
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2015-05-27 14:07:37 +0200

    Add better with_metaclass compatibility tool

--- pycroft/_compat.py
+++ pycroft/_compat.py
@@ -49,5 +49,13 @@ else:
     NativeStringIO = StringIO
 
 
-def with_metaclass(meta, base=object):
-    return meta("NewBase", (base,), {})
+def with_metaclass(meta, *bases):
+    # Copied from Flask._compat
+    class metaclass(meta):
+        __call__ = type.__call__
+        __init__ = type.__init__
+        def __new__(cls, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(cls, name, (), d)
+            return meta(name, bases, d)
+    return metaclass('temporary_class', None, {})
