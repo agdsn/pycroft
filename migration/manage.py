ebb00b5f66e509b5476a5f2a05ab52e80489d0d7
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2015-11-12 14:21:09 +0100

    Remove unused migration folder
    We'll probably use SQLAlchemy-Migration or Alembic in the future, but right
    now nothing in this folder is used. It is boilerplate code, which probably
    doesn't even work.

--- migration/manage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# Copyright (c) 2012 The Pycroft Authors. See the AUTHORS file.
-# This file is part of the Pycroft project and licensed under the terms of
-# the Apache License, Version 2.0. See the LICENSE file for details.
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(url='sqlite:////tmp/test.db', debug='False')
