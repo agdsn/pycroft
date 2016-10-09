1d8bd1210e2f5a446dcbaf32c387e7186eb68f1a
Sebastian Geisler <sgeisler@agdsn.de>
2016-10-09 17:19:16 +0200

    implement function to render user-data pdfs

---
+++ pycroft/lib/pdfgen.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+from flask import Response, render_template
+import tex
+
+def generate_user_data_pdf(filename, uid, **user_data):
+    resp = Response(
+        tex.latex2pdf(
+            render_template("tex/user_data.tex", user_data=user_data)
+        )
+    )
+
+    resp.headers['Content-type'] = "application/octet-stream"
+    resp.headers['Content-Disposition'] = "inline; filename=\"user-{}.pdf\"".format(uid)
+
+    return resp
