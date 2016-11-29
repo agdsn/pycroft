6d1386f157136fc04ffdad7c4fbd2586484e5ad3
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2016-11-29 23:21:58 +0100

    Include ldap group memberships ['Aktiv', 'Exaktiv'] in cache

--- legacy/cache_ldap.py
+++ legacy/cache_ldap.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8; -*-
 from __future__ import print_function
 
+from collections import namedtuple
+
 import ldap3
 from sqlalchemy import create_engine
 
@@ -8,7 +10,10 @@ from conn import conn_opts
 from ldap_model import Nutzer
 
 
-def get_ldap_results():
+_combined_response = namedtuple('CombinedLdapResponse',
+                                ['user_response', 'group_response'])
+
+def fetch_ldap_information():
     try:
         opts = conn_opts['ldap']
     except KeyError:
@@ -24,19 +29,58 @@ def get_ldap_results():
     connection.search(search_base=opts['base_dn'], search_scope=ldap3.SUBTREE,
                       search_filter="(objectClass=inetOrgPerson)",
                       attributes=ldap3.ALL_ATTRIBUTES)
+    user_response = connection.response
+    connection.search(search_base=opts['group_base_dn'], search_scope=ldap3.LEVEL,
+                      search_filter="(objectClass=*)",
+                      attributes=ldap3.ALL_ATTRIBUTES)
+    group_response = connection.response
 
-    return connection.response
+    return _combined_response(user_response, group_response)
 
 
 def create_ldap_tables(engine):
     Nutzer.metadata.create_all(bind=engine)
 
 
+def first_ldap_field(dn):
+    return dn.split(',')[0].split('=')[1]
+
+
+IGNORE_GROUPS = ['Alle']
+
+def parse_groups(group_response):
+    """Parse group result into a group_name→list of uids relation.
+
+    :param group_response: The group part of the response from the
+        ldap search
+
+    :returns: a mapping {group_name: list of member uids}
+    :rtype: dict
+    """
+    group_mappings = {}
+    for group in group_response:
+        attrs = group['attributes']
+        group_cn = attrs['cn'][0]
+        if group_cn in IGNORE_GROUPS:
+            continue
+
+        members = group_mappings[group_cn] = []
+        for member in attrs['member']:
+            member_uid = first_ldap_field(member)
+            members.append(member_uid)
+
+    return group_mappings
+
+
 def cache_ldap(session, engine):
     """Import ldap entries into the cache database."""
-    results = get_ldap_results()
+    response = fetch_ldap_information()
 
-    for result in results:
-        session.add(Nutzer.from_ldap_attributes(result['attributes']))
+    group_mappings = parse_groups(response.group_response)
+
+    for user_entry in response.user_response:
+
+        session.add(Nutzer.from_ldap_attributes(user_entry['attributes'],
+                                                group_mappings=group_mappings))
     session.commit()
-    print("Cached {} ldap entries".format(len(results)))
+    print("Cached {} ldap users".format(len(response.user_response)))
--- legacy/ldap_model.py
+++ legacy/ldap_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
@@ -16,16 +16,29 @@ class Nutzer(Base):
     uidNumber = Column(Integer, nullable=False)
     gidNumber = Column(Integer, nullable=False)
     loginShell = Column(String, nullable=False)
+    aktiv = Column(Boolean, nullable=False)
+    exaktiv = Column(Boolean, nullable=False)
 
     @classmethod
-    def from_ldap_attributes(cls, result):
+    def from_ldap_attributes(cls, result, group_mappings=None):
         if result.get('mail'):
             mail = result['mail'][0]
         else:
             mail = None
 
+        uid = result['uid'][0]
+
+        if group_mappings is not None:
+            groups = [cn for cn, members in group_mappings.items()
+                      if uid in members]
+            if len(groups) > 1:
+                raise ValueError("Non-Disjoint group memberships: {}"
+                                 .format(groups))
+        else:
+            groups = []
+
         return cls(
-            uid=result['uid'][0],
+            uid=uid,
             mail=mail,
             # we can safely assume users only have exactly one password.
             # I checked this for our production data on 2016-11-03.
@@ -34,4 +47,6 @@ class Nutzer(Base):
             uidNumber=result['uidNumber'],
             gidNumber=result['gidNumber'],
             loginShell=result['loginShell'],
+            aktiv='Aktiv' in groups,
+            exaktiv='Exaktiv' in groups,
         )
