6c954a87a8bf6a9e5d567d815cdd609c34a9a8ee
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2017-03-09 22:03:55 +0100

    Test `execute` functionality of `AddAction`
    This tests whether `AddAction` works as desired when running against a
    mocked ldap connection.

--- tests/ldap_sync/test_action.py
+++ tests/ldap_sync/test_action.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 
-from pycroft.ldap_sync.exporter import Record
-from pycroft.ldap_sync.action import Action, ModifyAction
+import ldap3
+
+from pycroft.ldap_sync.exporter import Record, dn_from_username
+from pycroft.ldap_sync.action import Action, AddAction, ModifyAction, LDAP_OBJECTCLASSES
 
 
 class ActionSubclassTestCase(TestCase):
@@ -66,3 +68,76 @@ class ModifyActionConstructorTestCase(TestCase):
             desired={'foo': None},
         )
         self.assertEqual(action.modifications, {'foo': None})
+
+
+class MockedLdapTestBase(TestCase):
+    base = 'ou=Nutzer,ou=Pycroft,dc=AG DSN,dc=de'
+
+    def setUp(self):
+        self.server = ldap3.Server('fake_server', get_info=ldap3.ALL)
+        self.connection = ldap3.Connection(self.server, user='cn=test', password='pw',
+                                           client_strategy=ldap3.MOCK_SYNC)
+        self.connection.open()
+        self.single_valued_attributes = ()
+
+    def get_all_objects(self):
+        self.connection.search(search_base=self.base, search_filter='(objectclass=*)',
+                               attributes='*')
+        return self.connection.response
+
+    def validate_attribute_type(self, key, value):
+        """Validate if some attribute value has the correct type.
+
+        This concerns the single-valuedness, which is estimated using
+        a hard-coded, global list :py:obj:`SINGLE_VALUED_ATTRIBUTES`
+        capturing teh most relevant attributes restricted to a single
+        value.
+        """
+        if key in SINGLE_VALUED_ATTRIBUTES and isinstance(value, list):
+            raise ValueError("Value '{}' for key '{}' should be a single value"
+                             .format(value, key))
+        if key not in SINGLE_VALUED_ATTRIBUTES and not isinstance(value, list):
+            raise ValueError("Value '{}' for key '{}' should be a list"
+                             .format(value, key))
+
+    def tearDown(self):
+        self.connection.unbind()
+
+
+SINGLE_VALUED_ATTRIBUTES = ['uidNumber', 'gidNumber', 'homeDirectory',
+                            'gecos', 'shadowMin', 'shadowMax',
+                            'shadowFlag', 'shadowExpire', 'loginShell']
+
+
+class AddActionTestCase(MockedLdapTestBase):
+    def setUp(self):
+        super(AddActionTestCase, self).setUp()
+        self.attributes = {'uid': ['shizzle'], 'emailAddress': ['bas'],
+                           'userPassword': ['$1$dtruiandetnuhgaldrensutrhawtruhs']}
+        self.uid = 'shizzle'
+        self.dn = dn_from_username(self.uid, base=self.base)
+        record = Record(dn=self.dn, attrs=self.attributes)
+        action = AddAction(record=record)
+
+        action.execute(self.connection)
+
+        self.objects = self.get_all_objects()
+
+    def test_dn_correct(self):
+        self.assertEqual(self.objects[0]['dn'], self.dn)
+
+    def test_correct_uid_in_attributes(self):
+        attrs = self.objects[0]['attributes']
+        # uid is not SINGLE VALUE according to schema
+        self.assertEqual(attrs['uid'][0], self.uid)
+
+    def test_correct_objectclasses(self):
+        classes = self.objects[0]['attributes']['objectClass']
+        self.assertEqual(classes, LDAP_OBJECTCLASSES)
+
+    def test_other_attributes_passed(self):
+        received_attributes = self.objects[0]['attributes']
+        for key, value in self.attributes.items():
+            self.validate_attribute_type(key=key, value=value)
+            self.assertIn(key, received_attributes)
+            self.assertEqual(received_attributes[key], value)
