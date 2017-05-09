766f4ba4356209e3cdcf92fe0dfdc10271b0be81
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2017-05-09 19:49:12 +0200

    Add license header to helper scripts

--- helpers/registry.sh
+++ helpers/registry.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2017 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
 
 # registry is needed in any way
 if [[ -z $REGISTRY ]]; then
