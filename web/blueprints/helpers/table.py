e5f3aff9acd18a1ccaafcd18f0a8b8d36fb15015
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2017-05-15 23:26:02 +0200

    Refactor table base class for easier extension
    This allows for an easy modification of e.g. the table header
    generation, which is important for implementing a splitted table.

--- web/blueprints/helpers/table.py
+++ web/blueprints/helpers/table.py
@@ -5,18 +5,48 @@ class Column:
     def __init__(self, name, title, formatter=None, width=0):
         self.name = name
         self.title = title
-        self.formatter = formatter
+        self.formatter = formatter if formatter is not None else False
         self.width = width
+
+    def build_col_args(self, **kwargs):
+        html_args = {
+            'class': "col-sm-{}".format(self.width) if self.width else False,
+            'data-sortable': "true",
+            'data-field': self.name,
+            'data-formatter': self.formatter,
+        }
+        html_args.update(kwargs)
+        return html_params(**html_args)
 
 
 class BootstrapTable:
-    def __init__(self, columns, data_url, row_style=False,
-                 server_pagination=False, sort_order='asc'):
+    def __init__(self, columns, data_url, table_args=None):
         self.columns = columns
         self.data_url = data_url
-        self.row_style = row_style
-        self.server_pagination = server_pagination
-        self.sort_order = sort_order
+        self.table_args = table_args if table_args is not None else {}
+        self._init_table_args()
+
+    def _init_table_args(self):
+        default_args = {
+            'class': "table table-striped",
+            'data-page-size': 20,
+            'data-toggle': "table",
+            'data-cache': "false",
+            'data-url': self.data_url,
+            'data-response-handler': "responseHandler",
+            'data-search': "true",
+            'data-pagination': "true",
+        }
+        for key, val in default_args.items():
+            self.table_args.setdefault(key, val)
+
+    def generate_table_header(self):
+        yield "<thead>"
+        yield "<tr>"
+        for col in self.columns:
+            yield "<th {}>{}</th>".format(col.build_col_args(), col.title)
+        yield "</tr>"
+        yield "</thead>"
 
     def render(self, table_id):
         # NB: in html_args, setting an argument to `False` makes it
@@ -28,73 +58,16 @@ class BootstrapTable:
                                    role="toolbar")
         html.append("<div {}></div>".format(toolbar_args))
 
-        table_args = {
+        table_args = self.table_args
+        table_args.update({
             'id': table_id,
-            'class': "table table-striped",
-            'data-page-size': 20,
-            'data-toggle': "table",
-            'data-cache': "false",
-            'data-url': self.data_url,
-            'data-response-handler': "responseHandler",
-            'data-search': "true",
-            'data-sort-order': self.sort_order,
-            'data-side-pagination': "server" if self.server_pagination else False,
-            'data-pagination': "true",
-            'data-row-style': self.row_style,  # default False
             'data-toolbar': "#{}-toolbar".format(table_id),
-        }
+        })
 
         html.append("<table {}>".format(html_params(**table_args)))
-        html.append("<thead>")
-        html.append("<tr>")
-        for col in self.columns:
-            th_args = {
-                'class': "col-sm-{}".format(col.width) if col.width else False,
-                'data-sortable': "true",
-                'data-field': col.name,
-                'data-formatter': col.formatter if col.formatter is not None else False,
-            }
-            html.append("<th {}>{}</th>".format(html_params(**th_args), col.title))
-
-        html.append("</tr>")
-        html.append("</thead>")
+        html += list(self.generate_table_header())
         html.append("</table>")
+
         return Markup("\n".join(html))
 
 
-"""
-    <div id="{{ table_id }}-toolbar" class="btn-toolbar" role="toolbar">
-        {% if caller is defined %}
-            {{ caller() }}
-        {% endif %}
-    </div>
-    <table id="{{ table_id }}" class="table table-striped"
-           data-page-size=20
-           data-toggle="table"
-           data-cache="false"
-           {% if data_url %}data-url="{{ data_url }}"{% endif %}
-           data-response-handler="responseHandler"
-           data-search="true"
-           data-sort-order="{{ sort_order }}"
-           {% if server_pagination %}data-side-pagination="server"{% endif %}
-           data-pagination="true"
-           {% if row_style %}data-row-style="{{ row_style }}"{% endif %}
-           data-toolbar="#{{ table_id }}-toolbar">
-        <thead>
-        <tr>
-            {% for col in display_columns %}
-                {# The formatter given as `col.formatter` determines
-                the parsing.  A function of this name must be
-                available in the document the table is built into. #}
-                <th data-sortable="true" data-field="{{ col.name }}"
-                    {% if col.formatter is defined %}data-formatter="{{ col.formatter }}"{% endif %}
-                    {% if col.width is defined %}class="col-sm-{{ col. width }}"{% endif %}
-                >{{ col.title }}</th>
-            {% endfor %}
-        </tr>
-        </thead>
-        {% if footer is defined %}<tfoot><tr>{% for footcol in footer %}
-            <td{% if footcol.colspan is defined and footcol.colspan > 0 %} colspan="{{ footcol.colspan }}"{% endif %}>{{ footcol.title }}</td>
-        {% endfor %}</tr></tfoot>{% endif %}
-    </table>
-"""
