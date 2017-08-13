0f3541d3b65b7aefbb5cd5b769ee3f6dcd1d3922
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2017-08-13 14:12:59 +0200

    Soft failing if HadesLogs not configured/registered to flask app
    The log view functions are now usable if no `HadesLogs` extension is
    registered.
    
    Besides that incorporates removal of TODOs and addition of Docstrings.

--- web/blueprints/user/log.py
+++ web/blueprints/user/log.py
@@ -4,10 +4,10 @@ web.blueprints.user.log
 This module contains functions that provide certain types of logs for
 a user.
 """
-from hades_logs import hades_logs
+from hades_logs import hades_logs, HadesConfigError
 from pycroft.model import session
 
-from ..helpers.log import format_hades_log_entry
+from ..helpers.log import format_hades_log_entry, format_hades_disabled_log
 
 
 def iter_hades_switch_ports(room):
@@ -18,7 +18,6 @@ def iter_hades_switch_ports(room):
     :returns: An iterator of (nasportid, nasipaddress) usable as
               arguments in ``HadesLogs.fetch_logs()``
     """
-    # TODO: use this in the Room's view
     from pycroft.model._all import Room, SwitchPatchPort, SwitchInterface, Switch
     query = (
         session.session
@@ -32,20 +31,36 @@ def iter_hades_switch_ports(room):
 
 
 def get_user_hades_logs(user):
-    try:
-        log_fetcher = hades_logs.fetch_logs
-    except RuntimeError:
-        return
+    """Iterate over a user's hades logs
 
+    :param User user: the user whose logs to display
+
+    :returns: an iterator over duples (interface, logentry).
+    :rtype: Iterator[SwitchInterface, RadiusLogEntry]
+    """
     for host in user.user_hosts:
         for patch_port in host.room.switch_patch_ports:
             interface = patch_port.switch_interface
             nasportid = interface.name
             nasipaddress = interface.host.management_ip
-            for logentry in log_fetcher(nasipaddress, nasportid):
+            for logentry in hades_logs.fetch_logs(nasipaddress, nasportid):
                 yield interface, logentry
 
 
 def formatted_user_hades_logs(user):
-    return (format_hades_log_entry(interface, entry)
-            for interface, entry in get_user_hades_logs(user))
+    """Iterate over the user's hades logs if configured correctly
+
+    In case of misconfiguration, i.e. if
+    :py:func:`get_user_hades_logs` raises a :py:cls:`RuntimeError`, a
+    dummy log entry containing a warning is yielded.
+
+    :param User user: the user whose logs to display
+
+    :returns: an iterator of log rows.  See :py:module:`..helpers.log`
+    """
+    try:
+        for interface, entry in get_user_hades_logs(user):
+            yield format_hades_log_entry(interface, entry)
+    except HadesConfigError:
+        yield format_hades_disabled_log()
+        return
