ba9eac1cb24dbdab2502622894ed570408750cba
Lukas Juhrich <lukasjuhrich@wh2.tu-dresden.de>
2017-09-16 15:44:49 +0200

    Add a helper script
    This can be used nicely with an interactive prompt.

---
+++ helpers/interactive.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Usage
+~~~~~
+
+This python module can be used to quickly initialize an interactive python
+session like this:
+
+```python
+from helpers.interactive import *
+```
+
+This automatically imports all ORM classes plus the `config` object,
+and initializes the session.
+
+"""
+import os
+
+from flask import _request_ctx_stack
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from pycroft.model.session import set_scoped_session
+from pycroft.model._all import *
+from pycroft import config
+
+
+connection_string = os.environ['PYCROFT_DB_URI']
+
+def setup():
+    engine = create_engine(connection_string, echo=True)
+    # TODO: don't set a scoped session, just a regular one
+    set_scoped_session(scoped_session(sessionmaker(bind=engine),
+                                      scopefunc=lambda: _request_ctx_stack.top))
+
+
+setup()
