c6b8993c1e3805c25d1aa2c45bd6172a3feff776
Sebastian Trebbin <sebastian@smapone.com>
2017-09-20 20:25:31 +0200

    Adds import-helper-script

---
+++ import.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+
+SQL_PATH=data/legacy.sql
+
+if [[ ! -f ${SQL_PATH} ]]; then
+    echo please provide legacy sql unter ${SQL_PATH}
+    exit 1
+fi
+
+COMPOSE="docker-compose  -f compose/default/docker-compose.yml"
+
+${COMPOSE} up -d db
+${COMPOSE} stop web
+
+
+echo importing sql dump
+${COMPOSE} exec --user=postgres db file "/pycroft/${SQL_PATH}"
+
+echo importing to pycroft database
+${COMPOSE} run --rm web python3 -m legacy.import_legacy
