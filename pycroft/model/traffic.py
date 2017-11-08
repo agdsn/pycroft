4cab457957fbec15ad13e5887ebea28b5cfade0c
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2017-11-08 20:59:21 +0100

    WIP: Add pmacct pseudo table and rules

--- pycroft/model/traffic.py
+++ pycroft/model/traffic.py
@@ -2,14 +2,20 @@
 # Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
-from sqlalchemy import Column, ForeignKey, CheckConstraint
+from sqlalchemy import Column, ForeignKey, CheckConstraint, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.sql import Insert
 from sqlalchemy.types import BigInteger, Enum, Integer, DateTime
 
 from pycroft.model.base import ModelBase, IntegerIdModel
+from pycroft.model.ddl import DDLManager, Rule
+from pycroft.model.types import IPAddress
 from pycroft.model.user import User
 from pycroft.model.functions import utcnow
 from pycroft.model.host import IP
+
+
+ddl = DDLManager()
 
 
 class TrafficBalance(ModelBase):
@@ -42,6 +48,46 @@ class TrafficVolume(TrafficEvent, IntegerIdModel):
                         uselist=False)
     packets = Column(Integer, CheckConstraint('amount >= 0'),
                      nullable=False)
+
+
+class PmacctTable(ModelBase):
+    __abstract__ = True
+    packets = Column(BigInteger, nullable=False)
+    bytes = Column(BigInteger, nullable=False)
+    stamp_inserted = Column(DateTime(timezone=True), nullable=False)
+    stamp_updated = Column(DateTime(timezone=True))
+
+
+class PmacctTrafficEgress(PmacctTable):
+    ip_src = Column(IPAddress, nullable=False)
+    __table_args__ = (
+        PrimaryKeyConstraint('ip_src', 'stamp_inserted'),
+    )
+
+
+"""
+INSERT INTO traffic_volume (type, ip_id, "timestamp", amount, user_id)  SELECT 'IN',
+            ip.id,
+            new.stamp_inserted,
+            new.bytes,
+            host.owner_id
+           FROM ip
+             JOIN interface ON ip.interface_id = interface.id
+             JOIN host ON interface.host_id = host.id
+          WHERE new.ip_dst = ip.address
+"""
+
+ddl.add_rule(
+    PmacctTrafficEgress,
+    Rule("pmacct_traffic_egress_insert", PmacctTrafficEgress, "INSERT",
+         Insert(TrafficVolume.__table__, {}), do_instead=True))
+
+
+class PmacctTrafficIngress(PmacctTable):
+    ip_dst = Column(IPAddress, nullable=False)
+    __table_args__ = (
+        PrimaryKeyConstraint('ip_dst', 'stamp_inserted'),
+    )
 
 
 class TrafficCredit(TrafficEvent, IntegerIdModel):
