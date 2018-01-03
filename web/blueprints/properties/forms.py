10972e3f5b5d3190c8f4fb835b5476ecc0d9a161
Marcel Beyer <git_mbeyer@it-maker.eu>
2018-01-03 23:04:08 +0100

    validierung der Eingabe

--- web/blueprints/properties/forms.py
+++ web/blueprints/properties/forms.py
@@ -6,9 +6,8 @@
 from datetime import date
 
 from flask_wtf import FlaskForm as Form
-from wtforms.validators import DataRequired, Regexp, NumberRange
+from wtforms.validators import DataRequired, Regexp, NumberRange, ValidationError
 from web.form.fields.core import TextField, IntegerField, DateField, DateTimeField
-
 
 class TrafficGroupForm(Form):
     name = TextField(u"Gruppenname", [
@@ -24,6 +23,23 @@ class TrafficGroupForm(Form):
         DataRequired(u"Wie viel GB?"),
         NumberRange(0, None, u"Muss eine natürliche Zahl sein!")
     ])
+
+    def validate_credit_interval(form, field):
+        # check if field.data is in "0 years 0 mons 0 days 0 hours 0 mins 0.00 secs" format
+        data = field.data.split(" ")
+        try:
+            data[0] = int(data[0])
+            data[2] = int(data[2])
+            data[4] = int(data[4])
+            data[6] = int(data[6])
+            data[8] = int(data[8])
+            data[10] = float(data[10])
+        except (TypeError, ValueError):
+            raise ValidationError('Eingabe ist in ungültigem Format.')
+        if not (data[1] == "years" and data[3] == "mons" and data[5] == "days" \
+            and data[7] == "hours" and data[9] == "mins" and data[11] == "secs"):
+            raise ValidationError('Eingabe ist in ungültigem Format.')
+
 
 
 class PropertyGroupForm(Form):
