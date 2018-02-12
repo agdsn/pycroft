7b10d7e39c13cc2a7075cabe5e1b0122e2d9b22a
Lukas Juhrich <lukas.juhrich@agdsn.de>
2018-02-12 05:01:27 +0100

    Add Columns for pmacct views and fix/extend tests

--- tests/model/test_traffic.py
+++ tests/model/test_traffic.py
@@ -1,28 +1,49 @@
-from pycroft.model.traffic import PmacctTrafficEgress, PmacctTrafficIngress, TrafficVolume
+from pycroft.model import session
+from pycroft.model.traffic import TrafficVolume, pmacct_traffic_egress, pmacct_traffic_ingress
 from tests import FactoryDataTestBase
-from tests.factories.traffic import PMAcctTrafficEgressFactory, PMAcctTrafficIngressFactory
 from tests.factories.user import UserWithHostFactory
 
 
-class PMAcctPseudoTableTest(FactoryDataTestBase):
+class PMAcctViewTest(FactoryDataTestBase):
     ip = '141.30.228.39'
     bad_ip = '141.30.228.1'
+    egress_table = pmacct_traffic_egress.table
+    ingress_table = pmacct_traffic_ingress.table
     def create_factories(self):
         self.user = UserWithHostFactory(host__interface__ip__str_address=self.ip)
 
+    def build_insert(self, type, **kwargs):
+        if type == 'Ingress':
+            table = self.ingress_table
+            ip_key = 'ip_dst'
+        elif type == 'Egress':
+            table = self.egress_table
+            ip_key = 'ip_src'
+        else:
+            raise ValueError("type must be one of 'Ingress', 'Egress'")
+        stamp = session.utcnow()
+        values = {
+            'bytes': 1024,
+            'packets': 20,
+            ip_key: self.ip,
+            'stamp_inserted': stamp,
+            'stamp_updated': stamp,
+        }
+
+        values.update(**kwargs)
+        return table.insert().values(**values)
+
     def test_egress_insert(self):
-        egress_traffic = PMAcctTrafficEgressFactory.create(ip_src=self.ip)
-        self.assertEqual(PmacctTrafficEgress.q.count(), 0)
+        session.session.execute(self.build_insert(type='Egress'))
         self.assertEqual(TrafficVolume.q.count(), 1)
         volume = TrafficVolume.q.one()
         self.assertEqual(volume.type, 'Egress')
-        self.assertEqual(volume.amount, egress_traffic.bytes)
-        self.assertEqual(volume.packets, egress_traffic.packets)
+        self.assertEqual(volume.amount, 1024)
+        self.assertEqual(volume.packets, 20)
         self.assertEqual(volume.user, self.user)
 
     def test_egress_insert_nonexistent_ip(self):
-        PMAcctTrafficEgressFactory.create(ip_src=self.bad_ip)
-        self.assertEqual(PmacctTrafficEgress.q.count(), 0)
+        session.session.execute(self.build_insert(type='Egress', ip_src="1.1.1.1"))
         self.assertEqual(TrafficVolume.q.count(), 0)
 
     def test_egress_update_successive_entries(self):
@@ -33,29 +54,40 @@ class PMAcctPseudoTableTest(FactoryDataTestBase):
             ('2018-03-15 23:59:00', 12, 7055),
         ]
         for stamp, packets, bytes in data:
-            PMAcctTrafficEgressFactory.create(
-                ip_src=self.ip,
-                stamp_inserted=stamp,
-                bytes=bytes,
-                packets=packets,
-            )
-        self.assertEqual(PmacctTrafficEgress.q.count(), 0)
+            session.session.execute(self.build_insert(type='Egress', packets=packets, bytes=bytes,
+                                                      stamp_inserted=stamp, stamp_updated=stamp))
         self.assertEqual(TrafficVolume.q.count(), 1)
         vol = TrafficVolume.q.one()
-        self.assertEqual(vol.timestamp, '2018-03-15 00:00:00')
+        self.assertEqual(str(vol.timestamp), '2018-03-15 00:00:00')
+        self.assertEqual(vol.packets, sum(x[1] for x in data))
+        self.assertEqual(vol.amount, sum(x[2] for x in data))
 
 
     def test_ingress_insert(self):
-        ingress_traffic = PMAcctTrafficIngressFactory.create(ip_dst=self.ip)
-        self.assertEqual(PmacctTrafficIngress.q.count(), 0)
+        session.session.execute(self.build_insert(type='Ingress'))
         self.assertEqual(TrafficVolume.q.count(), 1)
         volume = TrafficVolume.q.one()
         self.assertEqual(volume.type, 'Ingress')
-        self.assertEqual(volume.amount, ingress_traffic.bytes)
-        self.assertEqual(volume.packets, ingress_traffic.packets)
+        self.assertEqual(volume.amount, 1024)
+        self.assertEqual(volume.packets, 20)
         self.assertEqual(volume.user, self.user)
 
     def test_ingress_insert_nonexistent_ip(self):
-        PMAcctTrafficIngressFactory.create(ip_dst=self.bad_ip)
-        self.assertEqual(PmacctTrafficEgress.q.count(), 0)
+        session.session.execute(self.build_insert(type='Ingress', ip_dst="1.1.1.1"))
         self.assertEqual(TrafficVolume.q.count(), 0)
+
+    def test_ingress_update_successive_entries(self):
+        data = [
+            # timestamp, packets, amount
+            ('2018-03-15 00:15:00', 200, 1024),
+            ('2018-03-15 10:15:00', 324, 500),
+            ('2018-03-15 23:59:00', 12, 7055),
+        ]
+        for stamp, packets, bytes in data:
+            session.session.execute(self.build_insert(type='Ingress', packets=packets, bytes=bytes,
+                                                      stamp_inserted=stamp, stamp_updated=stamp))
+        self.assertEqual(TrafficVolume.q.count(), 1)
+        vol = TrafficVolume.q.one()
+        self.assertEqual(str(vol.timestamp), '2018-03-15 00:00:00')
+        self.assertEqual(vol.packets, sum(x[1] for x in data))
+        self.assertEqual(vol.amount, sum(x[2] for x in data))
