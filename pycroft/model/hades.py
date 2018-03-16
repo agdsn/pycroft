2bcda93f38ac920807a883b35b1d714850f11be8
Lukas Juhrich <lukas.juhrich@agdsn.de>
2018-03-16 16:35:13 +0100

    Finish radusergroup

--- pycroft/model/hades.py
+++ pycroft/model/hades.py
@@ -6,30 +6,90 @@ from pycroft.model.ddl import DDLManager, View
 from pycroft.model.facilities import Room
 from pycroft.model.host import Interface, Switch, SwitchPort, Host
 from pycroft.model.net import VLAN, Subnet
+from pycroft.model.user import User
+from pycroft.model.property import current_property
 
 hades_view_ddl = DDLManager()
+
+network_access_subq = (
+    # Select `user_id, 1` for all people with network_access
+    Query([current_property.table.c.user_id.label('user_id'),
+           literal(1).label('network_access')])
+    .filter(current_property.table.c.property_name == 'network_access')
+    .subquery('users_with_network_access')
+)
+
+radgroup_property_mappings = Table(
+    'radgroup_property_mappings',
+    ModelBase.metadata,
+    Column('property', String, primary_key=True),
+    Column('radgroup', String(128), nullable=False),
+)
 
 radusergroup = View(
     name='radusergroup',
     query=union_all(
-        # Priority 0: valid case (interface's mac w/ vlan at correct ports)
-        Query([]).add_columns(
+        # Priority 20: valid case (interface's mac w/ vlan at correct ports)
+        # <mac> @ <switch>/<port> → <vlan>_[un]tagged (Prio 20)
+        Query([
             Interface.mac.label('username'),
             # `host()` does not print the `/32` like `text` would
             func.host(Switch.management_ip).label('nasipaddress'),
             SwitchPort.name.label('nasportid'),
             # TODO: add `_tagged` instead if interface needs that
             (VLAN.name + '_untagged').label('groupname'),
+            literal(20).label('priority'),
+        ]).select_from(User)
+        .join(Host)
+        .join(Interface)
+        .join(Host.room)
+        .join(Room.connected_patch_ports)
+        .join(SwitchPort)
+        .join(Switch)
+        .join(Interface.ips)
+        .join(Subnet)
+        .join(VLAN)
+        .join(current_property.table, current_property.table.c.user_id == User.id)
+        .filter(current_property.table.c.property_name == 'network_access')
+        .statement,
+
+        # Priority 10: Blocking reason exists
+        # <mac> @ <switch>/<port> → finance (Prio 10)
+        Query([
+            Interface.mac.label('username'),
+            func.host(Switch.management_ip).label('nasipaddress'),
+            SwitchPort.name.label('nasportid'),
+            radgroup_property_mappings.c.radgroup.label('groupname'),
+            literal(10).label('priority'),
+        ]).select_from(User)
+        .join(Host)
+        .join(Host.interfaces)
+        .join(Host.room)
+        .join(Room.connected_patch_ports)
+        .join(SwitchPort)
+        .join(Switch)
+        .join(current_property.table, current_property.table.c.user_id == User.id)
+        .join(radgroup_property_mappings,
+              radgroup_property_mappings.c.property == current_property.table.c.property_name)
+        .statement,
+
+        # Priority 0: No blocking reason exists → assume no member (yet/anymore)
+        Query([
+            Interface.mac.label('username'),
+            func.host(Switch.management_ip).label('nasipaddress'),
+            SwitchPort.name.label('nasportid'),
+            literal('no_member').label('groupname'),
             literal(0).label('priority'),
-        ).select_from(Interface)
-         .join(Host)
-         .join(Room)
-         .join(Room.connected_patch_ports)
-         .join(SwitchPort)
-         .join(Switch)
-         .join(Interface.ips)
-         .join(Subnet)
-         .join(VLAN),
+        ]).select_from(User)
+        .outerjoin(network_access_subq, User.id == network_access_subq.c.user_id)
+        .filter(network_access_subq.c.network_access == None)
+        .join(User.hosts)
+        .join(Host.interfaces)
+        .join(Host.room)
+        .join(Room.connected_patch_ports)
+        .join(SwitchPort)
+        .join(Switch)
+        .statement,
     ),
 )
 
