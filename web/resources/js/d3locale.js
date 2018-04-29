3d3a5e6a158a83bef53587b6a6675ddbce14440e
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2018-04-29 14:24:21 +0200

    Use type-safe equality comparisons

--- web/resources/js/d3locale.js
+++ web/resources/js/d3locale.js
@@ -21,7 +21,7 @@ var timeFormat = de.timeFormat.multi([
 ["%H:%M", function(d) { return d.getMinutes(); }],
 ["%H:%M", function(d) { return d.getHours(); }],
 ["%a %d", function(d) { return d.getDay() && d.getDate() != 1; }],
-["%b %d", function(d) { return d.getDate() != 1; }],
+["%b %d", function(d) { return d.getDate() !== 1; }],
 ["%B", function(d) { return d.getMonth(); }],
 ["%Y", function() { return true; }]
 ]);
