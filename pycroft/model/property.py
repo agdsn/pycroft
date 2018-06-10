a7627d2c07be2a8d0f2de74d2d3e60427668bdcf
Jakob Müller <j.mueller@outlook.de>
2018-06-10 17:28:12 +0200

    Change current_property view to use new evaluate_properties function

--- pycroft/model/property.py
+++ pycroft/model/property.py
@@ -75,35 +75,10 @@ manager.add_function(
 current_property = View(
     name='current_property',
     #metadata=ModelBase.metadata,
-    query=union(
-        Query([User.id.label('user_id'), Property.name.label('property_name'),
-               literal(False).label('denied')])
-        .select_from(Membership)
-        .join(PropertyGroup)
-        .join(User)
-        .filter(and_(
-            or_(Membership.begins_at == null(), Membership.begins_at <= utcnow_sql()),
-            or_(Membership.ends_at == null(), utcnow_sql() <= Membership.ends_at),
-        ))
-        .join(Property)
-        .group_by(User.id, Property.name)
-        .having(func.every(Property.granted))
-        .statement,
-
-        Query([User.id.label('user_id'), Property.name.label('property_name'),
-               literal(True).label('denied')])
-        .select_from(Membership)
-        .join(PropertyGroup)
-        .join(User)
-        .filter(and_(
-            or_(Membership.begins_at == null(), Membership.begins_at <= utcnow_sql()),
-            or_(Membership.ends_at == null(), utcnow_sql() <= Membership.ends_at),
-        ))
-        .join(Property)
-        .group_by(User.id, Property.name)
-        # granted by ≥1 membership, but also denied by ≥1 membership
-        .having(and_(func.bool_or(Property.granted), ~func.every(Property.granted)))
-        .statement,
+    query=(
+        select([literal_column('user_id'), literal_column('property_name'),
+                literal_column('denied')])
+        .select_from(func.evaluate_properties(utcnow_sql()))
     ),
 )
 manager.add_view(Membership.__table__, current_property)
