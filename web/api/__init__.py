9756366d7bca75c0ea89ce1fface6a7d224f54f7
Georgeto <georgeto@wh2.tu-dresden.de>
2018-06-17 03:03:50 +0200

    Improve error handling of sipa API

--- web/api/__init__.py
+++ web/api/__init__.py
@@ -6,7 +6,4 @@ bp = Blueprint('api', __name__)
 v0.api.init_app(bp)
 
 def errorpage(e):
-    code = getattr(e, 'code', 500)
-    if code == 404:
-        return jsonify(msg=str(e))
-    return jsonify(msg="error", code=code)
+    return v0.api.handle_error(e)
