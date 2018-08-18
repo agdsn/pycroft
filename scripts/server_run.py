21d410968df778b5485d818a6d81047cccb16f98
Sebastian Schrader <shreyder@wh2.tu-dresden.de>
2018-08-18 14:09:06 +0200

    Wait for database in server_run.py

--- scripts/server_run.py
+++ scripts/server_run.py
@@ -5,10 +5,13 @@
 
 import argparse
 import os
-from time import time
+import time
+
+from itertools import chain, repeat
 
 from babel.support import Translations
 from flask import _request_ctx_stack, g, request
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.contrib.profiler import ProfilerMiddleware
 
@@ -18,38 +21,57 @@ from pycroft.model import create_engine
 from pycroft.model.session import set_scoped_session
 import web
 from scripts.schema import AlembicHelper, SchemaStrategist
+from web import make_app
 
 
 def server_run(args):
+    app = make_app(args.debug)
+
     try:
         connection_string = os.environ['PYCROFT_DB_URI']
     except KeyError:
         raise RuntimeError("Environment variable PYCROFT_DB_URI must be "
                            "set to an SQLAlchemy connection string.")
+
     engine = create_engine(connection_string)
-    connection = engine.connect()
+    if args.wait_for_database == 0:
+        max_wait = float('inf')
+    else:
+        max_wait = time.clock_gettime(time.CLOCK_MONOTONIC) + args.wait_for_database
+    for timeout in chain([1, 2, 5, 10, 30], repeat(60)):
+        try:
+            connection = engine.connect()
+            break
+        except OperationalError:
+            # Important: Use %r to print the URL, passwords are hidden by the
+            # __repr__ of sqlalchemy.engine.URL
+            app.logger.warn("Could not connect to database %r", engine.url)
+            timeout = min(timeout,
+                          max_wait - time.clock_gettime(time.CLOCK_MONOTONIC))
+            if timeout > 0:
+                app.logger.info("Waiting for %d seconds", timeout)
+                time.sleep(timeout)
+            else:
+                raise
     state = AlembicHelper(connection)
     strategy = SchemaStrategist(state).determine_schema_strategy()
     strategy()
-
-    from web import make_app
 
     print("If you're running in a docker setup, the port may differ"
           " from what is given below."
           " It is probably http://0.0.0.0:5001")
 
-    app = make_app(args.debug)
     @app.before_request
     def get_time():
-        g.request_time = time()
+        g.request_time = time.time()
 
     @app.teardown_request
     def time_response(exception=None):
-        time_taken = time() - g.request_time
+        time_taken = time.time() - g.request_time
         if time_taken > 0.5:
             app.logger.warn(
                 "Response took {duration} seconds for request {path}".format(
-                path=request.full_path, duration=time_taken))
+                    path=request.full_path, duration=time_taken))
 
     engine = create_engine(connection_string, echo=args.profile)
     set_scoped_session(scoped_session(sessionmaker(bind=engine),
@@ -86,6 +108,9 @@ def main():
                         dest='host', help="expose server on network")
     parser.add_argument("-p","--port", action="store",
                         help="port to run Pycroft on", type=int, default=5000)
+    parser.add_argument("-w", "--wait-for-database", type=int, default=30,
+                        help="Maximum time to wait for database to become "
+                             "available. Use 0 to wait forever.")
 
     server_run(parser.parse_args())
 
