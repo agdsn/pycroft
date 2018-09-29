bd8b2ee81413700791185706a915f1f998832a55
Jakob Müller <j.mueller@outlook.de>
2018-09-29 14:37:24 +0200

    Do not try to refill room data if room is None

--- web/blueprints/helpers/form.py
+++ web/blueprints/helpers/form.py
@@ -2,20 +2,21 @@ from pycroft.model.facilities import Room
 
 
 def refill_room_data(form, room):
-    form.building.data = room.building
+    if room:
+        form.building.data = room.building
 
-    levels = Room.q.filter_by(building_id=room.building.id).order_by(Room.level)\
-                   .distinct()
+        levels = Room.q.filter_by(building_id=room.building.id).order_by(Room.level)\
+                       .distinct()
 
-    form.level.choices = [(entry.level, str(entry.level)) for entry in
-                          levels]
-    form.level.data = room.level
+        form.level.choices = [(entry.level, str(entry.level)) for entry in
+                              levels]
+        form.level.data = room.level
 
-    rooms = Room.q.filter_by(
-        building_id=room.building.id,
-        level=room.level
-    ).order_by(Room.number).distinct()
+        rooms = Room.q.filter_by(
+            building_id=room.building.id,
+            level=room.level
+        ).order_by(Room.number).distinct()
 
-    form.room_number.choices = [(entry.number, str(entry.number))
-                                for entry in rooms]
-    form.room_number.data = room.number
+        form.room_number.choices = [(entry.number, str(entry.number))
+                                    for entry in rooms]
+        form.room_number.data = room.number
