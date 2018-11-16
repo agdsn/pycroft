8a46db108e1f257920df45565c4d9b7f811cd7fa
Julian Fölsch <julian.foelsch@pexxlab.xyz>
2018-11-16 17:01:49 +0100

    Add a few useful keyboard shortcuts

--- web/resources/js/keyboard-shortcuts.js
+++ web/resources/js/keyboard-shortcuts.js
@@ -1,5 +1,18 @@
 import hotkeys from 'hotkeys-js';
 
-hotkeys('command+b', function (event) {
-    alert('Hello world!');
+hotkeys('option+c', function (event) {
+    window.location = "/user/create";
 });
+hotkeys('shift+option+c', function (event) {
+    window.open("/user/create");
+});
+hotkeys('option+f', function (event) {
+    window.location = "/user/search";
+});
+hotkeys('shift+option+f', function (event) {
+    window.open("/user/search");
+});
+hotkeys('option+h', function (event) {
+    window.open("https://www.youtube.com/embed/Ita6WyQ3SFQ?autoplay=1&controls=0");
+});
+
