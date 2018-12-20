ed3e11d1f1a86b0adc462accd6e94641d94623ff
Jakob Müller <j.mueller@outlook.de>
2018-12-20 12:32:27 +0100

    Add PortForm

---
+++ web/blueprints/infrastructure/forms.py
@@ -0,0 +1,12 @@
+from wtforms.validators import DataRequired, IPAddress
+
+from web.blueprints.facilities.forms import SelectRoomForm
+from web.form.fields.core import TextField
+
+
+class PortForm(SelectRoomForm):
+    switchport_name = TextField(label="Switchport Name", validators=[DataRequired()])
+    patchport_name = TextField(label="→ Patchport Name", validators=[DataRequired()])
+
+    _order = ("switchport_name", "patchport_name")
+
