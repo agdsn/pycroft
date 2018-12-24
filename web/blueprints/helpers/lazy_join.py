61b0357b22c87954c0385822d065fd4f51175004
Lukas Juhrich <lukas.juhrich@agdsn.de>
2018-12-24 22:00:31 +0100

    Implement RuntimeError on exhaustion

--- web/blueprints/helpers/lazy_join.py
+++ web/blueprints/helpers/lazy_join.py
@@ -3,20 +3,49 @@ from types import FunctionType
 from typing import Generator, Callable, Iterable, overload
 
 
+def filled_iter(iter, filler):
+    """Inbetween every iteration, yield a constant filler."""
+    first = True
+    for elem in iter:
+        if not first:
+            yield filler
+        else:
+            first = False
+        yield elem
+
+
 class LazyJoined:
-    """A string that consists of multiple components"""
+    """A string that consists of multiple components
+
+    NOTE: Just like a generator, it will be exhausted after the first call!
+    """
     _components: Iterable
 
     def __init__(self, components: Iterable, glue: str = ""):
         self.glue = glue
         self._components = components
+        self.exhausted = False
+
+    @property
+    def _stringified_components(self):
+        return (str(c) for c in self._components)
 
     def __str__(self):
-        return self.glue.join(str(c) for c in self._components)
+        self._mark_exhausted()
+        return self.glue.join(self._stringified_components)
 
     def __iter__(self):
-        # TODO if glue, insert it every second time
-        return iter(self._components)
+        self._mark_exhausted()
+        if self.glue:
+            return filled_iter(self._stringified_components, filler=self.glue)
+        return iter(self._stringified_components)
+
+    def _mark_exhausted(self):
+        if self.exhausted:
+            raise RuntimeError("LazyJoined object already exhausted!"
+                               " You may call __str__ or __iter__ only once."
+                               " For re-use, turn it into a list.")
+        self.exhausted = True
 
 
 DecoratedInType = Callable[..., (Generator[str, None, None])]
