5345f8a81626157481e7c85cd226593eb5e7cbed
Jakob Müller <j.mueller@outlook.de>
2019-03-06 00:20:20 +0100

    Fix finance triggers
    Closes #308

--- pycroft/model/finance.py
+++ pycroft/model/finance.py
@@ -199,11 +199,19 @@ manager.add_function(
           s split;
           count integer;
           balance integer;
+          transaction_deleted boolean;
         BEGIN
-          s := COALESCE(NEW, OLD);
+          IF TG_OP = 'DELETE' THEN
+            s := OLD;
+          ELSE
+            s := COALESCE(NEW, OLD);
+          END IF;
+          
+          SELECT COUNT(*) = 0 INTO transaction_deleted FROM "transaction" WHERE "id" = s.transaction_id);
+            
           SELECT COUNT(*), SUM(amount) INTO STRICT count, balance FROM split
               WHERE transaction_id = s.transaction_id;
-          IF count < 2 THEN
+          IF count < 2 AND NOT transaction_deleted THEN
             RAISE EXCEPTION 'transaction %% has less than two splits',
             s.transaction_id
             USING ERRCODE = 'integrity_constraint_violation';
@@ -359,7 +367,12 @@ manager.add_function(
           v_bank_account_account_id integer;
           v_split split;
         BEGIN
-          v_activity := COALESCE(NEW, OLD);
+          IF TG_OP = 'DELETE' THEN
+            v_activity := OLD;
+          ELSE
+            v_activity := COALESCE(NEW, OLD);
+          END IF;
+          
           SELECT bank_account.account_id INTO v_bank_account_account_id FROM bank_account
               WHERE bank_account.id = v_activity.bank_account_id;
           SELECT * INTO v_split FROM split
