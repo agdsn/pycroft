ea67eb55bc9584be27b1c0f5a2bde6729d814983
Jakob Müller <j.mueller@outlook.de>
2019-06-23 19:35:52 +0200

    Close session after task execution

--- pycroft/helpers/task.py
+++ pycroft/helpers/task.py
@@ -4,12 +4,15 @@ import os
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from pycroft.model import create_engine
-from pycroft.model.session import set_scoped_session
+from pycroft.model.session import set_scoped_session, session
 
 
 class DBTask(Task):
     def run(self, *args, **kwargs):
         pass
+
+    def after_return(self, status, retval, task_id, args, kwargs, einfo=None):
+        session.close()
 
     try:
         connection_string = os.environ['PYCROFT_DB_URI']
