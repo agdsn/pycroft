692a4da10a141b6b52e6aa0c688695f9c23ea120
Jakob Müller <j.mueller@outlook.de>
2019-08-27 10:10:01 +0200

    Minimize database sessions

--- pycroft/helpers/task.py
+++ pycroft/helpers/task.py
@@ -8,17 +8,26 @@ from pycroft.model.session import set_scoped_session, session
 
 
 class DBTask(Task):
+    """
+    Base class for tasks which use the database.
+    """
+
     def run(self, *args, **kwargs):
         pass
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo=None):
         session.close()
 
-    try:
-        connection_string = os.environ['PYCROFT_DB_URI']
-    except KeyError:
-        raise RuntimeError("Environment variable PYCROFT_DB_URI must be "
-                           "set to an SQLAlchemy connection string.")
+    def __init__(self):
+        try:
+            connection_string = os.environ['PYCROFT_DB_URI']
+        except KeyError:
+            raise RuntimeError("Environment variable PYCROFT_DB_URI must be "
+                               "set to an SQLAlchemy connection string.")
 
-    engine = create_engine(connection_string)
-    set_scoped_session(scoped_session(sessionmaker(bind=engine)))
+        self.engine = create_engine(connection_string)
+        self.connection = self.engine.connect()
+        set_scoped_session(scoped_session(sessionmaker(bind=self.engine)))
+
+    def __del__(self):
+        self.connection.close()
--- pycroft/lib/task.py
+++ pycroft/lib/task.py
@@ -1,23 +1,13 @@
-import os
-import traceback
 from abc import ABC, abstractmethod
 
-from datetime import timedelta
-
-from celery import Celery
 from sqlalchemy.orm import with_polymorphic
 
-from pycroft.helpers.task import DBTask
 from pycroft.lib.logging import log_task_event
 from pycroft.model import session
 from pycroft.model.facilities import Room
 from pycroft.model.session import with_transaction
 from pycroft.model.task import UserTask, Task, TaskType, TaskStatus, \
     UserMoveSchema, UserMoveOutSchema, UserMoveInSchema
-from pycroft.model.traffic import TrafficVolume
-
-app = Celery('tasks', backend=os.environ['PYCROFT_CELERY_RESULT_BACKEND_URI'],
-             broker=os.environ['PYCROFT_CELERY_BROKER_URI'])
 
 
 class TaskImpl(ABC):
@@ -167,6 +157,12 @@ class UserMoveInTaskImpl(UserTaskImpl):
                 "Tried to move in user, but user was already living in a dormitory.")
 
 
+task_type_to_impl = {
+    TaskType.USER_MOVE: UserMoveTaskImpl,
+    TaskType.USER_MOVE_IN: UserMoveInTaskImpl,
+    TaskType.USER_MOVE_OUT: UserMoveOutTaskImpl
+}
+
 
 @with_transaction
 def schedule_user_task(task_type, due, user, parameters, processor):
@@ -199,85 +195,3 @@ def cancel_task(task, processor):
     log_task_event("Cancelled task {}.".format(task.id), processor, task)
 
     task.status = TaskStatus.CANCELLED
-
-
-@with_transaction
-def write_task_message(task, message, log=False):
-    message = str(message)
-
-    if log:
-        log_task_event(message, task.creator, task)
-
-    print(message)
-
-
-def repair_session():
-    if not session.session.is_active:
-        session.session.rollback()
-        print("Repaired session (rollback).")
-
-
-task_type_to_impl = {
-    TaskType.USER_MOVE: UserMoveTaskImpl,
-    TaskType.USER_MOVE_IN: UserMoveInTaskImpl,
-    TaskType.USER_MOVE_OUT: UserMoveOutTaskImpl
-}
-
-
-@app.task(base=DBTask)
-def execute_scheduled_tasks():
-    tasks = (session.session.query(with_polymorphic(Task, "*"))
-             .filter(Task.status == TaskStatus.OPEN,
-                     Task.due <= session.utcnow())
-             .all())
-
-    print("executing {} scheduled tasks".format(len(tasks)))
-
-    for task in tasks:
-        repair_session()
-
-        task_impl = task_type_to_impl.get(task.type)()
-
-        try:
-            task_impl.execute(task)
-        except Exception as e:
-            task_impl.errors.append(str(e))
-
-        repair_session()
-
-        if task_impl.new_status is not None:
-            task.status = task_impl.new_status
-
-        if task_impl.errors:
-            task.errors = task_impl.errors
-
-            for error in task.errors:
-                print("Error while executing task: {}".format(error))
-
-        write_task_message(task, "Processed {} task. Status: {}".format(
-            task.type.name, task.status.name), log=True)
-
-        session.session.commit()
-
-
-@app.task(base=DBTask)
-def remove_old_traffic_data():
-    TrafficVolume.q.filter(TrafficVolume.timestamp < (session.utcnow() - timedelta(7))).delete()
-
-    session.session.commit()
-
-    print("Deleted old traffic data")
-
-
-app.conf.update(
-    CELERYBEAT_SCHEDULE={
-        'execute-scheduled-tasks': {
-            'task': 'pycroft.lib.task.execute_scheduled_tasks',
-            'schedule': timedelta(hours=1)
-        },
-        'remove-old-traffic-data': {
-            'task': 'pycroft.lib.task.remove_old_traffic_data',
-            'schedule': timedelta(days=1)
-        },
-    },
-    CELERY_TIMEZONE='UTC')
