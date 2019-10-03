cc3fd3ab9d59388def8837ce3a075398c3dfe2b0
Lukas Juhrich <lukas.juhrich@agdsn.de>
2019-10-03 02:27:02 +0200

    Create address model #305

---
+++ pycroft/model/address.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 The Pycroft Authors. See the AUTHORS file.
+# This file is part of the Pycroft project and licensed under the terms of
+# the Apache License, Version 2.0. See the LICENSE file for details.
+from sqlalchemy import Column, String, UniqueConstraint
+
+from pycroft.model.base import IntegerIdModel
+
+
+DEFAULT_CITY = "Dresden"
+DEFAULT_STATE = "Sachsen"
+DEFAULT_COUNTRY = "Germany"
+
+class Address(IntegerIdModel):
+    street = Column(String(), nullable=False)
+    number = Column(String(), nullable=False)
+    addition = Column(String())
+    # Sometimes, zipcodes can contain things like dashes, so rather take String().
+    # we could probably impose some format by a check but that would be over engineering
+    zip_code = Column(String(), nullable=False)
+    city = Column(String(), nullable=False, default=DEFAULT_CITY)
+    state = Column(String(), nullable=False, default=DEFAULT_STATE)
+    country = Column(String(), nullable=False, default=DEFAULT_COUNTRY)
+
+    __table_args__ = (
+        UniqueConstraint('street', 'number', 'addition', 'zip_code', 'city', 'state', 'country'),
+    )
