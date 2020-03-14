a14fcec19471bffcb1dcbad1d0cff7100d0ef6bc
Jakob Mueller <jakob.mueller@ibot3.de>
2020-03-14 16:36:27 +0100

    Make interface name optional and remove unicode prefixes

--- web/blueprints/host/forms.py
+++ web/blueprints/host/forms.py
@@ -10,14 +10,16 @@ from web.form.fields.validators import MacAddress
 
 
 class HostForm(SelectRoomForm):
-    owner_id = UserIDField(u"Besitzer-ID")
-    name = TextField(u"Name", validators=[Optional()],
-                     description=u"z.B. TP-LINK WR841, FritzBox 4040 oder MacBook")
+    owner_id = UserIDField("Besitzer-ID")
+    name = TextField("Name", validators=[Optional()],
+                     description="z.B. TP-LINK WR841, FritzBox 4040 oder MacBook")
 
     _order = ("name", "owner_id")
 
 
 class InterfaceForm(Form):
-    name = TextField(u"Name",description=u"z.B. eth0, en0 oder enp0s29u1u1u5")
-    mac = MacField(u"MAC", [MacAddress(message=u"MAC ist ungültig!")])
+    name = TextField("Name",
+                     description="z.B. eth0, en0 oder enp0s29u1u1u5",
+                     validators=[Optional()])
+    mac = MacField("MAC", [MacAddress(message="MAC ist ungültig!")])
     ips = SelectMultipleField(u"IPs", validators=[Optional()])
