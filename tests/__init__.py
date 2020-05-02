10804b0055491ac8341020aea8ed86fd0bad745c
Lukas Juhrich <lukas.juhrich@agdsn.de>
2020-05-02 22:11:00 +0200

    Fix cleanup order
    First do the inner cleanup, then the cleanup of the next base.
    This is the reverse order of the setUps (first the more generic, then
    the more specific setUp).

--- tests/__init__.py
+++ tests/__init__.py
@@ -224,8 +224,8 @@ class FixtureDataTestBase(SQLAlchemyTestCase, DataTestCase, unittest.TestCase):
         )
 
     def cleanup(self):
-        super(FixtureDataTestBase, self).cleanup()
         self.data.teardown()
+        super(FixtureDataTestBase, self).cleanup()
 
 
 class DialectSpecificTestCase(FixtureDataTestBase):
