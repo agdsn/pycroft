63d499630468734d8eb08d2709046509fea741b1
Jakob Mueller <jakob.mueller@ibot3.de>
2020-08-31 15:19:16 +0200

    Remove old form files

--- web/form/fields/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2014 The Pycroft Authors. See the AUTHORS file.
-# This file is part of the Pycroft project and licensed under the terms of
-# the Apache License, Version 2.0. See the LICENSE file for details.
-__author__ = 'shreyder'
