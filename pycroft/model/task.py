dc686341b44995c7b6f0c915b0e042edfbe831c6
Jakob Mueller <jakob.mueller@ibot3.de>
2020-09-07 14:09:31 +0200

    Fix move_in task failing because of missing parameters

--- pycroft/model/task.py
+++ pycroft/model/task.py
@@ -21,6 +21,7 @@ class TaskStatus(AutoNumber):
     FAILED = ()
     CANCELLED = ()
 
+
 class Task(IntegerIdModel):
     discriminator = Column('task_type', String(50))
     __mapper_args__ = {'polymorphic_on': discriminator}
@@ -70,6 +71,7 @@ class UserMoveOutSchema(Schema):
     comment = fields.Str()
     end_membership = fields.Bool()
 
+
 class UserMoveSchema(Schema):
     room_number = fields.Str()
     level = fields.Int()
@@ -77,14 +79,16 @@ class UserMoveSchema(Schema):
     comment = fields.Str()
     end_membership = fields.Bool()
 
+
 class UserMoveInSchema(Schema):
     room_number = fields.Str()
     level = fields.Int()
     building_id = fields.Int()
-    mac = fields.Str()
-    birthdate = fields.Date()
-    begin_membership = fields.Bool()
-    host_annex = fields.Bool()
+    mac = fields.Str(allow_none=True, missing=None)
+    birthdate = fields.Date(allow_none=True, missing=None)
+    begin_membership = fields.Bool(missing=False)
+    host_annex = fields.Bool(missing=False)
+
 
 task_type_to_schema = {
     TaskType.USER_MOVE: UserMoveSchema,
