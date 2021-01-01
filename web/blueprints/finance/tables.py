49cf29d0ebaa4668abab7ac3444824dbda26b6e6
Lucas Hecht <lucas.hecht@agdsn.de>
2021-01-01 16:50:15 +0100

    Fix typo

--- web/blueprints/finance/tables.py
+++ web/blueprints/finance/tables.py
@@ -124,7 +124,7 @@ class UsersDueTable(BootstrapTable):
     user = LinkColumn("Name")
     valid_on = Column("Gültig am")
     description = Column("Beschreibung")
-    fee_account_id = LinkColumn("Beitragsonto")
+    fee_account_id = LinkColumn("Beitragskonto")
     amount = Column("Betrag", formatter="table.coloredFormatter")
 
 
