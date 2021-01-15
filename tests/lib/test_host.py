16610ee8efb4d164e4d33822db3b67bd31d6138f
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-01-15 16:04:08 +0100

    migrate test_host to factories #25

--- tests/lib/test_host.py
+++ tests/lib/test_host.py
@@ -2,25 +2,18 @@
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 from pycroft.lib.host import change_mac
-from pycroft.model import session
-from pycroft.model.host import Interface
-from pycroft.model.user import User
-from pycroft.model.logging import LogEntry
-from tests import FixtureDataTestBase
-from tests.fixtures.dummy.user import UserData
-from tests.fixtures.dummy.host import InterfaceData
+from tests import FactoryDataTestBase
+from tests.factories import UserFactory, UserWithHostFactory, InterfaceFactory
 
 
-class Test_005_change_mac_interface(FixtureDataTestBase):
-    datasets = [InterfaceData, UserData]
+class ChangeMacTest(FactoryDataTestBase):
+    def create_factories(self):
+        super().create_factories()
+        self.interface = InterfaceFactory.create()
+        UserWithHostFactory.create(host__interface=self.interface)
+        self.processor = UserFactory.create()
 
-    def setUp(self):
-        super(Test_005_change_mac_interface, self).setUp()
-        self.user = User.q.filter_by(login=UserData.dummy.login).one()
-
-    def test_0010_change_mac(self):
+    def test_change_mac(self):
         new_mac = "20:00:00:00:00:00"
-        interface = Interface.q.filter_by(
-            mac=InterfaceData.dummy.mac).one()
-        change_mac(interface, new_mac, self.user)
-        self.assertEqual(interface.mac, new_mac)
+        change_mac(self.interface, new_mac, self.processor)
+        self.assertEqual(self.interface.mac, new_mac)
