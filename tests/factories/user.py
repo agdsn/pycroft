4d80c5a7fa0231defc4e507eaa1ecb43d861b0ad
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-01-15 21:14:21 +0100

    Cache passwd hash creation in UserFactory

--- tests/factories/user.py
+++ tests/factories/user.py
@@ -2,23 +2,32 @@
 # Copyright (c) 2016 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
+import functools
+
 import factory
 from factory.faker import Faker
 
+from pycroft.helpers.user import hash_password
 from pycroft.model.user import User, RoomHistoryEntry, UnixAccount
 from .base import BaseFactory
 from .facilities import RoomFactory
 from .finance import AccountFactory
 
 
+@functools.lru_cache()
+def cached_hash(plaintext):
+    return hash_password(plaintext)
+
 class UserFactory(BaseFactory):
     class Meta:
         model = User
+        exclude = ('password',)
 
     login = Faker('user_name')
     name = Faker('name')
     registered_at = Faker('date_time')
     password = Faker('password')
+    passwd_hash = factory.LazyAttribute(lambda o: cached_hash(o.password))
     email = Faker('email')
     account = factory.SubFactory(AccountFactory, type="USER_ASSET")
     room = factory.SubFactory(RoomFactory)
