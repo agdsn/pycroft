57662344eebf9ea468716089d676b1b61d0f4c09
Georgeto <georgeto@wh2.tu-dresden.de>
2021-02-05 02:48:14 +0100

    Fix schedule of mail-negative-members task
    crontab(day_of_month=5)
    -> Execute every minute (!) on the fifth day of every month.

--- pycroft/task.py
+++ pycroft/task.py
@@ -151,7 +151,7 @@ app.conf.update(
         },
         'mail-negative-members': {
             'task': 'pycroft.task.mail_negative_members',
-            'schedule': crontab(day_of_month=5)
+            'schedule': crontab(0, 0, day_of_month=5)
         }
     },
     CELERY_ENABLE_UTC=True,
