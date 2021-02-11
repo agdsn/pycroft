7d76315c3ebd2dc8580cb339afe33371e44e0fbf
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-02-12 00:25:13 +0100

    Add test cases for address cleanup
    Refs #432

--- tests/model/test_address.py
+++ tests/model/test_address.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2019 The Pycroft Authors. See the AUTHORS file.
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
+from contextlib import contextmanager
 
+from pycroft.model.address import Address
 from tests import FactoryDataTestBase
-from tests.factories.address import AddressFactory
+from tests.factories import RoomFactory, UserFactory, AddressFactory
 from tests.factories.base import copy_factory
 
 
@@ -48,3 +50,55 @@ class AddressConstraintsTestCase(FactoryDataTestBase):
             with self.subTest(address=address):
                 # breakpoint()
                 self.assert_address_cannot_be_added(address)
+
+
+@contextmanager
+def assert_address_count(session, count):
+    yield
+    session.commit()
+    assert session.query(Address).count() == count
+
+
+class RoomAddressTriggersTestCase(FactoryDataTestBase):
+    def create_factories(self):
+        self.address = AddressFactory()
+        self.room = RoomFactory(address=self.address)
+
+    def test_room_update_cleanup(self):
+        with assert_address_count(self.session, 1):
+            self.room.address = AddressFactory()  # other address
+            self.session.add(self.room)
+
+    def test_room_delete_cleanup(self):
+        with assert_address_count(self.session, 0):
+            self.session.delete(self.room)
+
+
+class UserAddressTriggersTestCase(FactoryDataTestBase):
+    def create_factories(self):
+        self.address = AddressFactory()
+        self.user = UserFactory(address=self.address, room=None)
+
+    def test_user_update_cleanup(self):
+        with assert_address_count(self.session, 1):
+            self.user.address = AddressFactory()  # other address
+            self.session.add(self.user)
+
+    def test_user_delete_cleanup(self):
+        with assert_address_count(self.session, 0):
+            self.session.delete(self.user)
+
+
+class AddressStaysTestCase(FactoryDataTestBase):
+    def create_factories(self):
+        self.address = AddressFactory()
+        self.room = RoomFactory(address=self.address)
+        self.user = UserFactory(address=self.address, room=self.room)
+
+    def test_address_stays_after_room_delete(self):
+        with assert_address_count(self.session, 1):
+            self.session.delete(self.room)
+
+    def test_address_stays_after_user_delete(self):
+        with assert_address_count(self.session, 1):
+            self.session.delete(self.user)
