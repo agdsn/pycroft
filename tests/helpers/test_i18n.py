97efd0daad7bc5d369cece5775ec80bf74d61a22
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-05-07 11:48:56 +0200

    Migrate helpers.test_i18n to pytest

--- tests/helpers/test_i18n.py
+++ tests/helpers/test_i18n.py
@@ -4,10 +4,11 @@
 import datetime
 import json
 import traceback
-import unittest
-from unittest import TestCase
 from decimal import Decimal
+
 import jsonschema
+import pytest
+
 from pycroft.helpers.i18n import (
     ErroneousMessage, Message, NumericalMessage, SimpleMessage,
     deserialize_param, serialize_param, schema, deferred_dgettext,
@@ -17,123 +18,116 @@ from pycroft.helpers.interval import (
     UnboundedInterval, closed, closedopen, openclosed, open)
 
 
-class TestParameterSerialization(unittest.TestCase):
-    def assertValidSerialization(self, param):
-        s = serialize_param(param)
-        try:
-            json.dumps(s)
-        except (ValueError, TypeError):
-            self.fail("Param {} cannot be serialized to JSON.".format(param))
-        self.assertEqual(deserialize_param(s), param)
-
-    def test_serialize_string(self):
-        self.assertValidSerialization("test")
-
-    def test_serialize_unicode(self):
-        self.assertValidSerialization(u"test")
-
-    def test_serialize_bool(self):
-        self.assertValidSerialization(True)
-
-    def test_serialize_int(self):
-        self.assertValidSerialization(42)
-
-    def test_serialize_float(self):
-        self.assertValidSerialization(0.5)
-
-    def test_serialize_decimal(self):
-        self.assertValidSerialization(Decimal('3.8e2'))
-
-    def test_serialize_money(self):
-        self.assertValidSerialization(Money(Decimal(20.0), "EUR"))
-
-    def test_serialize_datetime(self):
-        self.assertValidSerialization(datetime.datetime.utcnow())
-
-    def test_serialize_date(self):
-        self.assertValidSerialization(datetime.date.today())
-
-    def test_serialize_timedelta(self):
-        self.assertValidSerialization(datetime.timedelta(1))
-
-    def test_serialize_time(self):
-        self.assertValidSerialization(datetime.datetime.utcnow().time())
-
-    def test_serialize_interval(self):
-        self.assertValidSerialization(UnboundedInterval)
-        now = datetime.datetime.utcnow()
-        then = now + datetime.timedelta(1)
-        self.assertValidSerialization(closed(now, then))
-        self.assertValidSerialization(closedopen(now, then))
-        self.assertValidSerialization(openclosed(now, then))
-        self.assertValidSerialization(open(now, then))
-
-    def test_serialize_unknown_type(self):
-        self.assertRaises(TypeError, serialize_param, object())
+now = datetime.datetime.utcnow()
+then = now + datetime.timedelta(days=1)
 
 
-class DeferredMessageTestCase(TestCase):
-    validator = jsonschema.Draft4Validator(schema)
-
-    def assertValidJSON(self, json_string):
-        try:
-            obj = json.loads(json_string)
-        except (ValueError, TypeError):
-            self.fail()
-        try:
-            self.validator.validate(obj)
-        except jsonschema.ValidationError as e:
-            self.fail("Export failed schema validation: {}".format(e))
-
-    def assertMessageEquals(self, m, domain, args, kwargs, expected_result):
-        self.assertEqual(m.domain, domain)
-        self.assertEqual(m.args, args)
-        self.assertEqual(m.kwargs, kwargs)
-        self.assertEqual(m.localize(), expected_result)
-
-    def assertSimpleMessageCorrect(self, m, message, domain, args, kwargs,
-                                   expected_result):
-        self.assertIsInstance(m, SimpleMessage)
-        self.assertEqual(m.message, message)
-        self.assertMessageEquals(m, domain, args, kwargs, expected_result)
-        json_string = m.to_json()
-        self.assertValidJSON(json_string)
-        m2 = Message.from_json(json_string)
-        self.assertIsInstance(m2, SimpleMessage)
-        self.assertMessageEquals(m2, domain, args, kwargs, expected_result)
-
-    def assertNumericMessageCorrect(self, m, singular, plural, n, domain,
-                                    args, kwargs, expected_result):
-        self.assertIsInstance(m, NumericalMessage)
-        self.assertEqual(m.singular, singular)
-        self.assertEqual(m.plural, plural)
-        self.assertEqual(m.n, n)
-        self.assertMessageEquals(m, domain, args, kwargs, expected_result)
-        json_string = m.to_json()
-        self.assertValidJSON(json_string)
-        m2 = Message.from_json(json_string)
-        self.assertIsInstance(m2, NumericalMessage)
-        self.assertMessageEquals(m2, domain, args, kwargs, expected_result)
+@pytest.mark.parametrize('value', [
+    "test",
+    True,
+    42,
+    0.5,
+    Decimal('3.8e2'),
+    Money(Decimal(20.0), "EUR"),
+    datetime.datetime.utcnow(),
+    datetime.date.today(),
+    datetime.timedelta(1),
+    datetime.datetime.utcnow().time(),
+    UnboundedInterval,
+    closed(now, then),
+    closedopen(now, then),
+    openclosed(now, then),
+    open(now, then),
+])
+def test_valid_serialization(value):
+    s = serialize_param(value)
+    try:
+        json.dumps(s)
+    except (ValueError, TypeError):
+        pytest.fail(f"Param {value} cannot be serialized to JSON.")
+    assert deserialize_param(s) == value
 
 
-class TestJSONExport(DeferredMessageTestCase):
-    def test_invalid_json(self):
-        self.assertIsInstance(Message.from_json("not JSON"), ErroneousMessage)
+def test_serialize_unknown_type():
+    with pytest.raises(TypeError):
+        serialize_param(object())
 
-    def test_wrong_json(self):
-        json_string = json.dumps({"key": "value"})
-        self.assertIsInstance(Message.from_json(json_string), ErroneousMessage)
 
+def assertMessageEquals(m, domain, args, kwargs, expected_result):
+    assert m.domain == domain
+    assert m.args == args
+    assert m.kwargs == kwargs
+    assert m.localize() == expected_result
+
+
+validator = jsonschema.Draft4Validator(schema)
+
+
+def assertValidJSON(json_string):
+    try:
+        obj = json.loads(json_string)
+    except (ValueError, TypeError):
+        pytest.fail()
+    try:
+        validator.validate(obj)
+    except jsonschema.ValidationError as e:
+        pytest.fail(f"Export failed schema validation: {e}")
+
+
+def assertSimpleMessageCorrect(m, message, domain, args, kwargs,
+                               expected_result):
+    assert isinstance(m, SimpleMessage)
+    assert m.message == message
+    assertMessageEquals(m, domain, args, kwargs, expected_result)
+    json_string = m.to_json()
+    assertValidJSON(json_string)
+    m2 = Message.from_json(json_string)
+    assert isinstance(m2, SimpleMessage)
+    assertMessageEquals(m2, domain, args, kwargs, expected_result)
+
+
+def assertNumericMessageCorrect(m, singular, plural, n, domain,
+                                args, kwargs, expected_result):
+    assert isinstance(m, NumericalMessage)
+    assert m.singular == singular
+    assert m.plural == plural
+    assert m.n == n
+    assertMessageEquals(m, domain, args, kwargs, expected_result)
+    json_string = m.to_json()
+    assertValidJSON(json_string)
+    m2 = Message.from_json(json_string)
+    assert isinstance(m2, NumericalMessage)
+    assertMessageEquals(m2, domain, args, kwargs, expected_result)
+
+
+@pytest.mark.parametrize('json', [
+    "not JSON",
+    '{"key": "value"}',
+])
+def test_erroneous_json(json: str):
+    assert isinstance(Message.from_json("not JSON"), ErroneousMessage)
+
+
+def get_format_error_message(message, args, kwargs):
+    try:
+        message.format(*args, **kwargs)
+    except (TypeError, ValueError, IndexError, KeyError) as e:
+        return u''.join(traceback.format_exception_only(type(e), e))
+    else:
+        raise AssertionError()
+
+
+class TestSimpleMessages:
     def test_simple(self):
         message = "test"
         m = deferred_gettext(message)
-        self.assertSimpleMessageCorrect(m, message, None, (), {}, message)
+        assertSimpleMessageCorrect(m, message, None, (), {}, message)
 
     def test_simple_with_domain(self):
         message = "test"
         domain = "domain"
         m = deferred_dgettext(domain, message)
-        self.assertSimpleMessageCorrect(m, message, domain, (), {}, message)
+        assertSimpleMessageCorrect(m, message, domain, (), {}, message)
 
     def test_simple_format_args(self):
         message = "test {} at {}"
@@ -141,8 +135,8 @@ class TestJSONExport(DeferredMessageTestCase):
         arg2 = datetime.datetime.utcnow()
         m = deferred_gettext(message).format(arg1, arg2)
         expected_result = message.format(arg1, format_datetime(arg2))
-        self.assertSimpleMessageCorrect(m, message, None, (arg1, arg2), {},
-                                        expected_result)
+        assertSimpleMessageCorrect(m, message,
+                                   None, (arg1, arg2), {}, expected_result)
 
     def test_simple_format_kwargs(self):
         message = "test {arg1} at {arg2}"
@@ -150,17 +144,17 @@ class TestJSONExport(DeferredMessageTestCase):
         arg2 = datetime.datetime.utcnow()
         m = deferred_gettext(message).format(arg1=arg1, arg2=arg2)
         expected_result = message.format(arg1=arg1, arg2=format_datetime(arg2))
-        self.assertSimpleMessageCorrect(m, message, None, (),
-                                        {"arg1": arg1, "arg2": arg2},
-                                        expected_result)
+        assertSimpleMessageCorrect(m, message,
+                                   None, (), {"arg1": arg1, "arg2": arg2}, expected_result)
 
+
+class TestNumericMessages:
     def test_singular(self):
         singular = "singular"
         plural = "plural"
         n = 1
         m = deferred_ngettext(singular, plural, n)
-        self.assertNumericMessageCorrect(m, singular, plural, n, None, (), {},
-                                         singular)
+        assertNumericMessageCorrect(m, singular, plural, n, None, (), {}, singular)
 
     def test_singular_domain(self):
         singular = "singular"
@@ -168,8 +162,7 @@ class TestJSONExport(DeferredMessageTestCase):
         n = 1
         domain = "domain"
         m = deferred_dngettext(domain, singular, plural, n)
-        self.assertNumericMessageCorrect(m, singular, plural, n, domain, (), {},
-                                         singular)
+        assertNumericMessageCorrect(m, singular, plural, n, domain, (), {}, singular)
 
     def test_plural(self):
         singular = "singular"
@@ -177,33 +170,26 @@ class TestJSONExport(DeferredMessageTestCase):
         n = 1000
         domain = "domain"
         m = deferred_dngettext(domain, singular, plural, n)
-        self.assertNumericMessageCorrect(m, singular, plural, n, domain, (), {},
-                                         plural)
+        assertNumericMessageCorrect(m, singular, plural, n, domain, (), {}, plural)
 
-    def get_format_error_message(self, message, args, kwargs):
-        try:
-            message.format(*args, **kwargs)
-        except (TypeError, ValueError, IndexError, KeyError) as e:
-            return u''.join(traceback.format_exception_only(type(e), e))
-        else:
-            raise AssertionError()
 
+class TestErrorFormatting:
     def test_missing_positional_argument(self):
         message = u"{0} {1}"
         args = (1,)
         kwargs = {}
-        error = self.get_format_error_message(message, args, kwargs)
+        error = get_format_error_message(message, args, kwargs)
         m = deferred_gettext(message).format(*args)
         text = (u'Could not format message "{}" (args={}, kwargs={}): {}'
                 .format(message, args, kwargs, error))
-        self.assertSimpleMessageCorrect(m, message, None, args, kwargs, text)
+        assertSimpleMessageCorrect(m, message, None, args, kwargs, text)
 
     def test_missing_keyword_argument(self):
         message = u"{foo}"
         args = (1,)
         kwargs = {}
-        error = self.get_format_error_message(message, args, kwargs)
+        error = get_format_error_message(message, args, kwargs)
         m = deferred_gettext(message).format(*args)
         text = (u'Could not format message "{}" (args={}, kwargs={}): {}'
                 .format(message, args, kwargs, error))
-        self.assertSimpleMessageCorrect(m, message, None, args, kwargs, text)
+        assertSimpleMessageCorrect(m, message, None, args, kwargs, text)
