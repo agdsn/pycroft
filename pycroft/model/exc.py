774a6f49f24c848c1c258b64aa3b843e52b04662
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-08-06 17:43:25 +0200

    Add missing file
    This is embarrassing and purely to blame on my haste.

---
+++ pycroft/model/exc.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2021. The Pycroft Authors. See the AUTHORS file.
+#  This file is part of the Pycroft project and licensed under the terms of
+#  the Apache License, Version 2.0. See the LICENSE file for details
+from pycroft.exc import PycroftException
+
+
+class PycroftModelException(PycroftException):
+    pass
