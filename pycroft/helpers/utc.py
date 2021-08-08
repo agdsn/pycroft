7bd67ef1c960c26c9ac48c23c1d71f60eded1d54
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-08-08 17:41:22 +0200

    Introduce static types like `DateTimeTz` and annotate `utc` helpers

--- pycroft/helpers/utc.py
+++ pycroft/helpers/utc.py
@@ -1,17 +1,23 @@
-from datetime import datetime, time, timezone
+from typing import NewType
+from datetime import datetime, time, timezone, date
 
 
-def time_min():
+TimeTz = NewType('TimeTz', time)
+DateTimeTz = NewType('DateTimeTz', datetime)
+DateTimeNoTz = NewType('DateTimeNoTz', datetime)
+
+
+def time_min() -> TimeTz:
     return time.min.replace(tzinfo=timezone.utc)
 
 
-def time_max():
+def time_max() -> TimeTz:
     return time.max.replace(tzinfo=timezone.utc)
 
 
-def datetime_min():
+def datetime_min() -> DateTimeTz:
     return datetime.min.replace(tzinfo=timezone.utc)
 
 
-def datetime_max():
+def datetime_max() -> DateTimeTz:
     return datetime.max.replace(tzinfo=timezone.utc)
