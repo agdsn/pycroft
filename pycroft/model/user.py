7028f25463b0114dbb0db515dee6baca18c1bb41
Lukas Juhrich <lukas.juhrich@agdsn.de>
2021-08-29 18:17:54 +0200

    Make `begins_at`/`ends_at` shims actually compatible
    The previous columns returned the underlying `Optional[datetime]`, not
    a `Bound`.
    
    Refs #160

--- pycroft/model/user.py
+++ pycroft/model/user.py
@@ -12,7 +12,7 @@
 """
 from __future__ import annotations
 import re
-from datetime import timedelta, date
+from datetime import timedelta, date, datetime
 from typing import Optional, List, Set
 
 from flask_login import UserMixin
@@ -28,7 +28,8 @@ from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.util import has_identity
 from sqlalchemy.sql import true, false
 
-from pycroft.helpers.interval import closed, single, Interval, Bound, closedopen
+from pycroft.helpers.interval import closed, single, Interval, closedopen, \
+    PositiveInfinity, NegativeInfinity
 from pycroft.helpers.user import hash_password, verify_password, cleartext_password, \
     clear_password_prefix
 from pycroft.model import session, ddl
@@ -495,12 +496,22 @@ class Membership(IntegerIdModel):
     active_during: Interval = Column(TsTzRange, nullable=False, index=True)
 
     @property
-    def begins_at(self) -> Bound:
-        return self.active_during.lower_bound
+    def begins_at(self) -> Optional[datetime]:
+        import warnings
+        warnings.warn("Compatability boilerplate. Use `active_during`.")
+        bound = self.active_during.lower_bound
+        if bound.value is NegativeInfinity:
+            return None
+        return bound.value
 
     @property
-    def ends_at(self) -> Bound:
-        return self.active_during.lower_bound
+    def ends_at(self) -> Optional[datetime]:
+        import warnings
+        warnings.warn("Compatability boilerplate. Use `active_during`.")
+        bound = self.active_during.upper_bound
+        if bound.value is PositiveInfinity:
+            return None
+        return bound.value
 
     ##### COMPAT BOILERPLATE
     @hybrid_method
