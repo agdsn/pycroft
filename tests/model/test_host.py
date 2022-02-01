309d199bbc1a8b81dd601e4a9d8bea96132ebf38
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-02-01 05:36:42 +0100

    Migrate `tests.model.test_host`

--- tests/model/test_host.py
+++ tests/model/test_host.py
@@ -4,236 +4,229 @@
 import re
 from itertools import chain
 
+import pytest
 from sqlalchemy import inspect
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.base import object_state
 
 from pycroft.lib.net import get_free_ip
-from pycroft.model import host
+from pycroft.model.host import Interface, IP, switch_port_default_vlans
 from pycroft.model.types import InvalidMACAddressException
-from ..legacy_base import FactoryDataTestBase
 from .. import factories
 
 
-class TestInterfaceValidators(FactoryDataTestBase):
-    mac_regex = re.compile(r"^[a-f0-9]{2}(:[a-f0-9]{2}){5}$")
+class TestInterfaceValidators:
+    @pytest.fixture(scope='class')
+    def host(self, class_session):
+        return factories.HostFactory()
 
-    def create_factories(self):
-        super().create_factories()
-        self.host = factories.HostFactory()
-
-    def assertSetMAC(self, interface, mac):
-        parts = mac.split(":")
-        if len(mac) != 17 or len(parts) != 6:
-            with self.assertRaises(InvalidMACAddressException):
-                interface.mac = mac
-            return
-        if self.mac_regex.match(mac) is None:
-            with self.assertRaises(InvalidMACAddressException):
-                interface.mac = mac
-            return
-        if int(parts[0], base=16) & 1:
-            with self.assertRaises(InvalidMACAddressException):
-                interface.mac = mac
-            return
-        interface.mac = mac
-
-    def test_mac_validation(self):
-        interface = host.Interface(host=self.host)
-        # assert that object is not added to session
-        # (see https://docs.sqlalchemy.org/en/14/orm/session_state_management.html#session-object-states)
+    @pytest.mark.parametrize('mac', [
+        'ff:ff:ff:ff:ff',
+        "ff:ff:ff:ff:ff:ff",
+        "ff:asjfjsdaf:ff:ff:ff:ff",
+        "aj:00:ff:1f:ff:ff",
+        "ff:ff:ff:ff:ff:ff:ff",
+    ])
+    def test_bad_macs(self, session, host, mac):
+        interface = Interface(host=host)
         assert object_state(interface).transient
 
-        # Try some bad macs
-        self.assertSetMAC(interface, "ff:ff:ff:ff:ff")
-        self.assertSetMAC(interface, "ff:ff:ff:ff:ff:ff")
-        self.assertSetMAC(interface, "ff:asjfjsdaf:ff:ff:ff:ff")
-        self.assertSetMAC(interface, "aj:00:ff:1f:ff:ff")
-        self.assertSetMAC(interface, "ff:ff:ff:ff:ff:ff:ff")
+        with pytest.raises(InvalidMACAddressException):
+            interface.mac = mac
+        with pytest.raises(IntegrityError):
+            session.add(interface)
+            session.flush()
 
-        # Assert that we have no mac assigned
-        self.session.add(interface)
-        self.assertRaises(IntegrityError, self.session.commit)
-        self.session.rollback()
+    @pytest.mark.parametrize('mac', [
+        "00:00:00:01:00:00",
+    ])
+    def test_good_macs(self, session, host, mac):
+        interface = Interface(host=host)
+        assert object_state(interface).transient
 
-        # Assert a correct mac
-        self.assertSetMAC(interface, "00:00:00:01:00:00")
+        interface.mac = mac
+        session.add(interface)
+        session.flush()
 
-        # Assert that we have the mac assigned
-        self.session.add(interface)
-        self.session.commit()
 
-        # Wipe the instance
-        self.session.delete(interface)
-        self.session.commit()
+class TestIpModel:
+    @pytest.fixture(scope='class')
+    def subnet(self, class_session):
+        return factories.SubnetFactory.create()
 
-class IpModelTestBase(FactoryDataTestBase):
-    def create_factories(self):
-        super().create_factories()
-        self.subnets = factories.SubnetFactory.create_batch(5)
-        self.subnet = self.subnets[0]
-        self.interface = factories.InterfaceFactory()
+    @pytest.fixture(scope='class')
+    def subnet2(self, class_session):
+        return factories.SubnetFactory.create()
 
-    def pick_ip(self):
-        ip, _ = get_free_ip((self.subnet,))
-        addr = host.IP(interface=self.interface, address=ip, subnet=self.subnet)
-        self.session.add(addr)
-        self.session.commit()
+    @pytest.fixture(scope='class')
+    def interface(self, class_session):
+        return factories.InterfaceFactory()
+
+    @pytest.fixture(scope='class')
+    def ip_addr(self, class_session, subnet, interface):
+        ip, _ = get_free_ip((subnet,))
+        addr = IP(interface=interface, address=ip, subnet=subnet)
+        class_session.add(addr)
         return addr
 
-
-class TestIpModel(IpModelTestBase):
-    def test_delete_address(self):
-        ip_addr = self.pick_ip()
-
-        with self.assertRaises(IntegrityError):
+    def test_delete_address(self, session, ip_addr):
+        with pytest.raises(IntegrityError):
             ip_addr.address = None
             assert ip_addr.address is None
-            self.session.commit()
+            session.flush()
 
-    def test_delete_subnet(self):
-        ip_addr = self.pick_ip()
-
-        with self.assertRaises(IntegrityError):
+    def test_delete_subnet(self, session, ip_addr):
+        with pytest.raises(IntegrityError):
             ip_addr.subnet = None
             assert ip_addr.subnet is None
-            self.session.commit()
+            session.flush()
 
+    def test_correct_subnet_and_ip(self, session, subnet, interface):
+        ip_address, _ = get_free_ip((subnet, ))
 
-class TestIpEvents(IpModelTestBase):
-    def test_correct_subnet_and_ip(self):
-        ip_address, _ = get_free_ip((self.subnet, ))
+        ip = IP(interface=interface, address=ip_address, subnet=subnet)
+        with session.begin_nested():
+            session.add(ip)
 
-        ip = host.IP(interface=self.interface, address=ip_address, subnet=self.subnet)
-        self.session.add(ip)
-        self.session.commit()
+        ip_address, _ = get_free_ip((subnet, ))
+        ip = IP(address=ip_address, subnet=subnet, interface=interface)
+        with session.begin_nested():
+            session.add(ip)
 
-        ip_address, _ = get_free_ip((self.subnet, ))
-        ip = host.IP(address=ip_address, subnet=self.subnet, interface=self.interface)
-        self.session.add(ip)
-        self.session.commit()
+        with session.begin_nested():
+            IP.q.filter(IP.interface == interface).delete()
 
-        host.IP.q.filter(host.IP.interface == self.interface).delete()
-        self.session.commit()
+    def test_missing_subnet(self, session, interface, subnet):
+        ip_address, _ = get_free_ip((subnet, ))
+        ip = IP(interface=interface, address=ip_address)
+        with pytest.raises(IntegrityError), session.begin_nested():
+                session.add(ip)
 
-    def test_missing_subnet(self):
-        ip_address, _ = get_free_ip((self.subnet, ))
-        ip = host.IP(interface=self.interface, address=ip_address)
+    def test_missing_ip(self, session, interface, subnet):
+        ip = IP(interface=interface, subnet=subnet)
+        with pytest.raises(IntegrityError), session.begin_nested():
+            session.add(ip)
 
-        with self.assertRaises(IntegrityError):
-            self.session.add(ip)
-            self.session.commit()
+    def test_wrong_subnet(self, interface, subnet, subnet2):
+        ip_address, _ = get_free_ip((subnet, ))
 
-    def test_missing_ip(self):
-        ip = host.IP(interface=self.interface, subnet=self.subnet)
+        ip = IP(interface=interface, address=ip_address)
 
-        with self.assertRaises(IntegrityError):
-            self.session.add(ip)
-            self.session.commit()
+        with pytest.raises(ValueError):
+            ip.subnet = subnet2
 
-    def test_wrong_subnet(self):
-        ip_address, _ = get_free_ip((self.subnets[0], ))
+        ip = IP(interface=interface, subnet=subnet2)
 
-        ip = host.IP(interface=self.interface, address=ip_address)
-
-        with self.assertRaises(ValueError):
-            ip.subnet = self.subnets[1]
-
-        ip = host.IP(interface=self.interface, subnet=self.subnets[1])
-
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ip.address = ip_address
 
-        with self.assertRaises(ValueError):
-            host.IP(interface=self.interface, subnet=self.subnets[1], address=ip_address)
+        with pytest.raises(ValueError):
+            IP(interface=interface, subnet=subnet2, address=ip_address)
 
 
-class TestVariousCascades(FactoryDataTestBase):
-    def create_factories(self):
-        super().create_factories()
-        self.user = factories.UserWithHostFactory()
-        self.host = self.user.hosts[0]
-        self.interface = self.host.interfaces[0]
-        ips = factories.IPFactory.create_batch(3, interface=self.interface)
+class TestVariousCascades:
+    @pytest.fixture(scope='class')
+    def user(self, class_session):
+        user = factories.UserWithHostFactory.build()
+        class_session.add(user)
+        return user
+
+    @pytest.fixture(scope='class')
+    def host(self, user):
+        return user.hosts[0]
+
+    @pytest.fixture(scope='class')
+    def interface(self, host):
+        return host.interfaces[0]
+
+    @pytest.fixture(scope='class')
+    def ips(self, class_session, interface):
+        ips = factories.IPFactory.create_batch(3, interface=interface)
         # there's probably a better way to do this, e.g. by introducing an `IpWithTrafficFactory`
         for ip in ips:
             factories.TrafficVolumeFactory.create_batch(4, ip=ip)
-        self.ip = self.interface.ips[0]
+        return ips
 
+    @pytest.fixture(scope='class')
+    def ip(self, ips):
+        return ips[0]
 
-    def test_traffic_volume_cascade_on_delete_ip(self):
-        test_ip = self.ip
+    def test_traffic_volume_cascade_on_delete_ip(self, ip, session):
+        test_ip = ip
         tv_of_test_ip = test_ip.traffic_volumes
-        self.session.delete(test_ip)
-        self.session.commit()
+        with session.begin_nested():
+            session.delete(test_ip)
         assert all(inspect(o).was_deleted for o in tv_of_test_ip)
 
-    def test_traffic_volume_cascade_on_delete_interface(self):
-        test_interface = self.interface
-        ips = test_interface.ips
+    def test_traffic_volume_cascade_on_delete_interface(self, session, user, interface, ips):
         traffic_volumes = tuple(chain(*(ip.traffic_volumes for ip in ips)))
-        self.session.delete(test_interface)
-        self.session.commit()
+        with session.begin_nested():
+            session.delete(interface)
         assert all(inspect(o).was_deleted for o in chain(ips, traffic_volumes))
 
-    def test_traffic_volume_cascade_on_delete_host(self):
-        test_host = self.host
+    def test_traffic_volume_cascade_on_delete_host(self, session, host):
+        test_host = host
         interfaces = test_host.interfaces
         ips = tuple(chain(*(d.ips for d in interfaces)))
         traffic_volumes = tuple(chain(*(ip.traffic_volumes for ip in ips)))
-        self.session.delete(test_host)
-        self.session.commit()
+        with session.begin_nested():
+            session.delete(test_host)
         assert all(inspect(o).was_deleted for o in chain(interfaces, ips, traffic_volumes))
 
-    def test_all_cascades_on_delete_user(self):
+    def test_all_cascades_on_delete_user(self, session, user):
         """Test that hosts, interfaces, ips, and traffic_volumes of a host are cascade deleted"""
-        test_user = self.user
+        test_user = user
         hosts = test_user.hosts
         interfaces = tuple(chain(*(h.interfaces for h in hosts)))
         ips = tuple(chain(*(d.ips for d in interfaces)))
         traffic_volumes = tuple(chain(*(ip.traffic_volumes for ip in ips)))
-        self.session.delete(test_user)
-        self.session.commit()
+        with session.begin_nested():
+            session.delete(test_user)
         assert all(inspect(o).was_deleted for o in chain(hosts, interfaces, ips, traffic_volumes))
 
 
-class TestDefaultVlanCascades(FactoryDataTestBase):
-    def create_factories(self):
-        super().create_factories()
-        # We need: SwitchPort <- `switch_port_default_vlans` -> Vlan
-        self.vlans = factories.VLANFactory.create_batch(2)
-        self.vlan = self.vlans[0]
-        self.ports = factories.SwitchPortFactory.create_batch(2, default_vlans=self.vlans)
-        self.port = self.ports[0]
+class TestDefaultVlanCascades:
+    @pytest.fixture(scope='class')
+    def vlans(self, class_session):
+        return factories.VLANFactory.create_batch(2)
 
-    def test_default_vlan_associations_cascade_on_delete_vlan(self):
-        associations_query = self.session.query(host.switch_port_default_vlans)\
-            .filter_by(vlan_id=self.vlan.id)
+    @pytest.fixture(scope='class')
+    def vlan(self, vlans):
+        return vlans[0]
 
-        assert associations_query.count() == 2
-        for subnet in self.vlan.subnets:
-            self.session.delete(subnet)
-        self.session.delete(self.vlan)
-        self.session.commit()
-        assert associations_query.count() == 0
+    # autouse because this provides the `default_vlans`
+    @pytest.fixture(scope='class', autouse=True)
+    def ports(self, vlans):
+        return factories.SwitchPortFactory.create_batch(2, default_vlans=vlans)
 
-    def test_default_vlan_associations_cascade_on_delete_switch_port(self):
-        associations_query = self.session.query(host.switch_port_default_vlans)\
-            .filter_by(switch_port_id=self.port.id)
+    @pytest.fixture(scope='class')
+    def port(self, ports):
+        return ports[0]
+
+    def test_default_vlan_associations_cascade_on_delete_vlan(self, session, vlan):
+        associations_query = session.query(switch_port_default_vlans)\
+            .filter_by(vlan_id=vlan.id)
 
         assert associations_query.count() == 2
-        self.session.delete(self.port)
-        self.session.commit()
+        for subnet in vlan.subnets:
+            session.delete(subnet)
+        session.delete(vlan)
+        assert associations_query.count() == 0
+
+    def test_default_vlan_associations_cascade_on_delete_switch_port(self, session, port):
+        associations_query = session.query(switch_port_default_vlans)\
+            .filter_by(switch_port_id=port.id)
+
+        assert associations_query.count() == 2
+        session.delete(port)
+        session.flush()
         assert associations_query.count() == 0
 
 
-class TestVLANAssociations(FactoryDataTestBase):
-    def create_factories(self):
-        super().create_factories()
-        self.vlans = factories.VLANFactory.create_batch(2)
-        self.port1 = factories.SwitchPortFactory(default_vlans=self.vlans[:1])
-        self.port2 = factories.SwitchPortFactory(default_vlans=self.vlans)
-
-    def test_secondary_relationship_works(self):
-        assert len(self.port1.default_vlans) == 1
-        assert len(self.port2.default_vlans) == 2
+class TestVLANAssociations:
+    def test_secondary_relationship_works(self, session):
+        vlans = factories.VLANFactory.create_batch(2)
+        port1 = factories.SwitchPortFactory(default_vlans=vlans[:1])
+        assert len(port1.default_vlans) == 1
+        port2 = factories.SwitchPortFactory(default_vlans=vlans)
+        assert len(port2.default_vlans) == 2
