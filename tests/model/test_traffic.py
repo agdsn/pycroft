a9118764bc7131beb8006e19b74e622b9249073f
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-02-06 20:21:25 +0100

    migrate `tests.model.test_traffic` to pytest

--- tests/model/test_traffic.py
+++ tests/model/test_traffic.py
@@ -1,53 +1,54 @@
-from pycroft.model import session
+import pytest
+
 from pycroft.model.traffic import TrafficVolume, pmacct_traffic_egress, \
     pmacct_traffic_ingress
-from tests.legacy_base import FactoryDataTestBase
 from tests.factories import UserWithHostFactory
 
+ip = '141.30.228.39'
+bad_ip = '141.30.228.1'
+egress_table = pmacct_traffic_egress.table
+ingress_table = pmacct_traffic_ingress.table
 
-class PMAcctViewTest(FactoryDataTestBase):
-    ip = '141.30.228.39'
-    bad_ip = '141.30.228.1'
-    egress_table = pmacct_traffic_egress.table
-    ingress_table = pmacct_traffic_ingress.table
-    def create_factories(self):
-        self.user = UserWithHostFactory(host__interface__ip__str_address=self.ip)
 
-    def build_insert(self, type, **kwargs):
-        if type == 'Ingress':
-            table = self.ingress_table
-            ip_key = 'ip_dst'
-        elif type == 'Egress':
-            table = self.egress_table
-            ip_key = 'ip_src'
-        else:
-            raise ValueError("type must be one of 'Ingress', 'Egress'")
-        stamp = session.utcnow()
-        values = {
-            'bytes': 1024,
-            'packets': 20,
-            ip_key: self.ip,
-            'stamp_inserted': stamp,
-            'stamp_updated': stamp,
-        }
+@pytest.fixture(scope='module')
+def user(module_session):
+    return UserWithHostFactory(host__interface__ip__str_address=ip)
 
-        values.update(**kwargs)
-        return table.insert().values(**values)
 
-    def test_egress_insert(self):
-        session.session.execute(self.build_insert(type='Egress'))
+def traffic_insert(stamp, table, ip_key, **kwargs):
+    return table.insert().values(**{
+        'bytes': 1024,
+        'packets': 20,
+        ip_key: ip,
+        'stamp_inserted': stamp,
+        'stamp_updated': stamp,
+        **kwargs
+    })
+
+
+def egress_insert(stamp, **kwargs):
+    return traffic_insert(stamp, table=egress_table, ip_key='ip_src', **kwargs)
+
+
+def ingress_insert(stamp, **kwargs):
+    return traffic_insert(stamp, table=ingress_table, ip_key='ip_dst', **kwargs)
+
+
+class TestPMAcctView:
+    def test_egress_insert(self, user, session, utcnow):
+        session.execute(egress_insert(utcnow))
         assert TrafficVolume.q.count() == 1
         volume = TrafficVolume.q.one()
         assert volume.type == 'Egress'
         assert volume.amount == 1024
         assert volume.packets == 20
-        assert volume.user == self.user
+        assert volume.user == user
 
-    def test_egress_insert_nonexistent_ip(self):
-        session.session.execute(self.build_insert(type='Egress', ip_src="1.1.1.1"))
+    def test_egress_insert_nonexistent_ip(self, session, utcnow):
+        session.execute(egress_insert(utcnow, ip_src="1.1.1.1"))
         assert TrafficVolume.q.count() == 0
 
-    def test_egress_update_successive_entries(self):
+    def test_egress_update_successive_entries(self, session, utcnow):
         data = [
             # timestamp, packets, amount
             ('2018-03-15 00:15:00', 200, 1024),
@@ -55,29 +56,30 @@ class PMAcctViewTest(FactoryDataTestBase):
             ('2018-03-15 23:59:00', 12, 7055),
         ]
         for stamp, packets, bytes in data:
-            session.session.execute(self.build_insert(type='Egress', packets=packets, bytes=bytes,
-                                                      stamp_inserted=stamp, stamp_updated=stamp))
+            session.execute(egress_insert(
+                utcnow, packets=packets, bytes=bytes,
+                stamp_inserted=stamp, stamp_updated=stamp
+            ))
         assert TrafficVolume.q.count() == 1
         vol = TrafficVolume.q.one()
         assert str(vol.timestamp) == '2018-03-15 00:00:00+00:00'
         assert vol.packets == sum(x[1] for x in data)
         assert vol.amount == sum(x[2] for x in data)
 
-
-    def test_ingress_insert(self):
-        session.session.execute(self.build_insert(type='Ingress'))
+    def test_ingress_insert(self, user, session, utcnow):
+        session.execute(ingress_insert(utcnow))
         assert TrafficVolume.q.count() == 1
         volume = TrafficVolume.q.one()
         assert volume.type == 'Ingress'
         assert volume.amount == 1024
         assert volume.packets == 20
-        assert volume.user == self.user
+        assert volume.user == user
 
-    def test_ingress_insert_nonexistent_ip(self):
-        session.session.execute(self.build_insert(type='Ingress', ip_dst="1.1.1.1"))
+    def test_ingress_insert_nonexistent_ip(self, session, utcnow):
+        session.execute(ingress_insert(utcnow, ip_dst="1.1.1.1"))
         assert TrafficVolume.q.count() == 0
 
-    def test_ingress_update_successive_entries(self):
+    def test_ingress_update_successive_entries(self, session, utcnow):
         data = [
             # timestamp, packets, amount
             ('2018-03-15 00:15:00', 200, 1024),
@@ -85,8 +87,10 @@ class PMAcctViewTest(FactoryDataTestBase):
             ('2018-03-15 23:59:00', 12, 7055),
         ]
         for stamp, packets, bytes in data:
-            session.session.execute(self.build_insert(type='Ingress', packets=packets, bytes=bytes,
-                                                      stamp_inserted=stamp, stamp_updated=stamp))
+            session.execute(ingress_insert(
+                utcnow, packets=packets, bytes=bytes,
+                stamp_inserted=stamp, stamp_updated=stamp
+            ))
         assert TrafficVolume.q.count() == 1
         vol = TrafficVolume.q.one()
         assert str(vol.timestamp) == '2018-03-15 00:00:00+00:00'
