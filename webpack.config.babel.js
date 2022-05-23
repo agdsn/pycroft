f311d8849cc3015227bd9c36726728944c7b7bf1
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-05-23 02:40:19 +0200

    Separate runtimechunk
    Including the runtime in `main` does not appear to work anymore.
    We thus separate it completely from the entrypoints.
    
    Refs #424

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -64,7 +64,7 @@ export default {
             }),
         ],
         runtimeChunk: {
-            name: "main",
+            name: "runtime",
         },
         splitChunks: {
             cacheGroups: {
