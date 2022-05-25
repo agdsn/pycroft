0d5998b55b02917ecb8f2d1858ca690c37e7bb69
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-05-25 19:27:51 +0200

    Use `let` and `const` in `balance-chart.js`

--- web/resources/js/balance-chart.js
+++ web/resources/js/balance-chart.js
@@ -7,65 +7,81 @@
 import {de, timeFormat} from './d3locale.js';
 
 d3.selectAll('[data-chart="balance"]').each(function(d, i) {
-  var parent = d3.select(this);
-  var _width = parent.node().getBoundingClientRect().width;
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+  const parent = d3.select(this);
+  const _width = parent.node().getBoundingClientRect().width;
+  const margin = {top: 20, right: 20, bottom: 30, left: 50},
       width = _width - margin.left - margin.right,
       height = 150 - margin.top - margin.bottom;
 
-  var x = d3.time.scale()
+  const x = d3.time.scale()
       .range([0, width]);
 
-  var y = d3.scale.linear()
+  const y = d3.scale.linear()
       .range([height, 0]);
 
-  var xAxis = d3.svg.axis()
+  const xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom")
       .tickFormat(timeFormat);
 
-  var yAxis = d3.svg.axis()
+  const yAxis = d3.svg.axis()
       .scale(y)
       .orient("left")
       .tickFormat(de.numberFormat("$s"));
 
-  var area_pos = d3.svg.area()
-      .x(function(d) { return x(d.valid_on); })
-      .y0(function(d) { return y(0); })
-      .y1(function(d) { return d.balance>0 ? y(d.balance) : y(0);})
+  const area_pos = d3.svg.area()
+      .x(function (d) {
+        return x(d.valid_on);
+      })
+      .y0(function (d) {
+        return y(0);
+      })
+      .y1(function (d) {
+        return d.balance > 0 ? y(d.balance) : y(0);
+      })
       .interpolate("step-after");
 
-  var area_neg = d3.svg.area()
-      .x(function(d) { return x(d.valid_on); })
-      .y0(function(d) { return d.balance<0 ? y(d.balance) : y(0);})
-      .y1(function(d) { return y(0);})
+  const area_neg = d3.svg.area()
+      .x(function (d) {
+        return x(d.valid_on);
+      })
+      .y0(function (d) {
+        return d.balance < 0 ? y(d.balance) : y(0);
+      })
+      .y1(function (d) {
+        return y(0);
+      })
       .interpolate("step-after");
 
-  var line = d3.svg.line()
-      .x(function(d) { return x(d.valid_on); })
-      .y(function(d) { return y(d.balance); })
+  const line = d3.svg.line()
+      .x(function (d) {
+        return x(d.valid_on);
+      })
+      .y(function (d) {
+        return y(d.balance);
+      })
       .interpolate("step-after");
 
-  var svg = parent.append("svg")
-     .attr("viewBox", "0 0 " +
-           (width + margin.left + margin.right) + " " +
-           (height+ margin.top + margin.bottom))
-     .classed("svg-content-responsive", true)
-    .append("g")
+  const svg = parent.append("svg")
+      .attr("viewBox", "0 0 " +
+          (width + margin.left + margin.right) + " " +
+          (height + margin.top + margin.bottom))
+      .classed("svg-content-responsive", true)
+      .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   d3.json(parent.attr("data-url"), function(error, resp) {
     if (error) throw error;
 
-    var data = resp.items;
+    const data = resp.items;
     data.forEach(function(d) {
       d.valid_on = d3.time.format.iso.parse(d.valid_on);
       d.balance = +d.balance/100.; //converts string to number
     });
 
-    var today = new Date();
-    var first = data[0];
-    var last = data[data.length-1];
+    const today = new Date();
+    const first = data[0];
+    const last = data[data.length - 1];
     // 'today' might be earlier than last valid_on though...
     data.push({'balance': last.balance, 'valid_on': today});
     data.splice(0, 0, {'balance': 0, 'valid_on': d3.time.day.offset(first.valid_on, -1)});
