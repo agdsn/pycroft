d43a03de130d5f5b93276f3010aaf5dc88f760e1
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-05-25 22:55:32 +0200

    Use ISO8601 as the datetime string representation
    Refs #83

--- web/resources/js/td-datetimepicker.ts
+++ web/resources/js/td-datetimepicker.ts
@@ -5,11 +5,34 @@
  */
 
 import * as td from "@eonasdan/tempus-dominus";
+import {DateTime} from "@eonasdan/tempus-dominus";
+import Dates from "@eonasdan/tempus-dominus/types/dates";
+import Display from "@eonasdan/tempus-dominus/types/display";
 
 td.DefaultOptions.display ??= {};
 td.DefaultOptions.display.components ??= {};
 td.DefaultOptions.display.components.useTwentyfourHour = true;
 td.DefaultOptions.allowInputToggle = true;
+
+interface TdClasses {
+    TempusDominus: typeof td.TempusDominus,
+    Dates: typeof Dates,
+    Display: typeof Display,
+}
+
+class IsoPlugin {
+    load(option: {}, tdClasses: TdClasses, tdFactory: any) {
+        // See https://getdatepicker.com/6/plugins/ for some general examples
+        tdClasses.Dates.prototype.formatInput = function (date: DateTime) {
+            return date.toISOString();
+        }
+        tdClasses.Dates.prototype.parseInput = function (input: string) {
+            return DateTime.convert(new Date(Date.parse(input)))
+        }
+    }
+}
+
+td.extend(new IsoPlugin(), {});
 
 const selector = 'form [data-role=datetimepicker]';
 
