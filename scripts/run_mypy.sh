7dd3bac05bf44ea7ca7666b135abe49e885fb0fa
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-07-16 07:03:47 +0200

    Check everything that's configured instead of single file in CI

--- scripts/run_mypy.sh
+++ scripts/run_mypy.sh
@@ -2,7 +2,7 @@
 # we don't want errexit
 set -uo pipefail
 
-mypy pycroft/lib/traffic.py | tee mypy_results.log;
+mypy | tee mypy_results.log;
 export mypy_status=$?
 python ./scripts/render_mypy_results.py mypy_results.log
 exit $mypy_status
