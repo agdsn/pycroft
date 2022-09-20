71d1933adeca948cba4d2858977ac55517e89b4f
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-09-20 22:59:17 +0200

    Type hint `lib.traffic` #559

--- pycroft/lib/traffic.py
+++ pycroft/lib/traffic.py
@@ -8,6 +8,7 @@ pycroft.lib.traffic
 This module contains functions concerning network traffic
 
 """
+import typing as t
 from datetime import timedelta
 
 from sqlalchemy import func, select, literal_column
@@ -18,13 +19,29 @@ from pycroft.model.traffic import TrafficVolume
 from pycroft.model.user import User
 
 
-def get_users_with_highest_traffic(days, limit):
-    return session.session.execute(
-        select(User.id, User.name, func.sum(TrafficVolume.amount).label('traffic_for_days'))
-        .select_from(join(User, TrafficVolume, TrafficVolume.user_id == User.id))
-        .where(User.id != 0)
-        .where(TrafficVolume.timestamp >= (session.utcnow() - timedelta(days - 1)).date())
-        .group_by(User.id, User.name)
-        .order_by(literal_column('traffic_for_days').desc())
-        .limit(limit)
-    ).fetchall()
+class UserTrafficInfo(t.Protocol):
+    id: int
+    name: str
+    traffic_for_days: int
+
+
+def get_users_with_highest_traffic(days: int, limit: int) -> list[UserTrafficInfo]:
+    return t.cast(
+        list[UserTrafficInfo],
+        session.session.execute(
+            select(
+                User.id,
+                User.name,
+                func.sum(TrafficVolume.amount).label("traffic_for_days"),
+            )
+            .select_from(join(User, TrafficVolume, TrafficVolume.user_id == User.id))
+            .where(User.id != 0)
+            .where(
+                TrafficVolume.timestamp
+                >= (session.utcnow() - timedelta(days - 1)).date()
+            )
+            .group_by(User.id, User.name)
+            .order_by(literal_column("traffic_for_days").desc())
+            .limit(limit)
+        ).fetchall(),
+    )
