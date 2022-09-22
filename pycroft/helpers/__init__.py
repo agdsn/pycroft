e23f3bf0ef4425c8c5f88af3df02859734b68a4d
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-09-22 18:46:29 +0200

    Forbid dependency pycroft.helpers → pycroft.model

--- pycroft/helpers/__init__.py
+++ pycroft/helpers/__init__.py
@@ -2,5 +2,7 @@
 # This file is part of the Pycroft project and licensed under the terms of
 # the Apache License, Version 2.0. See the LICENSE file for details.
 """
-This package contains different helper modules
+This package contains certain parts of the business logic which do not depend on our model.
+
+This can include logic concerning ip addresses, intervals, internationalization, etc.
 """
