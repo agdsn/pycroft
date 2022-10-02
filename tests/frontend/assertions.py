be3ced37ead671a573b88221c277bef3584ae504
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-10-02 22:28:09 +0200

    [tests] improve usability of `TestClient` assertions
    1. the assertion methods pass kwargs to `open` and returns the
       response
    2. `status` has been renamed to `code` for consistency
    
    Refs #580

--- tests/frontend/assertions.py
+++ tests/frontend/assertions.py
@@ -7,7 +7,7 @@ import typing as t
 import flask.testing
 import jinja2 as j
 import pytest
-from flask import url_for, template_rendered
+from flask import url_for, template_rendered, Response
 
 
 class TestClient(flask.testing.FlaskClient):
@@ -20,46 +20,57 @@ class TestClient(flask.testing.FlaskClient):
         super().__init__(*a, **kw)
         self.test_client = self
 
-    def assert_url_response_code(self, url: str, status: int) -> None:
-        resp = self.get(url)
-        assert resp.status_code == status, \
-            f"Expected url {url} to return {status}, got {resp.status_code}"
+    def assert_url_response_code(
+        self, url: str, code: int, method: str = "GET", **kw
+    ) -> Response:
+        resp = self.open(url, method=method, **kw)
+        assert resp.status_code == code, \
+            f"Expected url {url} to return {code}, got {resp.status_code}"
+        return resp
 
-    def assert_response_code(self, endpoint: str, status: int) -> None:
-        self.assert_url_response_code(url_for(endpoint), status)
+    def assert_response_code(self, endpoint: str, code: int, **kw) -> Response:
+        return self.assert_url_response_code(url_for(endpoint), code, **kw)
 
-    def assert_url_ok(self, url: str):
-        self.assert_url_response_code(url, status=200)
+    def assert_url_ok(self, url: str, **kw) -> Response:
+        return self.assert_url_response_code(url, code=200, **kw)
 
-    def assert_ok(self, endpoint: str):
-        self.assert_response_code(endpoint, status=200)
+    def assert_ok(self, endpoint: str, **kw) -> Response:
+        return self.assert_response_code(endpoint, code=200, **kw)
 
-
-    def assert_url_redirects(self, url: str, expected_location: str | None = None):
-        resp = self.get(url)
+    def assert_url_redirects(
+        self, url: str, expected_location: str | None = None, method: str = "GET", **kw
+    ) -> Response:
+        resp = self.open(url, method=method, **kw)
         assert 300 <= resp.status_code < 400, \
             f"Expected {url!r} to redirect, got status {resp.status}"
-        if expected_location is None:
-            return
-        assert resp.location == expected_location
+        if expected_location is not None:
+            assert resp.location == expected_location
+        return resp
 
-    def assert_redirects(self, endpoint: str, expected_location: str | None = None):
-        resp = self.get(url_for(endpoint))
+    def assert_redirects(
+        self,
+        endpoint: str,
+        expected_location: str | None = None,
+        method: str = "GET",
+        **kw,
+    ) -> Response:
+        resp = self.open(url_for(endpoint), method=method, **kw)
         assert 300 <= resp.status_code < 400, \
             f"Expected endpoint {endpoint} to redirect, got status {resp.status}"
-        if expected_location is None:
-            return
-        assert resp.location == expected_location
+        if expected_location is not None:
+            assert resp.location == expected_location
+        return resp
 
-    def assert_url_forbidden(self, url: str):
-        resp = self.get(url)
+    def assert_url_forbidden(self, url: str, method: str = "GET", **kw) -> Response:
+        resp = self.open(url, method=method, **kw)
         status = resp.status_code
         assert (
             status == 403
         ), f"Access to {url} expected to be forbidden, got status {status}"
+        return resp
 
-    def assert_forbidden(self, endpoint: str):
-        self.assert_url_forbidden(url_for(endpoint))
+    def assert_forbidden(self, endpoint: str, method: str = "GET", **kw) -> Response:
+        return self.assert_url_forbidden(url_for(endpoint), method=method, **kw)
 
     @contextlib.contextmanager
     def renders_template(self, template: str, allow_others: bool = False):
