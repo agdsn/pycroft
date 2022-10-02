9622837a0c8cb72abb1409789f7914b656a9466a
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-10-02 22:30:21 +0200

    [tests] Migrate `test_properties` to pytest
    Refs #580

--- tests/frontend/test_properties.py
+++ tests/frontend/test_properties.py
@@ -1,18 +1,37 @@
+import pytest
 from flask import url_for
 
 from pycroft.model._all import PropertyGroup
-from tests.frontend.legacy_base import FrontendWithAdminTestBase
+from ..factories import UserFactory, AdminPropertyGroupFactory
+from .assertions import TestClient
+from .fixture_helpers import login_context
 
 
-class PropertiesFrontendTestCase(FrontendWithAdminTestBase):
-    def test_property_gets_added(self):
-        url = url_for('properties.property_group_create')
+class TestPropertiesFrontend:
+    @pytest.fixture(scope="class", autouse=True)
+    def admin_logged_in(self, class_session, test_client: TestClient):
+        login = "testadmin2"
+        UserFactory.create(
+            login=login,
+            with_membership=True,
+            membership__group=AdminPropertyGroupFactory.create(),
+            membership__includes_today=True,
+        )
+        class_session.flush()
+        with login_context(test_client=test_client, login=login, password="password"):
+            yield
+
+    def test_property_gets_added(self, test_client: TestClient):
         group_name = "This is my first property group"
         # first time: a redirect
-        response = self.assert_response_code(url, code=302, method='post',
-                                             data={'name': group_name})
+        response = test_client.assert_response_code(
+            "properties.property_group_create",
+            code=302,
+            method="post",
+            data={"name": group_name},
+        )
         assert response.location == url_for('properties.property_groups', _external=True)
-        response = self.assert_response_code(response.location, code=200)
+        response = test_client.assert_url_response_code(response.location, code=200)
 
         content = response.data.decode('utf-8')
         # This actually should be `assert_flashed`
