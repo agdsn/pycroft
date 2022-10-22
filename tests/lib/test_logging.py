c1b424385bfa0f0683a6b897fb863d02d6688180
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-10-22 18:24:16 +0200

    [tests] simplify `test_logging` tests
    Re-fetching from the database is overcomplicated.  Instead, we test
    whether sqlalchemy reports `persistent`.

--- tests/lib/test_logging.py
+++ tests/lib/test_logging.py
@@ -4,20 +4,20 @@
 from datetime import timedelta
 
 import pytest
+from sqlalchemy import inspect
 from sqlalchemy.orm import Session
 
 from pycroft.lib.logging import log_user_event, log_room_event
 from pycroft.model.facilities import Room
-from pycroft.model.logging import RoomLogEntry, LogEntry
 from pycroft.model.user import User
 from tests.factories import UserFactory, RoomFactory
 
 
 def assert_log_entry(log_entry, expected_author, expected_created_at, expected_message):
+    assert inspect(log_entry).persistent
     assert log_entry.message == expected_message
     assert abs(log_entry.created_at - expected_created_at) < timedelta(seconds=5)
     assert log_entry.author == expected_author
-    assert LogEntry.get(log_entry.id) is not None
 
 
 @pytest.fixture(scope="session")
@@ -41,22 +41,9 @@ def test_user_log_entry(session, utcnow, message, user):
     assert_log_entry(user_log_entry, user, utcnow, message)
     assert user_log_entry.user == user
 
-    session.delete(user_log_entry)
-    session.flush()
-    assert LogEntry.get(user_log_entry.id) is None
-
 
 def test_create_room_log_entry(session, utcnow, message, user, room):
     room_log_entry = log_room_event(message=message, author=user, room=room)
 
-    assert RoomLogEntry.get(room_log_entry.id) is not None
-
-    db_room_log_entry = RoomLogEntry.get(room_log_entry.id)
-
-    assert_log_entry(db_room_log_entry, user, utcnow, message)
-    assert db_room_log_entry.room == room
-
-    assert LogEntry.get(db_room_log_entry.id) is not None
-    session.delete(db_room_log_entry)
-    session.flush()
-    assert LogEntry.get(db_room_log_entry.id) is None
+    assert_log_entry(room_log_entry, user, utcnow, message)
+    assert room_log_entry.room == room
