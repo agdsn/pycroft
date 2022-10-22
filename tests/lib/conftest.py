6b12e99baf43b79f933665f28b01ab319f8cb7c7
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-10-22 18:19:45 +0200

    [tests] migrate `test_logging` to pytest
    Refs #580

--- tests/lib/conftest.py
+++ tests/lib/conftest.py
@@ -1,12 +1,17 @@
 #  Copyright (c) 2022. The Pycroft Authors. See the AUTHORS file.
 #  This file is part of the Pycroft project and licensed under the terms of
 #  the Apache License, Version 2.0. See the LICENSE file for details
-
 import pytest
+from sqlalchemy import func
+from sqlalchemy.future import select
 
 from pycroft.model import _all as m
 from tests import factories
 
+
+@pytest.fixture(scope='module')
+def utcnow(module_session):
+    return module_session.scalar(select(func.current_timestamp()))
 
 @pytest.fixture(scope="module")
 def processor(module_session) -> m.User:
--- tests/lib/test_logging.py
+++ tests/lib/test_logging.py
@@ -3,59 +3,60 @@
 # the Apache License, Version 2.0. See the LICENSE file for details.
 from datetime import timedelta
 
+import pytest
+from sqlalchemy.orm import Session
+
 from pycroft.lib.logging import log_user_event, log_room_event
-from pycroft.model import session
+from pycroft.model.facilities import Room
 from pycroft.model.logging import RoomLogEntry, LogEntry
-from tests.legacy_base import FactoryDataTestBase
+from pycroft.model.user import User
 from tests.factories import UserFactory, RoomFactory
 
 
-class LogTestBase(FactoryDataTestBase):
-    message = "test_message"
-
-    def create_factories(self):
-        super().create_factories()
-        self.user = UserFactory.create()
-
-    def assert_log_entry(self, log_entry, expected_author, expected_created_at, expected_message):
-        assert log_entry.message == expected_message
-        assert abs(log_entry.created_at - expected_created_at) < timedelta(seconds=5)
-        assert log_entry.author == expected_author
-        assert LogEntry.get(log_entry.id) is not None
+def assert_log_entry(log_entry, expected_author, expected_created_at, expected_message):
+    assert log_entry.message == expected_message
+    assert abs(log_entry.created_at - expected_created_at) < timedelta(seconds=5)
+    assert log_entry.author == expected_author
+    assert LogEntry.get(log_entry.id) is not None
 
 
-class UserLogEntryTest(LogTestBase):
-    def test_user_log_entry(self):
-        user_log_entry = log_user_event(message=self.message,
-                                        author=self.user,
-                                        user=self.user)
-
-        self.assert_log_entry(user_log_entry, self.user, session.utcnow(), self.message)
-        assert user_log_entry.user == self.user
-
-        session.session.delete(user_log_entry)
-        session.session.commit()
-        assert LogEntry.get(user_log_entry.id) is None
+@pytest.fixture(scope="session")
+def message() -> str:
+    return "test_message"
 
 
-class RoomLogEntryTest(LogTestBase):
-    def create_factories(self):
-        super().create_factories()
-        self.room = RoomFactory.create()
+@pytest.fixture(scope="module")
+def user(module_session: Session) -> User:
+    return UserFactory.create()
 
-    def test_create_room_log_entry(self):
-        room_log_entry = log_room_event(message=self.message,
-                                        author=self.user,
-                                        room=self.room)
 
-        assert RoomLogEntry.get(room_log_entry.id) is not None
+@pytest.fixture(scope="module")
+def room(module_session: Session) -> Room:
+    return RoomFactory.create()
 
-        db_room_log_entry = RoomLogEntry.get(room_log_entry.id)
 
-        self.assert_log_entry(db_room_log_entry, self.user, session.utcnow(), self.message)
-        assert db_room_log_entry.room == self.room
+def test_user_log_entry(session, utcnow, message, user):
+    user_log_entry = log_user_event(message=message, author=user, user=user)
 
-        assert LogEntry.get(db_room_log_entry.id) is not None
-        session.session.delete(db_room_log_entry)
-        session.session.commit()
-        assert LogEntry.get(db_room_log_entry.id) is None
+    assert_log_entry(user_log_entry, user, utcnow, message)
+    assert user_log_entry.user == user
+
+    session.delete(user_log_entry)
+    session.flush()
+    assert LogEntry.get(user_log_entry.id) is None
+
+
+def test_create_room_log_entry(session, utcnow, message, user, room):
+    room_log_entry = log_room_event(message=message, author=user, room=room)
+
+    assert RoomLogEntry.get(room_log_entry.id) is not None
+
+    db_room_log_entry = RoomLogEntry.get(room_log_entry.id)
+
+    assert_log_entry(db_room_log_entry, user, utcnow, message)
+    assert db_room_log_entry.room == room
+
+    assert LogEntry.get(db_room_log_entry.id) is not None
+    session.delete(db_room_log_entry)
+    session.flush()
+    assert LogEntry.get(db_room_log_entry.id) is None
