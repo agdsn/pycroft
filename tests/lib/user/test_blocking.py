3e8cee05b7908d41ee5b18c80c451d2ee485ad7a
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-10-23 15:32:47 +0200

    [tests] simplify `test_blocking` test

--- tests/lib/user/test_blocking.py
+++ tests/lib/user/test_blocking.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 import pytest
 from sqlalchemy.orm import Session
 
-from pycroft import config
 from pycroft.helpers.interval import closedopen
 from pycroft.lib.user import block, unblock
 from pycroft.model.config import Config
@@ -28,36 +27,20 @@ class TestUserBlockingAndUnblocking:
     def _sanity(self, user_to_block, config):
         assert config.violation_group not in user_to_block.active_property_groups()
 
-    @staticmethod
-    def assert_violation_membership(user, config, subinterval=None):
-        if subinterval is None:
-            assert not user.has_property("network_access")
-            assert user.member_of(config.violation_group)
-            return
-
-        assert user.member_of(config.violation_group, when=subinterval)
-
-    @staticmethod
-    def assert_no_violation_membership(user, subinterval=None):
-        if subinterval is None:
-            assert user.has_property("network_access")
-            assert not user.member_of(config.violation_group)
-            return
-
-        assert not user.member_of(config.violation_group, when=subinterval)
-
     def test_deferred_blocking_and_unblocking_works(self, session, utcnow, user_to_block, config):
         u = user_to_block
+        blockage, unblockage = (utcnow + timedelta(days=d) for d in (1, 3))
 
-        blockage = utcnow + timedelta(days=1)
-        unblockage = blockage + timedelta(days=2)
+        # deferred blocking
         blocked_user = block(u, reason="test", processor=u, during=closedopen(blockage, None))
 
         blocked_during = closedopen(blockage, unblockage)
         assert u.latest_log_entry.author == blocked_user
-        self.assert_violation_membership(blocked_user, config, subinterval=blocked_during)
+        assert blocked_user.member_of(config.violation_group, when=blocked_during)
 
+        # deferred unblocking
         unblocked_user = unblock(blocked_user, processor=u, when=unblockage)
 
         assert unblocked_user.log_entries[0].author == unblocked_user
-        self.assert_violation_membership(unblocked_user, config, subinterval=blocked_during)
+        assert unblocked_user.member_of(config.violation_group, when=blocked_during)
+
