31e8d346d9cc883bb89e4f26c62dacec39e94457
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-10-23 16:20:31 +0200

    [tests] migrate `test_create_user` to pytest #580
    Mail tests have been mocked out for portability, as we currenly don't
    test for mails, anyway.

--- tests/lib/user/test_create_user.py
+++ tests/lib/user/test_create_user.py
@@ -1,39 +1,72 @@
-from pycroft.lib import user as UserHelper
+import dataclasses
+from datetime import date
+
+import pytest
+
+from pycroft.lib.user import create_user
 from tests import factories
-from tests.factories import UserFactory
-from ...legacy_base import FactoryDataTestBase
-from . import ExampleUserData
 from .assertions import assert_account_name, assert_membership_groups, assert_logmessage_startswith
 
 
-class UserCreationTest(FactoryDataTestBase):
-    def create_factories(self):
-        super().create_factories()
-        self.config = factories.ConfigFactory()
-        self.room = factories.RoomFactory(level=1, number="1", patched_with_subnet=True)
-        self.processing_user = UserFactory()
+@dataclasses.dataclass
+class UserData:
+    name: str
+    login: str
+    email: str
+    mac: str
+    birthdate: date
 
-    user = ExampleUserData
 
-    def test_user_create(self):
+class TestUserCreation:
+    @pytest.fixture(scope="class")
+    def member_group(self, class_session):
+        return factories.property.MemberPropertyGroupFactory.create()
+
+    @pytest.fixture(scope="class")
+    def room(self, class_session):
+        return factories.RoomFactory.create(patched_with_subnet=True)
+
+    @pytest.fixture(scope="class")
+    def user_data(self) -> UserData:
+        return UserData(
+            name="Hans",
+            login="hans66",
+            email="hans@hans.de",
+            mac="12:11:11:11:11:11",
+            birthdate=date.fromisoformat("1990-01-01"),
+        )
+
+    @pytest.fixture(scope="class")
+    def user_mail_capture(self):
+        # TODO actually test whether mails are sent out correctly instead of mocking
+        # mocking is only done because we don't test for the mails anyway
+        from unittest.mock import patch
+        with patch("pycroft.lib.user.user_send_mails") as p:
+            yield p
+
+    @pytest.fixture(scope="class", autouse=True)
+    def new_user(self, class_session, user_data, room, processor, member_group, user_mail_capture):
+        new_user, _ = create_user(
+            user_data.name,
+            user_data.login,
+            user_data.email,
+            user_data.birthdate,
+            processor=processor,
+            groups=(member_group,),
+            address=room.address,
+        )
+        return new_user
+
+    def test_user_create(self, new_user, user_data, member_group, user_mail_capture):
         # needs: new_user, self.user (the initiating data),
         # self.config.member_group
-        new_user, _ = UserHelper.create_user(
-            self.user.name,
-            self.user.login,
-            self.user.email,
-            self.user.birthdate,
-            processor=self.processing_user,
-            groups=[self.config.member_group],
-            address=self.room.address,
-        )
-        assert new_user.name == self.user.name
-        assert new_user.login == self.user.login
-        assert new_user.email == self.user.email
+        assert new_user.name == user_data.name
+        assert new_user.login == user_data.login
+        assert new_user.email == user_data.email
         # TODO fix signature and check for explicitly supplied address.
         # assert new_user.address == config.dummy_address
         assert_account_name(new_user.account, f"User {new_user.id}")
-        assert_membership_groups(new_user.active_memberships(), [self.config.member_group])
+        assert_membership_groups(new_user.active_memberships(), [member_group])
         assert new_user.unix_account.home_directory == f"/home/{new_user.login}"
         assert len(new_user.log_entries) == 2
         first, second = new_user.log_entries
@@ -41,3 +74,4 @@ class UserCreationTest(FactoryDataTestBase):
         assert_logmessage_startswith(second, "User created")
         assert new_user.account is not None
         assert new_user.account.balance == 0
+        user_mail_capture.assert_called()
