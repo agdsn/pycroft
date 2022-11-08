c50a0f11c288b771193b1ea4f67bf8355aab85db
Lukas Juhrich <lukas.juhrich@agdsn.de>
2022-11-08 09:09:53 +0100

    [tests] Migrate `test_edits` to pytest #580

--- tests/lib/user/test_edits.py
+++ tests/lib/user/test_edits.py
@@ -1,21 +1,24 @@
-from pycroft.lib import user as UserHelper
+import pytest
+
+from pycroft.lib import user as lib_user
+from pycroft.model.user import User
 from tests.factories import UserFactory
-from tests.legacy_base import FactoryDataTestBase
 
 
-class UserEditsTestCase(FactoryDataTestBase):
-    def create_factories(self):
-        super().create_factories()
-        self.user = UserFactory()
+@pytest.fixture(scope="module")
+def user(module_session) -> User:
+    return UserFactory.create()
 
-    def test_correct_new_name(self):
-        new_name = "A new name"
-        assert new_name != self.user.name
-        UserHelper.edit_name(self.user, new_name, self.user)
-        assert self.user.name == new_name
 
-    def test_correct_new_email(self):
-        new_mail = "user@example.net"
-        assert new_mail != self.user.email
-        UserHelper.edit_email(self.user, new_mail, False, self.user)
-        assert self.user.email == new_mail
+def test_correct_new_name(user):
+    new_name = "A new name"
+    assert new_name != user.name
+    lib_user.edit_name(user, new_name, user)
+    assert user.name == new_name
+
+
+def test_correct_new_email(user):
+    new_mail = "user@example.net"
+    assert new_mail != user.email
+    lib_user.edit_email(user, new_mail, False, user)
+    assert user.email == new_mail
