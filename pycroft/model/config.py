056ce68a97fda2e0f5cc213b855afadb192f3b9d
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-01-31 00:01:54 +0100

    [model] migrate `config` model to sqla v2

--- pycroft/model/config.py
+++ pycroft/model/config.py
@@ -5,70 +5,73 @@
 pycroft.model.config
 ~~~~~~~~~~~~~~~~~~~~
 """
-from sqlalchemy import CheckConstraint, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, Mapped
+import typing as t
+from sqlalchemy import CheckConstraint, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column as col
 
 from pycroft.model.base import IntegerIdModel
 from pycroft.model.finance import Account, BankAccount
 from pycroft.model.user import PropertyGroup
 
 
+fkey_pgroup = t.Annotated[int, col(ForeignKey(PropertyGroup.id))]
 class Config(IntegerIdModel):
-    member_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False
-    )
-    member_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[member_group_id]
-    )
-    network_access_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
+    member_group_id: Mapped[fkey_pgroup] = col()
+    member_group: Mapped[PropertyGroup] = relationship(foreign_keys=[member_group_id])
+
+    network_access_group_id: Mapped[fkey_pgroup] = col()
     network_access_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[network_access_group_id])
-    violation_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
+        foreign_keys=[network_access_group_id]
+    )
+
+    violation_group_id: Mapped[fkey_pgroup] = col()
     violation_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[violation_group_id])
-    external_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
+        foreign_keys=[violation_group_id]
+    )
+
+    external_group_id: Mapped[fkey_pgroup] = col()
     external_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[external_group_id])
-    blocked_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
-    blocked_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[blocked_group_id])
-    caretaker_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
+        foreign_keys=[external_group_id]
+    )
+
+    blocked_group_id: Mapped[fkey_pgroup] = col()
+    blocked_group: Mapped[PropertyGroup] = relationship(foreign_keys=[blocked_group_id])
+
+    caretaker_group_id: Mapped[fkey_pgroup] = col()
     caretaker_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[caretaker_group_id])
-    treasurer_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
+        foreign_keys=[caretaker_group_id]
+    )
+
+    treasurer_group_id: Mapped[fkey_pgroup] = col()
     treasurer_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[treasurer_group_id])
-    pre_member_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False)
+        foreign_keys=[treasurer_group_id]
+    )
+
+    pre_member_group_id: Mapped[fkey_pgroup] = col()
     pre_member_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[pre_member_group_id]
+        foreign_keys=[pre_member_group_id]
     )
-    traffic_limit_exceeded_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False
-    )
+
+    traffic_limit_exceeded_group_id: Mapped[fkey_pgroup] = col()
     traffic_limit_exceeded_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[traffic_limit_exceeded_group_id]
+        foreign_keys=[traffic_limit_exceeded_group_id]
     )
-    payment_in_default_group_id: Mapped[int] = Column(
-        Integer, ForeignKey(PropertyGroup.id), nullable=False
-    )
+
+    payment_in_default_group_id: Mapped[fkey_pgroup] = col()
     payment_in_default_group: Mapped[PropertyGroup] = relationship(
-        PropertyGroup, foreign_keys=[payment_in_default_group_id]
+        foreign_keys=[payment_in_default_group_id]
     )
-    membership_fee_account_id: Mapped[int] = Column(
-        Integer, ForeignKey(Account.id), nullable=False)
+
+    membership_fee_account_id: Mapped[int] = col(ForeignKey(Account.id))
     membership_fee_account: Mapped[Account] = relationship(
-        Account, foreign_keys=[membership_fee_account_id])
-    membership_fee_bank_account_id: Mapped[Mapped[int]] = Column(
-        Integer, ForeignKey(BankAccount.id), nullable=False)
+        foreign_keys=[membership_fee_account_id]
+    )
+
+    membership_fee_bank_account_id: Mapped[int] = col(ForeignKey(BankAccount.id))
     membership_fee_bank_account: Mapped[BankAccount] = relationship(
-        BankAccount, foreign_keys=[membership_fee_bank_account_id])
-    fints_product_id: Mapped[str] = Column(String, nullable=True)
+        foreign_keys=[membership_fee_bank_account_id]
+    )
+
+    fints_product_id: Mapped[str | None]
 
     __table_args__ = (CheckConstraint("id = 1"),)
