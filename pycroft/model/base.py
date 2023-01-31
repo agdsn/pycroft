53f8c5dd1fd293dbac7b5f0877a1d4c9a7f44bdd
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-01-31 16:04:46 +0100

    [model] patch `MappedAsDataclass` to allow for dataclass usage
    Prepares #602

--- pycroft/model/base.py
+++ pycroft/model/base.py
@@ -13,7 +13,14 @@ import re
 
 import ipaddr
 from sqlalchemy import String
-from sqlalchemy.orm import declared_attr, Query, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import (
+    declared_attr,
+    Query,
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    MappedAsDataclass as OrigMappedAsDataclass,
+)
 
 from pycroft.model.session import session
 from pycroft.model.type_aliases import str50, str255, str40, mac_address
@@ -102,6 +109,35 @@ class ModelBase(DeclarativeBase, metaclass=_ModelMeta):
         def get(cls: type[T], *a, **kw) -> T:
             pass
 
+
+class _MappedAsDataclassPatchedMeta(type(OrigMappedAsDataclass), _ModelMeta):
+    ...
+
+
+class MappedAsDataclass(OrigMappedAsDataclass, metaclass=_MappedAsDataclassPatchedMeta):
+    """`MappedAsDataclass`, but with metaclass which includes our custom metaclass.
+
+    This exists because the following does not work:
+
+    .. code-block:: python
+
+        from sqlalchemy import
+        class Foo(MappedAsDataclass, ModelBase):
+            ...
+
+    The reason is that MappedAsDataclass implements its functionality with its own metaclass.
+    However, since classes can only have one metaclass, the metaclass of `MappedAsDataclass`
+    subclasses`DeclarativeMeta`.
+
+    In our case, this is not sufficient, since our `ModelBase` uses a custom metaclass
+    `_ModelMeta` for the (legacy) `.q` shorthand;
+    to fix this, we create a new metaclass inheriting from both `type(MappedAsDataclass)`
+    and `_ModelMeta`.
+    """
+
+    ...
+
+
 class IntegerIdModel(ModelBase):
     """
     Abstract base class for database models with an Integer primary column,
@@ -110,4 +146,9 @@ class IntegerIdModel(ModelBase):
 
     __abstract__ = True
 
+    # init=False is required once we're moving to dataclass-based mappings.
+    # see https://docs.sqlalchemy.org/en/20/orm/dataclasses.html#integration-with-annotated
+    # and the following chapters.
+    # however, if we inherit with a _non-dataclass-based_ model, sqlalchemy will complain about
+    # the superfluous `init=False`.
     id: Mapped[int] = mapped_column(primary_key=True)
