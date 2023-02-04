375ee680ecc837f5124e12b4de12a6de5153cb51
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-02-04 17:26:21 +0100

    Turn tabs into spaces in migration

--- pycroft/model/alembic/versions/249743eb7b94_add_hostname_column_to_dhcphost_view.py
+++ pycroft/model/alembic/versions/249743eb7b94_add_hostname_column_to_dhcphost_view.py
@@ -18,28 +18,28 @@ depends_on = None
 
 def upgrade():
     op.execute("""
-		CREATE OR REPLACE VIEW dhcphost AS
-		 SELECT interface.mac AS "Mac",
-			host(ip.address) AS "IpAddress",
-			host.name AS "Hostname"
-		   FROM "user"
-			 JOIN current_property ON "user".id = current_property.user_id AND NOT current_property.denied
-			 JOIN host ON "user".id = host.owner_id
-			 JOIN interface ON host.id = interface.host_id
-			 JOIN ip ON interface.id = ip.interface_id
-		  WHERE current_property.property_name::text = 'network_access'::text
+        CREATE OR REPLACE VIEW dhcphost AS
+         SELECT interface.mac AS "Mac",
+            host(ip.address) AS "IpAddress",
+            host.name AS "Hostname"
+           FROM "user"
+             JOIN current_property ON "user".id = current_property.user_id AND NOT current_property.denied
+             JOIN host ON "user".id = host.owner_id
+             JOIN interface ON host.id = interface.host_id
+             JOIN ip ON interface.id = ip.interface_id
+          WHERE current_property.property_name::text = 'network_access'::text
     """)
 
 
 def downgrade():
     op.execute("""
-		CREATE OR REPLACE VIEW dhcphost AS
-		 SELECT interface.mac AS "Mac",
-			host(ip.address) AS "IpAddress"
-		   FROM "user"
-			 JOIN current_property ON "user".id = current_property.user_id AND NOT current_property.denied
-			 JOIN host ON "user".id = host.owner_id
-			 JOIN interface ON host.id = interface.host_id
-			 JOIN ip ON interface.id = ip.interface_id
-		  WHERE current_property.property_name::text = 'network_access'::text
+        CREATE OR REPLACE VIEW dhcphost AS
+         SELECT interface.mac AS "Mac",
+            host(ip.address) AS "IpAddress"
+           FROM "user"
+             JOIN current_property ON "user".id = current_property.user_id AND NOT current_property.denied
+             JOIN host ON "user".id = host.owner_id
+             JOIN interface ON host.id = interface.host_id
+             JOIN ip ON interface.id = ip.interface_id
+          WHERE current_property.property_name::text = 'network_access'::text
     """)
