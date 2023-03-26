1e1410d4f3cc832cb0313a2ee2440d2f4d9630eb
Lukas Juhrich <lukas.juhrich@agdsn.de>
2023-03-26 16:20:37 +0200

    [model] use overlaps for `BankAccountActivity` relationships
    Both a bank account activity and a Split reference an Account and a
    Transaction.
    
    Indeed, if a BankAccountActivity is matched to a transaction, it must
    be matched to the (unique!) split of the transaction belonging to the
    bank account.
    
    This is an „overlap“ in foreign keys, whose consistency is checked by
    a trigger.  The `overlaps` parameter is there to assure sqlalchemy we
    know what we're doing.
    
    Indeed, setting `transaction.splits.append(Split(…))` will override
    the `transaction_id` of the split; this may override the
    `transaction_id` set by `split.bank_account_activity =
    BankAccountActivity(…)`. However, in any scenario, the overwrite would
    produce the same value, since this is the consistency the trigger
    guarantees.

--- pycroft/model/finance.py
+++ pycroft/model/finance.py
@@ -399,6 +399,7 @@ class BankAccountActivity(IntegerIdModel):
     )
     transaction: Mapped[Transaction | None] = relationship(
         back_populates="bank_account_activities",
+        overlaps="bank_account_activity",
     )
     account_id: Mapped[int | None] = mapped_column(
         ForeignKey(Account.id, onupdate="CASCADE", ondelete="SET NULL")
@@ -407,6 +408,7 @@ class BankAccountActivity(IntegerIdModel):
     split: Mapped[Split] = relationship(
         foreign_keys=(transaction_id, account_id),
         back_populates="bank_account_activity",
+        overlaps="transaction",
     )
 
     # associations
