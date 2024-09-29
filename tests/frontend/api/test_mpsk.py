e4dec3aa69f1c9f1e3fb8932fe028bdef19f8146
Alex <robokatze182@gmx.de>
2024-09-29 16:54:14 +0200

    added mpsks change tests
    added test for changing the mpsk clients
    
    Signed-off-by: Alex <robokatze182@gmx.de>
    #744

--- tests/frontend/api/test_mpsk.py
+++ tests/frontend/api/test_mpsk.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pycroft.lib.mpsk_client import mpsk_client_create
+from pycroft.model.mpsk_client import MPSKClient
 from pycroft.model.user import User
 
 from tests import factories as f
@@ -148,6 +149,97 @@ class TestGetMPSKS:
         resp = client.assert_url_ok(url, headers=auth_header, method="GET")
         assert isinstance(j := resp.json, list)
         assert len(j) == len(user.mpsk_clients)
+
+    def test_none_list(self, client, auth_header, user, url, session):
+        user.mpsk_clients.clear()
+        session.add(user)
+        session.flush()
+
+        resp = client.assert_url_ok(url, headers=auth_header, method="GET")
+        assert isinstance(j := resp.json, list)
+        assert len(j) == 0
+
+
+class TestEditMPSK:
+    VALID_DATA = {"password": "password", "mac": "00:de:ad:be:ef:00", "name": "Fancy TV"}
+    INVALID_PASSWORD = {"password": "p", "mac": "00:de:ad:be:ef:00", "name": "Fancy TV"}
+
+    @pytest.fixture(scope="module")
+    def url(self, user) -> str:
+        return f"api/v0/user/{user.id}/change-mpsk/"
+
+    @pytest.mark.parametrize(
+        "data",
+        (
+            {},
+            {"password": "password"},
+            {"mac": "00:de:ad:be:ef:00"},
+            {"name": "Fancy TV"},
+            VALID_DATA | {"mac": "badmac"},
+        ),
+    )
+    def test_unfound_change(self, client, auth_header, url, data, session):
+        client.assert_url_response_code(
+            url + "0", code=404, headers=auth_header, method="POST", data=self.VALID_DATA
+        )
+
+    def get_mpsk(self, user, session, mac="00:de:ad:be:ef:00") -> MPSKClient:
+        client = mpsk_client_create(session, owner=user, mac=mac, name="Fancy TV", processor=user)
+        session.flush()
+        return client
+
+    def test_unautherized_change(self, client, auth_header, url, session, user):
+        mpsk = self.get_mpsk(user, session)
+        client.assert_url_response_code(
+            url + str(mpsk.id),
+            code=401,
+            headers=auth_header,
+            method="POST",
+            data=self.INVALID_PASSWORD,
+        )
+
+    def test_no_change(self, client, auth_header, url, session, user):
+        mpsk = self.get_mpsk(user, session)
+        client.assert_url_ok(
+            url + str(mpsk.id), headers=auth_header, method="POST", data=self.VALID_DATA
+        )
+
+    def test_duplicate_mac(self, client, auth_header, url, session, user):
+        mpsk = self.get_mpsk(user, session)
+        mpsk1 = self.get_mpsk(user, session, mac="00:de:ad:be:ef:01")
+
+        data = self.VALID_DATA | {"mac": mpsk1.mac}
+
+        client.assert_url_response_code(
+            url + str(mpsk.id), code=409, headers=auth_header, data=data, method="POST"
+        )
+
+    @pytest.mark.parametrize(
+        "data",
+        (
+            VALID_DATA | {"name": ""},
+            VALID_DATA | {"name": " "},
+        ),
+    )
+    def test_invalid_name(self, client, auth_header, url, session, user, data):
+        mpsk = self.get_mpsk(user, session)
+        client.assert_url_response_code(
+            url + str(mpsk.id), code=400, headers=auth_header, data=data, method="POST"
+        )
+
+    @pytest.mark.parametrize(
+        "data",
+        (
+            VALID_DATA | {"mac": ""},
+            VALID_DATA | {"mac": "ff:ff:ff:ff:ff"},
+        ),
+    )
+    def test_invalid_mac(self, client, auth_header, url, session, user, data):
+        mpsk = self.get_mpsk(user, session)
+        client.assert_url_response_code(
+            url + str(mpsk.id), code=422, headers=auth_header, data=data, method="POST"
+        )
+
 
 @pytest.fixture(scope="module")
 def user(module_session) -> User:
