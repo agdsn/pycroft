6279ae61ec1dba2216002ec7fc404c6358926423
Lukas Juhrich <lukas.juhrich@agdsn.de>
2025-11-22 15:20:53 +0100

    Mark templates as final

--- pycroft/lib/mail/templates.py
+++ pycroft/lib/mail/templates.py
@@ -1,46 +1,56 @@
+import typing as t
 from .concepts import MailTemplate
 
 
+@t.final
 class UserConfirmEmailTemplate(MailTemplate):
     template = "user_confirm_email.html"
     subject = "Bitte bestätige deine E-Mail Adresse // Please confirm your email address"
 
 
+@t.final
 class UserResetPasswordTemplate(MailTemplate):
     template = "user_reset_password.html"
     subject = "Neues Passwort setzen // Set a new password"
 
 
+@t.final
 class UserMovedInTemplate(MailTemplate):
     template = "user_moved_in.html"
     subject = "Wohnortänderung // Change of residence"
 
 
+@t.final
 class UserCreatedTemplate(MailTemplate):
     template = "user_created.html"
     subject = "Willkommen bei der AG DSN // Welcome to the AG DSN"
 
 
+@t.final
 class MemberRequestPendingTemplate(MailTemplate):
     template = "member_request_pending.html"
     subject = "Deine Mitgliedschaftsanfrage // Your member request"
 
 
+@t.final
 class MemberRequestDeniedTemplate(MailTemplate):
     template = "member_request_denied.html"
     subject = "Mitgliedschaftsanfrage abgelehnt // Member request denied"
 
 
+@t.final
 class MemberRequestMergedTemplate(MailTemplate):
     template = "member_request_merged.html"
     subject = "Mitgliedskonto zusammengeführt // Member account merged"
 
 
+@t.final
 class TaskFailedTemplate(MailTemplate):
     template = "task_failed.html"
     subject = "Aufgabe fehlgeschlagen // Task failed"
 
 
+@t.final
 class MemberNegativeBalance(MailTemplate):
     template = "member_negative_balance.html"
     subject =  "Deine ausstehenden Zahlungen // Your due payments"
