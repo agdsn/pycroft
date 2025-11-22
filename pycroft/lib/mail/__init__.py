c77e7e55e28667709a8f6b2ed70d216e5f57f02d
Lukas Juhrich <lukas.juhrich@agdsn.de>
2025-11-22 14:59:26 +0100

    move `Mail` class into `pycroft.lib.mail.concepts`

--- pycroft/lib/mail/__init__.py
+++ pycroft/lib/mail/__init__.py
@@ -12,10 +12,6 @@ import traceback
 import typing as t
 from contextvars import ContextVar
 from dataclasses import dataclass, field, InitVar
-from email.header import Header
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import make_msgid, formatdate
 from functools import lru_cache
 
 import jinja2
@@ -32,42 +28,7 @@ logger = logging.getLogger('mail')
 logger.setLevel(logging.INFO)
 
 
-@dataclass
-class Mail:
-    to_name: str
-    to_address: str
-    subject: str
-    body_plain: str
-    body_html: str | None = None
-    reply_to: str | None = None
-
-    @property
-    def body_plain_mime(self) -> MIMEText:
-        return MIMEText(self.body_plain, "plain", _charset="utf-8")
-
-    @property
-    def body_html_mime(self) -> MIMEText | None:
-        if not self.body_html:
-            return None
-        return MIMEText(self.body_html, "html", _charset="utf-8")
-
-
-    def compose(self, from_: str, default_reply_to: str | None) -> MIMEMultipart:
-        msg = MIMEMultipart("alternative", _charset="utf-8")
-        msg["Message-Id"] = make_msgid()
-        msg["From"] = from_
-        msg["To"] = str(Header(self.to_address))
-        msg["Subject"] = self.subject
-        msg["Date"] = formatdate(localtime=True)
-        msg.attach(self.body_plain_mime)
-        if (html := self.body_html_mime) is not None:
-            msg.attach(html)
-        if reply_to := self.reply_to or default_reply_to:
-            msg["Reply-To"] = reply_to
-
-        print(msg)
-
-        return msg
+from .concepts import Mail
 
 class MailTemplate:
     template: str
