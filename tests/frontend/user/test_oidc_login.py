b7687f460f190c902c1a778deaa06e0a8febb392
Jonathan B. <45127993+Crasher508@users.noreply.github.com>
2026-04-09 12:14:21 +0200

    Remove unused imports

--- tests/frontend/user/test_oidc_login.py
+++ tests/frontend/user/test_oidc_login.py
@@ -1,7 +1,6 @@
 #  Copyright (c) 2026. The Pycroft Authors. See the AUTHORS file.
 #  This file is part of the Pycroft project and licensed under the terms of
 #  the Apache License, Version 2.0. See the LICENSE file for details
-from unittest.mock import patch
 
 import pytest
 from flask import url_for
