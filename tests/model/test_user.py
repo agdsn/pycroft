12c916d2dae14d8482f07211c76c4c33928d402b
Jonathan B. <45127993+Crasher508@users.noreply.github.com>
2026-04-12 14:23:59 +0200

    Replace deprecated User.q

--- tests/model/test_user.py
+++ tests/model/test_user.py
@@ -17,9 +17,9 @@ class Test_User_Passwords:
     def user(self, class_session):
         return factories.UserFactory()
 
-    def test_get(self, user):
-        assert User.get(user.login) == user
-        assert User.get(user.login + "_wrong") is None
+    def test_get(self, user, session):
+        assert User.get(user.login, session) == user
+        assert User.get(user.login + "_wrong", session) is None
 
     def test_password_hash_validator(self, user, session):
         password = generate_password(4)
